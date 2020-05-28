b9fc6f5ab1beb0131656b3d7a9857ad877cd26df
Zach Bialecki <zach.bialecki@gmail.com>
2020-05-28 19:16:27 -0400

    Add ability to search from the nav bar

--- components/nav.js
+++ components/nav.js
@@ -1,41 +1,73 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import NavUserLinks from './NavUserLinks'
+import Icon from './Icon'
 
-const Nav = () => (
-  <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
-    <div className="container">
+function Nav() {
+  const router = useRouter()
 
-      <div className="navbar-header">
-        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-          <span className="sr-only">Toggle navigation</span>
-          <span className="icon-bar" />
-          <span className="icon-bar" />
-          <span className="icon-bar" />
-        </button>
-        <Link href="/">
-          <a className="navbar-brand home push-link">
-            <strong>OpenReview</strong>
-            .net
-          </a>
-        </Link>
+  const handleSearch = (e) => {
+    e.preventDefault()
+    const formData = new FormData(e.target)
+    const query = {}
+    formData.forEach((value, name) => {
+      query[name] = value
+    })
+    router.push({ pathname: '/search', query })
+  }
+
+  return (
+    <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div className="container">
+        <div className="navbar-header">
+          <button
+            type="button"
+            className="navbar-toggle collapsed"
+            data-toggle="collapse"
+            data-target="#navbar"
+            aria-expanded="false"
+            aria-controls="navbar"
+          >
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar" />
+            <span className="icon-bar" />
+            <span className="icon-bar" />
+          </button>
+          <Link href="/">
+            <a className="navbar-brand home push-link">
+              <strong>OpenReview</strong>
+              .net
+            </a>
+          </Link>
+        </div>
+
+        <div id="navbar" className="navbar-collapse collapse">
+          <form
+            className="navbar-form navbar-left profile-search"
+            role="search"
+            onSubmit={handleSearch}
+          >
+            <div className="form-group has-feedback">
+              <input
+                name="term"
+                type="text"
+                className="form-control"
+                placeholder="Search OpenReview..."
+                autoComplete="off"
+              />
+              <Icon name="search" extraClasses="form-control-feedback" />
+            </div>
+            <input name="group" type="hidden" value="all" />
+            <input name="content" type="hidden" value="all" />
+            <input name="source" type="hidden" value="all" />
+            <input name="sort" type="hidden" value="cdate:desc" />
+          </form>
+
+          <NavUserLinks />
+        </div>
       </div>
-
-      <div id="navbar" className="navbar-collapse collapse">
-        <form className="navbar-form navbar-left profile-search" role="search">
-          <div className="form-group has-feedback">
-            <input id="search_input" type="text" className="form-control" placeholder="Search OpenReview..." autoComplete="off" />
-            <span className="glyphicon glyphicon-search form-control-feedback" aria-hidden="true" />
-          </div>
-
-          <input id="search_group" type="hidden" value="all" />
-          <input id="search_content" type="hidden" value="all" />
-        </form>
-
-        <NavUserLinks />
-      </div>
-
-    </div>
-  </nav>
-)
+    </nav>
+  )
+}
 
 export default Nav
