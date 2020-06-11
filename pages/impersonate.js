b745395015dc28d129cee7566af097530d9ddb42
xk <60613434+xkopenreview@users.noreply.github.com>
2020-06-11 10:41:49 -0400

    impersonate page with same behavior as existing impersonate

---
+++ pages/impersonate.js
@@ -0,0 +1,37 @@
+import { useEffect, useContext } from 'react'
+import withError from '../components/withError'
+import { auth, isSuperUser } from '../lib/auth'
+import api from '../lib/api-client'
+import UserContext from '../components/UserContext'
+
+const Impersonate = ({ groupIdToImpersonate, superToken }) => {
+  const { loginUser } = useContext(UserContext)
+
+  useEffect(() => {
+    // eslint-disable-next-line no-use-before-define
+    getImpersonatedToken()
+  }, [])
+
+  const getImpersonatedToken = async () => {
+    const result = await api.get('/impersonate', { groupId: groupIdToImpersonate }, { accessToken: superToken })
+    const { user, token } = result
+    loginUser(user, token)
+  }
+
+  return (
+    null
+  )
+}
+
+Impersonate.getInitialProps = async (context) => {
+  if (!context.query.groupId) {
+    return { statusCode: 400, message: 'GroupId is missing' }
+  }
+  const { user, token } = auth(context)
+  if (!isSuperUser(user)) {
+    return { statusCode: 400, message: 'Not logged in as super user' }
+  }
+  return { groupIdToImpersonate: context.query.groupId, superToken: token }
+}
+
+export default withError(Impersonate)
