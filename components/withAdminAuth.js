69a59d5d4327fbf0cca1e7f62d40fde1d21e43aa
Zach Bialecki <zach.bialecki@gmail.com>
2020-06-11 08:37:43 -0400

    Add warning

--- components/withAdminAuth.js
+++ components/withAdminAuth.js
@@ -12,8 +12,13 @@ export default function withAdminAuth(Component) {
       return { statusCode: 403, message: 'Forbidden. Access to this page is restricted.' }
     }
 
-    // This HOC does NOT call the getInitialProps method of the wrapped component
-    // it always passes the query params and access token as props
+    if (Component.getInitialProps) {
+      // withAdminAuth always passes the query params and access token as props to wrappen
+      // component, so need to warn the user if that component has getInitialProps definied
+      // eslint-disable-next-line no-console
+      console.warn('withAdminAuth does not call getInitialProps of wrapped component')
+    }
+
     return { ...ctx.query, accessToken: token }
   }
 
