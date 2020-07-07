701a526380277dea640b8caff1520e280e1fda38
xk <60613434+xkopenreview@users.noreply.github.com>
2020-07-07 11:06:55 -0400

    add feedback to error and contact page

--- components/ErrorDisplay.js
+++ components/ErrorDisplay.js
@@ -1,46 +1,55 @@
 import Head from 'next/head'
+import { useState } from 'react'
+import FeedbackModal from './FeedbackModal'
 
-const ErrorDisplay = ({ statusCode, message }) => (
-  <div className="row error-display">
-    <Head>
-      <title key="title">Error | OpenReview</title>
-    </Head>
+const ErrorDisplay = ({ statusCode, message }) => {
+  const [showFeedbackModal, setShowFeedbackModal] = useState(false)
+  return (
+    <>
+      <div className="row error-display">
+        <Head>
+          <title key="title">Error | OpenReview</title>
+        </Head>
 
-    <header className="col-xs-12 col-md-10 col-md-offset-1 text-center">
-      <h1>{`Error ${statusCode}`}</h1>
-      <hr />
-    </header>
+        <header className="col-xs-12 col-md-10 col-md-offset-1 text-center">
+          <h1>{`Error ${statusCode}`}</h1>
+          <hr />
+        </header>
 
-    <div className="col-xs-12 col-md-10 col-md-offset-1 text-center">
-      <h4>The server responded with the following message:</h4>
-      <pre className="error-message">{message}</pre>
+        <div className="col-xs-12 col-md-10 col-md-offset-1 text-center">
+          <h4>The server responded with the following message:</h4>
+          <pre className="error-message">{message}</pre>
 
-      {statusCode === 403 && (
-        <>
-          <p className="error-help">
-            <strong>
-              Important: If this URL was sent to you in an email, then in order to access this
-              page you must first add that same email address to your OpenReview profile.
-            </strong>
-          </p>
-          <p className="error-help">
-            To add a new email to your profile, navigate to the
+          {statusCode === 403 && (
+            <>
+              <p className="error-help">
+                <strong>
+                  Important: If this URL was sent to you in an email, then in order to access this
+                  page you must first add that same email address to your OpenReview profile.
+                </strong>
+              </p>
+              <p className="error-help">
+                To add a new email to your profile, navigate to the
+                {' '}
+                <a href="/profile/edit">Edit Profile page</a>
+                , click the + icon at the bottom of the Emails section and enter the new email
+                address. Then, click the Save Profile Changes button at the bottom of the page.
+              </p>
+            </>
+          )}
+
+          <p>
+            If you&apos;d like to report this error to the developers, please use the
             {' '}
-            <a href="/profile/edit">Edit Profile page</a>
-            , click the + icon at the bottom of the Emails section and enter the new email
-            address. Then, click the Save Profile Changes button at the bottom of the page.
+            { /* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+            <a href="/contact" data-toggle="modal" data-target="#feedback-modal" onClick={() => setShowFeedbackModal(true)}>Feedback form</a>
+            .
           </p>
-        </>
-      )}
-
-      <p>
-        If you&apos;d like to report this error to the developers, please use the
-        {' '}
-        <a href="/contact" data-toggle="modal" data-target="#feedback-modal">Feedback form</a>
-        .
-      </p>
-    </div>
-  </div>
-)
+        </div>
+      </div>
+      <FeedbackModal displayFlag={showFeedbackModal} cancelClickHandler={() => setShowFeedbackModal(false)} />
+    </>
+  )
+}
 
 export default ErrorDisplay
--- pages/contact.js
+++ pages/contact.js
@@ -1,36 +1,45 @@
 import Head from 'next/head'
+import { useState } from 'react'
+import FeedbackModal from '../components/FeedbackModal'
 
-const Contact = () => (
-  <div>
-    <Head>
-      <title key="title">Contact | OpenReview</title>
-    </Head>
+const Contact = () => {
+  const [showFeedbackModal, setShowFeedbackModal] = useState(false)
+  return (
+    <>
+      <div>
+        <Head>
+          <title key="title">Contact | OpenReview</title>
+        </Head>
 
-    <div className="row">
-      <div className="col-xs-12 col-md-10 col-md-offset-1"><h1>Contact Us</h1></div>
-    </div>
+        <div className="row">
+          <div className="col-xs-12 col-md-10 col-md-offset-1"><h1>Contact Us</h1></div>
+        </div>
 
-    <div className="row">
-      <div className="col-xs-12 col-md-10 col-md-offset-1">
-        <p>
-          We are currently supporting several computer science conferences and
-          workshops, and are open to hosting journals and conferences in any
-          field; please contact us at
-          {' '}
-          <a href="mailto:info@openreview.net">info@openreview.net</a>
-          {' '}
-          for more information.
-        </p>
-        <p>
-          If you would like to send feedback to the developers, please use the
-          {' '}
-          <a data-toggle="modal" data-target="#feedback-modal">Feedback form</a>
-          .
-        </p>
+        <div className="row">
+          <div className="col-xs-12 col-md-10 col-md-offset-1">
+            <p>
+              We are currently supporting several computer science conferences and
+              workshops, and are open to hosting journals and conferences in any
+              field; please contact us at
+              {' '}
+              <a href="mailto:info@openreview.net">info@openreview.net</a>
+              {' '}
+              for more information.
+            </p>
+            <p>
+              If you would like to send feedback to the developers, please use the
+              {' '}
+              {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+              <a href="#" data-toggle="modal" data-target="#feedback-modal" onClick={() => setShowFeedbackModal(true)}>Feedback form</a>
+              .
+            </p>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-)
+      <FeedbackModal displayFlag={showFeedbackModal} cancelClickHandler={() => setShowFeedbackModal(false)} />
+    </>
+  )
+}
 
 Contact.bodyClass = 'contact'
 
