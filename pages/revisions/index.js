8e8b83113e23b5267b5ce115b25da53e84149d95
Zach Bialecki <zach.bialecki@gmail.com>
2020-07-08 15:32:14 -0400

    Convert revisions page to use view.mkNotePanel

--- pages/revisions/index.js
+++ pages/revisions/index.js
@@ -1,11 +1,16 @@
+/* eslint-disable no-param-reassign */
+/* globals $: false */
+/* globals view: false */
+/* globals Handlebars: false */
 /* globals promptLogin: false */
+/* globals promptError: false */
+/* globals promptMessage: false */
 
 import { useEffect, useContext, useState } from 'react'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
 import useQuery from '../../hooks/useQuery'
 import UserContext from '../../components/UserContext'
-import Note from '../../components/Note'
 import LoadingSpinner from '../../components/LoadingSpinner'
 import ErrorAlert from '../../components/ErrorAlert'
 import api from '../../lib/api-client'
@@ -13,7 +18,9 @@ import { forumLink } from '../../lib/banner-links'
 
 import '../../styles/pages/revisions.less'
 
-const RevisionsList = ({ revisions, selectedIndexes, setSelectedIndexes }) => {
+const RevisionsList = ({
+  revisions, user, selectedIndexes, setSelectedIndexes,
+}) => {
   const toggleSelected = (idx, checked) => {
     if (checked) {
       setSelectedIndexes([...selectedIndexes, idx].sort((a, b) => a - b))
@@ -21,6 +28,95 @@ const RevisionsList = ({ revisions, selectedIndexes, setSelectedIndexes }) => {
       setSelectedIndexes(selectedIndexes.filter(existingIdx => existingIdx !== idx))
     }
   }
+
+  const showEditorModal = (note, invitation, editorOptions) => {
+    if (invitation.expdate && invitation.expdate < Date.now()) {
+      promptError('Cannot edit the note. Invitation has expired.')
+      return
+    }
+
+    $('#note-editor-modal').remove()
+    $('body').append(Handlebars.templates.genericModal({
+      id: 'note-editor-modal',
+      extraClasses: 'modal-lg',
+      showHeader: false,
+      showFooter: false,
+    }))
+    $('#note-editor-modal').modal('show')
+
+    // Tell the note editor to submit both the referent and the note id so that
+    // the API doesn't create a new reference
+    note.updateId = note.id
+
+    view.mkNoteEditor(note, invitation, user, {
+      onNoteEdited: (newNote) => {
+        $('#note-editor-modal').modal('hide')
+        promptMessage('Note updated successfully')
+        return true
+      },
+      onError: (errors) => {
+        $('#note-editor-modal .modal-body .alert-danger').remove()
+
+        $('#note-editor-modal .modal-body').prepend('<div class="alert alert-danger"><strong>Error:</strong> </div>')
+        let errorText = 'Could not save note'
+        if (errors && errors.length) {
+          errorText = window.translateErrorMessage(errors[0])
+        }
+        $('#note-editor-modal .modal-body .alert-danger').append(errorText)
+        $('#note-editor-modal').animate({ scrollTop: 0 }, 400)
+      },
+      onNoteCancelled: () => {
+        $('#note-editor-modal').modal('hide')
+      },
+      onCompleted: (editor) => {
+        $('#note-editor-modal .modal-body').empty().addClass('legacy-styles').append(editor)
+      },
+    })
+  }
+
+  const buildNotePanel = (note, revisionInvitation) => {
+    if (!revisionInvitation && note.details) {
+      note.details.originalWritable = false
+    }
+    if (note.details && typeof note.details.writable === 'undefined' && note.details.originalWritable) {
+      note.details.writable = true
+    }
+
+    return view.mkNotePanel(note, {
+      invitation: revisionInvitation,
+      withContent: true,
+      withReplyCount: false,
+      withRevisionsLink: false,
+      isReference: true,
+      withModificationDate: true,
+      withDateTime: true,
+      withBibtexLink: false,
+      user,
+      onEditRequested: (inv, options) => {
+        const noteToShow = options.original ? note.details.original : note
+        const editorOptions = options.original ? { fullNote: note } : {}
+        showEditorModal(noteToShow, revisionInvitation, editorOptions)
+      },
+      onTrashedOrRestored: () => {
+        $('.references-list').find(`#note-${note.id}`).hide()
+        promptMessage('Revision deleted')
+      },
+    }).removeClass('panel')
+  }
+
+  useEffect(() => {
+    if (!revisions) return
+
+    $('.references-list .note-container').each(function appendNotePanel(index) {
+      const [reference, invitation] = revisions[index]
+      $(this).append(buildNotePanel(reference, invitation))
+    })
+
+    // eslint-disable-next-line consistent-return
+    return () => {
+      $('#note-editor-modal').remove()
+    }
+  }, [revisions])
 
   if (!revisions) return <LoadingSpinner />
 
@@ -34,7 +130,7 @@ const RevisionsList = ({ revisions, selectedIndexes, setSelectedIndexes }) => {
       )}
 
       {revisions.map(([reference, invitation], index) => (
-        <div key={reference.id} className="row" data-id={reference.id}>
+        <div key={reference.id} className="row">
           <div className="checkbox col-sm-1">
             <label>
               <input
@@ -44,13 +140,7 @@ const RevisionsList = ({ revisions, selectedIndexes, setSelectedIndexes }) => {
               />
             </label>
           </div>
-          <div className="col-sm-11">
-            <Note
-              note={reference}
-              invitation={invitation}
-              options={{ showContents: true, pdfLink: true, htmlLink: true }}
-            />
-          </div>
+          <div className="col-sm-11 note-container" />
         </div>
       ))}
 
@@ -66,7 +156,7 @@ const Revisions = ({ appContext }) => {
   const [revisions, setRevisions] = useState(null)
   const [error, setError] = useState(null)
   const [selectedIndexes, setSelectedIndexes] = useState(null)
-  const { accessToken, userLoading } = useContext(UserContext)
+  const { user, accessToken, userLoading } = useContext(UserContext)
   const router = useRouter()
   const query = useQuery()
   const { setBannerContent, setBannerHidden } = appContext
@@ -185,6 +275,7 @@ const Revisions = ({ appContext }) => {
       ) : (
         <RevisionsList
           revisions={revisions}
+          user={user}
           selectedIndexes={selectedIndexes}
           setSelectedIndexes={setSelectedIndexes}
         />
