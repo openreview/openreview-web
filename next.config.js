ec11076954db1a6ea73d9829112b4e4658272615
Zach Bialecki <zach.bialecki@gmail.com>
2020-07-10 15:15:08 -0400

    Add SUPER_USER to .env file so it can be specified at run time, e.g. > openreview-web@0.1.0 start /Users/zb/Code/openreview-web > next start --port $NEXT_PORT
    info  - Loaded env from /Users/zb/Code/openreview-web/.env
    openreview.net

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withLess = require('@zeit/next-less')
 
 require('dotenv').config()
 
+console.log(process.env.SUPER_USER)
 // Without CSS Modules, with PostCSS
 module.exports = withLess({
   import: true,
@@ -11,7 +12,7 @@ module.exports = withLess({
     API_URL: process.env.API_URL,
     SECURE_ACTIVATION: false,
     USER_MODERATION: false,
-    SUPER_USER: 'OpenReview.net',
+    SUPER_USER: process.env.SUPER_USER,
     FEEDBACK_EMAIL: 'info@openreview.net',
     ERROR_EMAIL: 'errors@openreview.net',
   },
