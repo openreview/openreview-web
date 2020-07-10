e39c51e0313113fa589fe53ef689002a37f6f3c5
Melisa Bok <melisabok@gmail.com>
2020-07-10 11:17:03 -0400

    add more tests

--- tests/regisrer.ts
+++ tests/regisrer.ts
@@ -1,4 +1,4 @@
-import { Selector } from 'testcafe'
+import { Selector, ClientFunction } from 'testcafe'
 import api from '../lib/api-client'
 import { setup, teardown } from './test-utils.js'
 
@@ -42,12 +42,22 @@ test('request a new activation link', async t => {
     .typeText(Selector('input').withAttribute('placeholder', 'Email'), 'melisa@test.com')
     .click(Selector('a').withText('Didn\'t receive email confirmation?'))
     .expect(Selector('#flash-message-container').exists).ok()
-    .expect(Selector('span').withAttribute('class', 'important_message').innerText).eql('A confirmation email with the subject "OpenReview signup confirmation" has been sent to test@mail.com. Please click the link in this email to confirm your email address and complete registration.')
+    .expect(Selector('span').withAttribute('class', 'important_message').innerText).eql('A confirmation email with the subject "OpenReview signup confirmation" has been sent to melisa@test.com. Please click the link in this email to confirm your email address and complete registration.')
+
+  await new Promise(r => setTimeout(r, 2000));
 
   const result = await api.post('/login', { id: 'openreview.net', password: '1234' })
   const result2 = await api.get('/messages?to=melisa@test.com', {}, { accessToken: result.token })
   await t.expect(result2.messages[0].content.text).contains('http://localhost:3030/profile/activate?token=')
   await t.expect(result2.messages[1].content.text).contains('http://localhost:3030/profile/activate?token=')
+})
+
+test('request a reset password with no active profile', async t => {
+  const getPageUrl = ClientFunction(() => window.location.href.toString());
+  await t
+    .typeText(Selector('input').withAttribute('placeholder', 'Email'), 'melisa@test.com')
+    .click(Selector('a').withText('Forgot your password?'))
+    .expect(getPageUrl()).contains('http://localhost:3030/reset', { timeout: 10000 });
 })
 
 fixture `Activate`
