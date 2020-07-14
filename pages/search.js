54342cc47688b87b53f5739e968d121bdd61a9d2
Zach Bialecki <zach.bialecki@gmail.com>
2020-07-14 12:21:05 -0400

    Improve search page by using shallow routing

--- pages/search.js
+++ pages/search.js
@@ -1,9 +1,11 @@
-import { useState, useEffect } from 'react'
-import { useRouter } from 'next/router'
+import { useState, useEffect, useContext } from 'react'
 import Head from 'next/head'
-import isEmpty from 'lodash/isEmpty'
-import pick from 'lodash/pick'
+import { useRouter } from 'next/router'
+import truncate from 'lodash/truncate'
+import useQuery from '../hooks/useQuery'
 import api from '../lib/api-client'
+import { inflect } from '../lib/utils'
+import UserContext from '../components/UserContext'
 import NoteList from '../components/NoteList'
 import PaginationLinks from '../components/PaginationLinks'
 import LoadingSpinner from '../components/LoadingSpinner'
@@ -12,14 +14,16 @@ import ErrorAlert from '../components/ErrorAlert'
 // Page Styles
 import '../styles/pages/search.less'
 
-const FilterForm = ({ searchQuery, setSearchQuery }) => {
+const FilterForm = ({ searchQuery }) => {
+  const router = useRouter()
   const sourceOptions = { all: 'All', forum: 'Papers Only', reply: 'Replies Only' }
   const contentOptions = {
     all: 'All Content', authors: 'Authors', tags: 'Tags', keywords: 'Keywords',
   }
 
   const updateQuery = (field, value) => {
-    setSearchQuery({ ...searchQuery, [field]: value })
+    const newSearchQuery = { ...searchQuery, [field]: value }
+    router.push({ pathname: '/search', query: newSearchQuery }, undefined, { shallow: true })
   }
 
   return (
@@ -68,26 +72,31 @@ const FilterForm = ({ searchQuery, setSearchQuery }) => {
 }
 
 const Search = ({ appContext }) => {
-  const router = useRouter()
-  const [searchQuery, setSearchQuery] = useState(null)
+  const query = useQuery()
   const [searchResults, setSearchResults] = useState(null)
-  const [page, setPage] = useState(0)
   const [error, setError] = useState(null)
-
+  const { accessToken, userLoading } = useContext(UserContext)
+  const { setBannerHidden } = appContext
+  const page = parseInt(query?.page, 10) || 1
+  const pageSize = 25
   const displayOptions = {
     pdfLink: true,
     htmlLink: true,
     showContents: false,
+    emptyMessage: '',
   }
-  const pageSize = 25
 
   const loadSearchResults = async () => {
     try {
       const searchRes = await api.get('/notes/search', {
-        ...searchQuery,
+        term: query.term,
+        type: 'terms',
+        content: query.content || 'all',
+        group: query.group || 'all',
+        source: query.source || 'all',
         limit: pageSize,
         offset: pageSize * (page - 1),
-      }, {})
+      }, { accessToken })
 
       if (searchRes.notes) {
         setSearchResults(searchRes)
@@ -101,19 +110,17 @@ const Search = ({ appContext }) => {
   }
 
   useEffect(() => {
-    appContext.setBannerHidden(true)
+    if (userLoading || !query) return
 
-    if (isEmpty(router.query)) return
+    setBannerHidden(true)
 
-    setSearchQuery(pick(router.query, ['term', 'content', 'group', 'source', 'sort']))
-    setPage(parseInt(router.query.page, 10) || 1)
-  }, [router.query])
-
-  useEffect(() => {
-    if (!searchQuery || !page) return
+    if (!query.term) {
+      setError({ message: 'Missing search term or query' })
+      return
+    }
 
     loadSearchResults()
-  }, [searchQuery, page])
+  }, [userLoading, query])
 
   if (error) {
     return <ErrorAlert error={error} />
@@ -127,11 +134,16 @@ const Search = ({ appContext }) => {
         <title key="title">Search | OpenReview</title>
       </Head>
 
-      <FilterForm searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
+      <FilterForm searchQuery={query} />
 
       <div className="search-results">
-        {/* eslint-disable-next-line react/jsx-one-expression-per-line */}
-        <h3>{searchResults.count} results found for &quot;{searchQuery.term}&quot;</h3>
+        <h3>
+          {inflect(searchResults.count, 'result', 'results', true)}
+          {' '}
+          found for &quot;
+          {truncate(query.term, { length: 200, separator: /,? +/ })}
+          &quot;
+        </h3>
         <hr className="small" />
 
         <NoteList notes={searchResults.notes} displayOptions={displayOptions} />
@@ -141,7 +153,8 @@ const Search = ({ appContext }) => {
           itemsPerPage={pageSize}
           totalCount={searchResults.count}
           baseUrl="/search"
-          queryParams={searchQuery}
+          queryParams={query}
+          options={{ useShallowRouting: true }}
         />
       </div>
     </div>
