658e890ed6a978b0ea8f16e204bad513e1ac679e
xk <60613434+xkopenreview@users.noreply.github.com>
2020-07-16 14:41:49 -0400

    fix test error in index

--- tests/indexPage.ts
+++ tests/indexPage.ts
@@ -10,10 +10,10 @@ test('show active venues', async t => {
     .expect(Selector('#active-venues').find('a').nth(0).textContent).eql(baseGroupId)
     .expect(Selector('#active-venues').find('a').nth(1).textContent).eql(`Another${baseGroupId}`)
     //open for submission has 2 items
-    .expect(Selector('#submissions').child.length).eql(2)
-    .expect(Selector('#submissions').find('a').nth(0).textContent).eql(conferenceGroupId.replace(/\//g,' '))
-    .expect(Selector('#submissions').find('a').nth(1).textContent).eql(`Another${conferenceGroupId}`.replace(/\//g,' '))
-    .expect(Selector('.invitation_duedate').count).eql(2)
+    .expect(Selector('#open-venues').child.length).eql(2)
+    .expect(Selector('#open-venues').find('a').nth(0).textContent).eql(conferenceGroupId.replace(/\//g,' '))
+    .expect(Selector('#open-venues').find('a').nth(1).textContent).eql(`Another${conferenceGroupId}`.replace(/\//g,' '))
+    .expect(Selector('span').withText('Due').count).eql(2)
     //all venues has 2 items
     .expect(Selector('#all-venues').child.length).eql(2)
     .expect(Selector('#all-venues').find('a').nth(0).textContent).eql(baseGroupId)
