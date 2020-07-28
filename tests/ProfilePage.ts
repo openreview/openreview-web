d4e349f714c6b8bb568553f97195c83ccc53a369
xk <60613434+xkopenreview@users.noreply.github.com>
2020-07-28 16:12:46 -0400

    try same route as profile view recent pubs

--- tests/ProfilePage.ts
+++ tests/ProfilePage.ts
@@ -153,7 +153,8 @@ test('unlink paper', async (t) => {
 })
 test('check import history', async (t) => {
   const { api, superUserToken } = t.fixtureCtx
-  let result = await api.get(`/notes/search?content=authors&term=${userB.tildeId}&_=${Date.now()}`, {}, { accessToken: superUserToken })
+  // let result = await api.get(`/notes/search?content=authors&term=${userB.tildeId}&cache=false}`, {}, { accessToken: superUserToken })
+  let result = await api.get(`/notes?content.authorids=${userB.tildeId}`, {}, { accessToken: superUserToken })
   // should have only 1 note
   await t.expect(result.count).eql(1)
   const importedPaperId = result.notes[0].id
