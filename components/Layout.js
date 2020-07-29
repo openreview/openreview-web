29fd565a5f62a3c4438f481584d042edb18ac841
Zach Bialecki <zach.bialecki@gmail.com>
2020-07-28 20:10:53 -0400

    Eliminate custom _document.js by moving tags to Layout component

--- components/Layout.js
+++ components/Layout.js
@@ -5,6 +5,7 @@ import FlashAlert from './FlashAlert'
 import Footer from './Footer'
 import FooterMinimal from './FooterMinimal'
 import FeedbackModal from './FeedbackModal'
+import GoogleAnalyticsScript from './GoogleAnalyticsScript'
 
 export default function Layout({
   children, bodyClass, bannerHidden, bannerContent, fullWidth, minimalFooter,
@@ -25,6 +26,11 @@ export default function Layout({
         <meta property="og:site_name" key="og:site_name" content="OpenReview" />
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@openreviewnet" />
+
+        <link href="https://fonts.gstatic.com" rel="preconnect" crossOrigin="true" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Sans:400,400i,700,700i&display=swap&subset=latin-ext" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Nav />
@@ -51,6 +57,11 @@ export default function Layout({
       )}
 
       <FeedbackModal />
+
+      <GoogleAnalyticsScript />
+
+      {/* This script tag is necessary to prevent a flash of unstyled content */}
+      <script> </script>
     </>
   )
 }
