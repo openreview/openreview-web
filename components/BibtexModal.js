c23c6b27fecb066ac25fc72a7081c1a6945c9bca
xk <60613434+xkopenreview@users.noreply.github.com>
2020-08-10 20:35:55 -0400

    add bibtex modal

---
+++ components/BibtexModal.js
@@ -0,0 +1,31 @@
+/* globals $: false */
+import { useEffect, useState, useRef } from 'react'
+import BasicModal from './BasicModal'
+
+// eslint-disable-next-line arrow-body-style
+const BibtexModal = () => {
+  const [bibtexContent, setBibtexContent] = useState(null)
+  const bibTexContentRef = useRef(null)
+  useEffect(() => {
+    $('#bibtex-modal').on('shown.bs.modal', (e) => {
+      setBibtexContent(decodeURIComponent(e.relatedTarget.dataset.bibtex))
+    })
+    return () => {
+      $('#bibtex-modal').off('shown.bs.modal')
+    }
+  }, [])
+
+  const handleBixtexContentClick = () => {
+    window.getSelection().selectAllChildren(bibTexContentRef.current)
+  }
+
+  return (
+    <BasicModal id="bibtex-modal" title="BibTeX Record" cancelButtonText="Done" primaryButtonText={null}>
+      {/* eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions */}
+      <pre ref={bibTexContentRef} className="bibtex-content" onClick={handleBixtexContentClick}>{bibtexContent}</pre>
+      <em className="instructions">Click anywhere on the box above to highlight complete record</em>
+    </BasicModal>
+  )
+}
+
+export default BibtexModal
