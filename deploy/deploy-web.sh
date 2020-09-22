417700113dfd31ee631bbc888cfb3388b90281ee
Carlos Mondragon <carlos@openreview.net>
2020-09-22 17:16:26 -0500

    Run in background

--- deploy/deploy-web.sh
+++ deploy/deploy-web.sh
@@ -2,7 +2,7 @@
 
 nvm='export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" &&'
 
-gcloud compute --project "sunlit-realm-131518" ssh --zone "us-central1-b" openreview@openreview-web-1 --command "${nvm} bash bin/deploy-web.sh ${1}" --quiet
+gcloud compute --project "sunlit-realm-131518" ssh --zone "us-central1-b" openreview@openreview-web-1 --command "${nvm} bash bin/deploy-web.sh ${1}" --quiet &
 
 gcloud compute --project "sunlit-realm-131518" ssh --zone "us-central1-b" openreview@openreview-web-2 --command "${nvm} bash bin/deploy-web.sh ${1}" --quiet
 
