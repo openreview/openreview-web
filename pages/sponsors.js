a3d76d90b25913cb2197b1536787b248b98a2181
celestemg <acmartinez@umass.edu>
2020-12-03 09:47:49 -0500

    First draft of sponsors page

--- pages/sponsors.js
+++ pages/sponsors.js
@@ -22,6 +22,7 @@ export default function Sponsors({ sponsors }) {
       <div className="row">
         <div className="col-xs-12 col-md-10 col-md-offset-1">
           <h1>OpenReview Sponsors</h1>
+          <h5>We gratefully acknowledge the support of the OpenReview sponsors:</h5>
         </div>
       </div>
 
@@ -30,12 +31,22 @@ export default function Sponsors({ sponsors }) {
 
           <h2>Platinum</h2>
           {sponsors.platinum.map(sponsor => (
-            <Sponsor name={sponsor.name} />
+            <Sponsor name={sponsor.name} image={sponsor.image} />
           ))}
 
           <h2>Gold</h2>
           {sponsors.gold.map(sponsor => (
-            <Sponsor name={sponsor.name} />
+            <Sponsor name={sponsor.name} image={sponsor.image} />
+          ))}
+
+          <h2>Silver</h2>
+          {sponsors.silver.map(sponsor => (
+            <Sponsor name={sponsor.name} image={sponsor.image} />
+          ))}
+
+          <h2>Bronze</h2>
+          {sponsors.bronze.map(sponsor => (
+            <Sponsor name={sponsor.name} image={sponsor.image} />
           ))}
 
         </div>
@@ -49,13 +60,25 @@ export async function getStaticProps() {
     props: {
       sponsors: {
         platinum: [
-          { name: 'Google', image: 'google.jpg' },
+          { name: 'Facebook', image: 'facebook.png' },
         ],
         gold: [
-          { name: 'Google' },
+          { name: 'CZI', image: 'czi.png' },
+          { name: 'Amazon', image: 'amazon.png' },
         ],
         silver: [
-          { name: 'Google' },
+          { name: 'Google', image: 'google.jpg' },
+          { name: 'Bloomberg', image: 'bloomberg.png' },
+          { name: 'IBM', image: 'ibm.png' },
+          { name: 'DeepMind', image: 'deepmind.png' },
+        ],
+        bronze: [
+          { name: 'CVF', image: 'cvf.jpg' },
+          { name: 'MILA', image: 'mila.png' },
+          { name: 'AIR', image: 'air.png' },
+          { name: 'UMass IESL', image: 'iesl.png' },
+          { name: 'UMass CDS', image: 'cds.png' },
+          { name: 'UMass CIIR ', image: 'ciir.png' },
         ],
       },
     },
