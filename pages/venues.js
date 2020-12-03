7ceca42cc146639c4b8aca074fc979f3b1ec25cf
Parag Pachpute <paragpachpute3@gmail.com>
2020-12-03 11:53:05 -0500

    flag based display new venue page

--- pages/venues.js
+++ pages/venues.js
@@ -4,11 +4,12 @@ import withError from '../components/withError'
 import api from '../lib/api-client'
 import PaginationLinks from '../components/PaginationLinks'
 import { auth } from '../lib/auth'
+import { prettyId } from '../lib/utils'
 
 // Page Styles
 import '../styles/pages/venues.less'
 
-const VenueItem = ({ venue }) => (
+const VenueItemDBLP = ({ venue }) => (
   <p>
     {venue.content.shortname}
     {' – '}
@@ -16,6 +17,17 @@ const VenueItem = ({ venue }) => (
       <a title={`View proceedings of ${venue.content.name}`}>{venue.content.name}</a>
     </Link>
   </p>
+)
+
+// Post migration Delete this
+const VenueItem = ({ id, name }) => (
+  <h3>
+    <Link href={`/submissions?venue=${id}`}>
+      <a title="View submissions for this venue">
+        {name}
+      </a>
+    </Link>
+  </h3>
 )
 
 const Venues = ({ venues, pagination }) => (
@@ -31,18 +43,28 @@ const Venues = ({ venues, pagination }) => (
 
     <div className="groups">
       {process.env.USE_DBLP_VENUES ? venues.map(venue => (
-        <VenueItem key={venue.id} venue={venue} />
+        <VenueItemDBLP key={venue.id} venue={venue} />
       )) : (
-        <h4>Venue</h4>
+        <div className="groups">
+          <ul className="list-unstyled venues-list">
+            {venues.map(group => (
+              <li key={group.id}>
+                <VenueItem id={group.id} name={group.name} />
+              </li>
+            ))}
+          </ul>
+        </div>
       )}
     </div>
 
-    <PaginationLinks
-      currentPage={pagination.currentPage}
-      itemsPerPage={pagination.notesPerPage}
-      totalCount={pagination.totalCount}
-      baseUrl={pagination.baseUrl}
-    />
+    {process.env.USE_DBLP_VENUES && (
+      <PaginationLinks
+        currentPage={pagination.currentPage}
+        itemsPerPage={pagination.notesPerPage}
+        totalCount={pagination.totalCount}
+        baseUrl={pagination.baseUrl}
+      />
+    )}
   </>
 )
 
@@ -72,8 +94,34 @@ Venues.getInitialProps = async (ctx) => {
       baseUrl: '/venues',
     }
     return { venues, pagination }
+
+  // Post mighration delete the entire else clause
+  // eslint-disable-next-line no-else-return
   } else {
-    return {}
+    const apiRes = await api.get('/groups', { id: 'host' })
+    const group = apiRes.groups && apiRes.groups[0]
+    if (!group) {
+      return {
+        statusCode: 400,
+        message: 'Venues list unavailable. Please try again later',
+      }
+    }
+
+    const venues = group.members
+      .map(id => ({ id, name: prettyId(id) }))
+      .sort((groupA, groupB) => {
+        const nameA = groupA.name.toLowerCase()
+        const nameB = groupB.name.toLowerCase()
+        if (nameA < nameB) {
+          return -1
+        }
+        if (nameA > nameB) {
+          return 1
+        }
+        return 0
+      })
+
+    return { venues }
   }
 }
 
