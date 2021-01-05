94250cd940b8561a9419fdbf8110563bf407886d
celestemg <acmartinez@umass.edu>
2021-01-05 17:35:25 -0500

    Escape backslash

--- pages/faq.js
+++ pages/faq.js
@@ -184,7 +184,7 @@ Example:
   "location": "Burlingame, CA",
   "deadline": "Submission Deadline: November 30, 2020 11:59pm AOE",
   "website": "https://tinyml.org/home/index.html",
-  "instructions": "<h1>Important Dates</h1><li>Submission Deadline: November <strike>23</strike> 30, 2020 11:59pm AOE <b>(extended)</b></li><li>Author Notification: Jan 15th, 2021</li><li>Camera Ready: Feb 15th, 2021</li></br><h1>Submission Format</h1><li>Page limit is 6 to 8 pages, including references and any appendix material</li><li>Submissions must be blind for the double-blind review process</li><li>For paper formatting, please use the <a href=\"https://www.acm.org/publications/proceedings-template\">ACM SIGCONF template</a>.</li></br>"
+  "instructions": "<h1>Important Dates</h1><li>Submission Deadline: November <strike>23</strike> 30, 2020 11:59pm AOE <b>(extended)</b></li><li>Author Notification: Jan 15th, 2021</li><li>Camera Ready: Feb 15th, 2021</li></br><h1>Submission Format</h1><li>Page limit is 6 to 8 pages, including references and any appendix material</li><li>Submissions must be blind for the double-blind review process</li><li>For paper formatting, please use the <a href=\\"https://www.acm.org/publications/proceedings-template\\">ACM SIGCONF template</a>.</li></br>"
 }
 \`\`\`
 which will be displayed as:
