053cb5ed6707941f05ed2bbfda586961a6f8eb0c
xkopenreview <60613434+xkopenreview@users.noreply.github.com>
2021-02-23 16:37:17 -0500

    undo changes

--- components/Icon.js
+++ components/Icon.js
@@ -3,7 +3,6 @@ const Icon = ({ name, tooltip, extraClasses = '' }) => (
     className={`glyphicon glyphicon-${name} ${extraClasses}`}
     data-toggle={tooltip ? 'tooltip' : null}
     data-placement={tooltip ? 'top' : null}
-    data-container="body"
     title={tooltip || null}
     aria-hidden="true"
   />
