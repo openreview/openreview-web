a082044fcbdfe257a1d3101181633e80ec98c319
celestemartinez <32438984+celestemartinez@users.noreply.github.com>
2021-03-03 11:19:42 -0500

    Melisa feedback
    Co-authored-by: Melisa Bok <melisabok@gmail.com>

--- pages/faq.js
+++ pages/faq.js
@@ -274,9 +274,9 @@ which will be displayed as:
     id: 'question-post-submission-button',
     a: `On the request form for your venue, click on the ‘Post Submission’ button to make submissions available according to the settings selected in the fields ‘Author and Reviewer Anonymity’ and ‘Open Reviewing Policy’ of the venue request.
   This means that:
-  - If submission are double blind, blind copies of submissions will be created (make sure to select Force=True). You can also choose which fields are kept hidden (author names are automatically hidden).
+  - If submissions are double blind, blind copies of submissions will be created (make sure to select Force=True). You can also choose which fields are kept hidden (author names are automatically hidden).
   - If submissions should be public, then they will be released to everyone.
-  - If submissions are private, then they will be released to area chairs and reviewers.`,
+  - If submissions are private, then they will be released to the committee groups (senior area chairs, area chairs  and reviewers).`,
   }, {
     q: 'How do I make reviews visible to authors?',
     id: 'question-release-reviews-authors',
