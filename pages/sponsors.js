279f7ee9e36ad2da4d7bff1c03942df058fd48fc
Melisa Bok <melisabok@gmail.com>
2021-05-19 16:59:52 -0400

    update sponsors list

--- pages/sponsors.js
+++ pages/sponsors.js
@@ -28,30 +28,58 @@ export default function Sponsors({ sponsors }) {
       <div className="row">
         <div className="col-xs-12 col-md-10 col-md-offset-1 sponsors-container">
 
-          <h2>Platinum</h2>
-          <div className="row">
-            {sponsors.platinum.map(sponsor => (
-              <Sponsor name={sponsor.name} image={sponsor.image} size={5} />
-            ))}
-          </div>
-
           <h2>Gold</h2>
           <div className="row">
             {sponsors.gold.map(sponsor => (
-              <Sponsor name={sponsor.name} image={sponsor.image} size={4} />
+              <Sponsor name={sponsor.name} image={sponsor.image} size={5} />
             ))}
           </div>
 
           <h2>Silver</h2>
           <div className="row">
             {sponsors.silver.map(sponsor => (
-              <Sponsor name={sponsor.name} image={sponsor.image} size={3} />
+              <Sponsor name={sponsor.name} image={sponsor.image} size={4} />
             ))}
           </div>
 
           <h2>Bronze</h2>
           <div className="row">
             {sponsors.bronze.map(sponsor => (
+              <Sponsor name={sponsor.name} image={sponsor.image} size={3} />
+            ))}
+          </div>
+
+          <h2>Benefactors</h2>
+          <div className="row">
+            {sponsors.benefactors.map(sponsor => (
+              <Sponsor name={sponsor.name} image={sponsor.image} size={3} />
+            ))}
+          </div>
+
+          <h2>Large Patrons</h2>
+          <div className="row">
+            {sponsors.large_patrons.map(sponsor => (
+              <Sponsor name={sponsor.name} image={sponsor.image} size={2} />
+            ))}
+          </div>
+
+          <h2>Small Patrons</h2>
+          <div className="row">
+            {sponsors.small_patrons.map(sponsor => (
+              <Sponsor name={sponsor.name} image={sponsor.image} size={2} />
+            ))}
+          </div>
+
+          <h2>Supporters</h2>
+          <div className="row">
+            {sponsors.supporters.map(sponsor => (
+              <Sponsor name={sponsor.name} image={sponsor.image} size={2} />
+            ))}
+          </div>
+
+          <h2>Friends</h2>
+          <div className="row">
+            {sponsors.friends.map(sponsor => (
               <Sponsor name={sponsor.name} image={sponsor.image} size={2} />
             ))}
           </div>
@@ -66,26 +94,37 @@ export async function getStaticProps() {
   return {
     props: {
       sponsors: {
-        platinum: [
-          { name: 'Facebook', image: 'facebook.png' },
-        ],
         gold: [
-          { name: 'CZI', image: 'czi.png' },
-          { name: 'Amazon', image: 'amazon.png' },
+          { name: 'Facebook', image: 'facebook.png' },
+          { name: 'UMass IESL', image: 'umass_iesl.png' },
         ],
         silver: [
+          { name: 'Amazon', image: 'amazon.png' },
+          { name: 'CZI', image: 'czi.png' },
+        ],
+        bronze: [
           { name: 'Google', image: 'google.jpg' },
           { name: 'Bloomberg', image: 'bloomberg.png' },
           { name: 'IBM', image: 'ibm.png' },
           { name: 'DeepMind', image: 'deepmind.png' },
         ],
-        bronze: [
-          { name: 'CVF', image: 'cvf.jpg' },
+        benefactors: [
+          { name: 'NeurIPS', image: 'neurips.png' },
+          { name: 'CDS', image: 'cds.png' },
+        ],
+        large_patrons: [
           { name: 'MILA', image: 'mila.png' },
+          { name: 'ICML', image: 'icml.png' },
+          { name: 'CVF', image: 'cvf.jpg' },
+        ],
+        small_patrons: [
+          { name: 'MIDL', image: 'midl.png' },
+        ],
+        supporters: [
           { name: 'AIR', image: 'air.png' },
-          { name: 'UMass IESL', image: 'iesl.png' },
-          { name: 'UMass CDS', image: 'cds.png' },
-          { name: 'UMass CIIR ', image: 'ciir.png' },
+        ],
+        friends: [
+          { name: 'ESWC', image: 'eswc.jpeg' },
         ],
       },
     },
