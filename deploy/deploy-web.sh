f7e4827dbe44f8fd3b82bede2f766f1e28d8dfa6
Carlos Mondragon <carlos@openreview.net>
2021-05-20 11:00:02 -0500

    Deploy to running instances

--- deploy/deploy-web.sh
+++ deploy/deploy-web.sh
@@ -1,9 +1,40 @@
 #!/bin/sh
 
+instance_prefix='openreview-web-'
+
+nginx='nginx'
+
 nvm='export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" &&'
 
-gcloud compute --project "sunlit-realm-131518" ssh --zone "us-central1-b" openreview@openreview-web-1 --command "${nvm} bash bin/deploy-web.sh ${1}" --quiet &> ./deploy-out-1
+instances=$(gcloud compute instances list | grep "$instance_prefix" | grep RUNNING | tr -s ' ' | cut -d' ' -f1,2)
 
-gcloud compute --project "sunlit-realm-131518" ssh --zone "us-central1-b" openreview@openreview-web-2 --command "${nvm} bash bin/deploy-web.sh ${1}" --quiet
+instances_arr=(${instances// / })
 
-gcloud compute --project "sunlit-realm-131518" ssh --zone "us-central1-b" openreview@instance-nginx-2 --command "${nvm} bash bin/deploy.sh ${1}" --quiet
+instance_names=()
+zones=()
+for i in ${!instances_arr[@]}; do
+  if echo "${instances_arr[$i]}" | grep -q "$instance_prefix"; then
+    instance_names+=(${instances_arr[$i]})
+  else
+    zones+=(${instances_arr[$i]})
+  fi
+done
+
+nginx=$(gcloud compute instances list | grep "$nginx" | grep RUNNING | tr -s ' ' | cut -d' ' -f1,2)
+
+nginx_arr=(${nginx// / })
+nginx_name=${nginx_arr[0]}
+nginx_zone=${nginx_arr[1]}
+
+pids=()
+for i in ${!instance_names[@]}; do
+  echo Deploying to ${instance_names[$i]}
+  gcloud compute --project "sunlit-realm-131518" ssh --zone ${zones[$i]} openreview@${instance_names[$i]} --command "${nvm} bash bin/deploy-web.sh ${1}" &
+  pids+=($!)
+done
+
+for pid in ${pids[@]}; do
+  wait $pid
+done
+
+gcloud compute --project "sunlit-realm-131518" ssh --zone $nginx_zone openreview@$nginx_name --command "${nvm} bash bin/deploy.sh ${1}" --quiet
