c3ca71cd25b1166b6d48ce1bf75f77ecb52a947d
xkopenreview <60613434+xkopenreview@users.noreply.github.com>
2021-06-21 16:57:31 -0400

    fix some edge browser issues

--- components/browser/EdgeBrowser.js
+++ components/browser/EdgeBrowser.js
@@ -287,7 +287,9 @@ export default class EdgeBrowser extends React.Component {
 
   async lookupSignatures() {
     const editInvitationSignaturesMap = []
-    this.editInvitations?.forEach(async (editInvitation) => {
+    // traverseInvitation may be edited without being in edit param
+    const editTranerseInvitations = [...this.editInvitations, this.traverseInvitation]
+    editTranerseInvitations?.forEach(async (editInvitation) => {
       // this case is handled here to reduce num of calls to /groups,other cases handled at entity
       if (editInvitation.signatures['values-regex'] && !editInvitation.signatures['values-regex']?.startsWith('~.*')) {
         if (editInvitation.signatures.default) {
