a7b899c91ed6d95065a1f0afacd762ec75fbf8bf
xkopenreview <60613434+xkopenreview@users.noreply.github.com>
2021-06-28 14:43:57 -0400

    remove unused import

--- pages/profile/compare.js
+++ pages/profile/compare.js
@@ -1,6 +1,6 @@
 /* globals promptError: false */
 
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Router from 'next/router'
 import Head from 'next/head'
 import isEmpty from 'lodash/isEmpty'
