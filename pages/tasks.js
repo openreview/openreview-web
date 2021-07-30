4f3174f4ad658785bae3dbc480ffec4db1f8eaa8
xukun <xukun@openreview.net>
2021-07-30 18:16:11 -0400

    add call to get v2 invitations in v1 format

--- pages/tasks.js
+++ pages/tasks.js
@@ -55,17 +55,53 @@ const Tasks = ({ appContext }) => {
       apiRes.invitations.map(inv => ({ ...inv, [propertyName]: true }))
     )
 
-    Promise.all([
-      api.get('/invitations', {
-        invitee: true, duedate: true, replyto: true, details: 'replytoNote,repliedNotes',
-      }, { accessToken, cachePolicy: 'no-ie' }).then(addPropertyToInvitations('noteInvitation')),
-      api.get('/invitations', {
-        invitee: true, duedate: true, type: 'tags', details: 'repliedTags',
-      }, { accessToken, cachePolicy: 'no-ie' }).then(addPropertyToInvitations('tagInvitation')),
-      api.get('/invitations', {
-        invitee: true, duedate: true, type: 'edges', details: 'repliedEdges',
-      }, { accessToken, cachePolicy: 'no-ie' }).then(addPropertyToInvitations('tagInvitation')),
-    ])
+    let invitationPromises = []
+    const commonQueryParam = {
+      invitee: true, duedate: true, details: 'repliedTags',
+    }
+    const commonOption = { accessToken, cachePolicy: 'no-ie' }
+    if (process.env.API_V2_URL) {
+      invitationPromises = [
+        api.getCombined({
+          path: '/invitations',
+          data1: {
+            ...commonQueryParam, replyto: true, details: 'replytoNote,repliedNotes',
+          },
+          data2: {
+            ...commonQueryParam, replyto: true, details: 'replytoNote,repliedNotes', version: 1,
+          },
+          options: commonOption,
+        }).then(addPropertyToInvitations('noteInvitation')),
+        api.getCombined({
+          path: '/invitations',
+          data1: {
+            ...commonQueryParam, type: 'tags',
+          },
+          data2: {
+            ...commonQueryParam, type: 'tags', version: 1,
+          },
+          options: commonOption,
+        }).then(addPropertyToInvitations('tagInvitation')),
+        api.getCombined({
+          path: '/invitations',
+          data1: {
+            ...commonQueryParam, type: 'edges', details: 'repliedEdges',
+          },
+          data2: {
+            ...commonQueryParam, type: 'edges', details: 'repliedEdges', version: 1,
+          },
+          options: commonOption,
+        }).then(addPropertyToInvitations('tagInvitation')),
+      ]
+    } else {
+      invitationPromises = [
+        api.get('/invitations', { ...commonQueryParam, replyto: true, details: 'replytoNote,repliedNotes' }, commonOption).then(addPropertyToInvitations('noteInvitation')),
+        api.get('/invitations', { ...commonQueryParam, type: 'tags' }, commonOption).then(addPropertyToInvitations('tagInvitation')),
+        api.get('/invitations', { ...commonQueryParam, type: 'edges', details: 'repliedEdges' }, commonOption).then(addPropertyToInvitations('tagInvitation')),
+      ]
+    }
+
+    Promise.all(invitationPromises)
       .then(allInvitations => setGroupedTasks(formatTasksData(allInvitations)))
       .catch(apiError => setError(apiError))
   }, [accessToken])
