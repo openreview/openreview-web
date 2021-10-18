19f0376ff4a13e2c4b223edc6e6e3b78e39d89b9
Zach Bialecki <zach.bialecki@gmail.com>
2021-10-18 14:55:33 -0400

    Enable proper Google Scholar author meta tags

--- pages/forum.js
+++ pages/forum.js
@@ -67,13 +67,9 @@ const ForumPage = ({ forumNote, query, appContext }) => {
             {content.title && (
               <meta name="citation_title" content={content.title} />
             )}
-            {/*
             {authors.map(author => (
               <meta key={author} name="citation_author" content={author} />
             ))}
-            */}
-            {/* temporary hack to get google scholar to work, revert to above when next.js unique issue is solved */}
-            <meta name="citation_authors" content={authors.join('; ')} />
             <meta name="citation_publication_date" content={creationDate} />
             <meta name="citation_online_date" content={modificationDate} />
             {content.pdf && (
