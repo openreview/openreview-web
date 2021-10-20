672a693d5d478cf9f18143f508d39793bc66c6ef
Zach Bialecki <86174+zbialecki@users.noreply.github.com>
2021-10-20 12:37:41 -0400

    Feature/ Use Next.js env loader (#656)
    * Remove dotenv dependency and use Next.js' built in env loader
    
    * Update README

--- next.config.js
+++ next.config.js
@@ -1,23 +1,16 @@
 const withLess = require('@zeit/next-less')
 const nextBuildId = require('next-build-id')
 
-require('dotenv').config()
-
 // Without CSS Modules, with PostCSS
 module.exports = withLess({
   import: true,
   env: {
     IS_PRODUCTION: process.env.NODE_ENV === 'production',
     IS_STAGING: process.env.NODE_ENV === 'staging',
-    NEXT_PORT: process.env.NEXT_PORT,
     API_URL: process.env.API_URL,
     API_V2_URL: process.env.API_V2_URL,
-    SECURE_ACTIVATION: false,
-    USER_MODERATION: false,
-    USE_DBLP_VENUES: process.env.USE_DBLP_VENUES === 'true',
     SUPER_USER: process.env.SUPER_USER,
-    FEEDBACK_EMAIL: 'info@openreview.net',
-    ERROR_EMAIL: 'errors@openreview.net',
+    USE_DBLP_VENUES: process.env.USE_DBLP_VENUES === 'true',
     GA_PROPERTY_ID: process.env.GA_PROPERTY_ID,
   },
   generateBuildId: () => nextBuildId({ dir: __dirname, describe: true }),
