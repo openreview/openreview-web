26eaec45a64d6a17db1063f62b5be35098646ce1
xkopenreview <60613434+xkopenreview@users.noreply.github.com>
2021-11-23 15:33:58 -0500

    Fix/react implementation of profile edit page (#661)
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * validation; dblp import
    
    * validation related changes
    
    * active page changes
    
    * switch edit page
    
    * fix test wip
    
    * fix merge profile error message
    
    * fix error message display
    
    * fix all tests
    
    * fix lint
    
    * fix register tests
    
    * some changes based on feedback
    
    * use icon component
    
    * performance improvement; fix preferred email
    
    * fix test
    
    * some changes based on feedback
    
    * fix test
    
    * some changes based on feedback; performance improvements
    
    * fix test
    
    * some changes based on feedback
    
    * fix test
    
    * change responsive screen size to lg
    
    * move links validation to child component
    
    * fix test
    
    * fix expertise display
    
    * enable clear dropdown
    
    * use new error format
    
    * add env var to use new profile edit page on condition
    
    * cleanup
    
    * add new param to build config
    
    * remove legacy route; add empty domain check
    
    * fix alt names
    
    * use new error format
    
    * removed unused import
    
    * Code formatting and cleanup
    
    * Refactor code to simplify and increase cohesion
    
    * More CSS tweaks, mainly for mobile
    
    * More formatting fixes
    
    * change flag to string
    
    * add missing param
    
    * Add CSS to reduce size of minus buttons
    
    * update format param and post param
    
    Co-authored-by: Melisa Bok <melisabok@gmail.com>
    Co-authored-by: Zach Bialecki <86174+zbialecki@users.noreply.github.com>
    Co-authored-by: Zach Bialecki <zach.bialecki@gmail.com>

---
+++ components/profile/PersonalLinksSection.js
@@ -0,0 +1,150 @@
+/* globals $,promptError: false */
+
+import { useEffect, useReducer } from 'react'
+import DblpImportModal from '../DblpImportModal'
+import Icon from '../Icon'
+import { isValidURL } from '../../lib/utils'
+
+const PersonalLinkInput = ({ type, links, setLinks }) => {
+  const handleBlur = (e) => {
+    const { value } = e.target
+    if (!value.trim()?.length) return
+    switch (type) {
+      case 'gscholar': {
+        const isValid = isValidURL(value) && value.startsWith('https://scholar.google')
+        if (!isValid) promptError(`${value} is not a valid Google Scholar URL`, { scrollToTop: false })
+        setLinks({ type, payload: { data: value, valid: isValid } })
+        break
+      }
+      case 'semanticScholar': {
+        const isValid = isValidURL(value) && value.startsWith('https://www.semanticscholar.org')
+        if (!isValid) promptError(`${value} is not a valid Semantic Scholar URL`, { scrollToTop: false })
+        setLinks({ type, payload: { data: value, valid: isValid } })
+        break
+      }
+      default: {
+        const isValid = isValidURL(value)
+        if (!isValid) promptError(`${value} is not a valid ${type} URL`, { scrollToTop: false })
+        setLinks({ type, payload: { data: value, valid: isValid } })
+      }
+    }
+  }
+  return (
+    <input
+      id={`${type}_url`}
+      className={`form-control personal-links__input ${links[type]?.valid === false ? 'invalid-value' : ''}`}
+      value={links[type]?.value ?? ''}
+      onChange={(e) => { setLinks({ type, payload: { data: e.target.value } }) }}
+      onBlur={handleBlur}
+    />
+  )
+}
+
+const PersonalLinksSection = ({
+  profileLinks, updateLinks, profileId, names, preferredEmail, renderPublicationsEditor, hideDblpButton,
+}) => {
+  const linksReducer = (state, action) => ({
+    ...state,
+    [action.type]: {
+      value: action.payload.data,
+      valid: action.payload.valid,
+    },
+  })
+
+  const [links, setLinks] = useReducer(linksReducer, profileLinks ?? {})
+
+  const handleAddDBLPButtonClick = () => {
+    $('#dblp-import-modal').modal({
+      show: true,
+      backdrop: 'static',
+      keyboard: false,
+    })
+  }
+
+  useEffect(() => {
+    updateLinks(links)
+    $('#dblp-import-modal').on('hidden.bs.modal', () => {
+      renderPublicationsEditor()
+    })
+  }, [links])
+
+  return (
+    <div className="container personal-links">
+      <div className="row">
+        <div className="col-md-4 personal-links__column">
+          <div className="small-heading">Homepage URL</div>
+          <PersonalLinkInput type="homepage" links={links} setLinks={setLinks} />
+        </div>
+        <div className="col-md-4 personal-links__column">
+          <div className="small-heading">Google Scholar URL</div>
+          <PersonalLinkInput type="gscholar" links={links} setLinks={setLinks} />
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-md-4 personal-links__column">
+          <div className="small-heading">
+            DBLP URL
+            <a className="personal-links__faqlink" href="/faq#question-dblp-import" target="_blank" rel="noreferrer">
+              <Icon name="info-sign" />
+            </a>
+          </div>
+          <PersonalLinkInput type="dblp" links={links} setLinks={setLinks} />
+        </div>
+        <div className="col-md-4 personal-links__column">
+          <div className="row ml-0 hidden-xs hidden-sm">
+            <div className="small-heading">&nbsp;</div>
+          </div>
+          {!hideDblpButton && (
+            <button
+              className="btn btn-primary personal-links__adddblpbtn"
+              type="button"
+              disabled={!links.dblp?.value}
+              onClick={handleAddDBLPButtonClick}
+            >
+              Add DBLP Papers to Profile
+            </button>
+          )}
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-md-4 personal-links__column">
+          <div className="small-heading">ORCID URL</div>
+          <PersonalLinkInput type="orcid" links={links} setLinks={setLinks} />
+        </div>
+        <div className="col-md-4 personal-links__column">
+          <div className="small-heading">Wikipedia URL</div>
+          <PersonalLinkInput type="wikipedia" links={links} setLinks={setLinks} />
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-md-4 personal-links__column">
+          <div className="small-heading">Linkedin URL</div>
+          <PersonalLinkInput type="linkedin" links={links} setLinks={setLinks} />
+        </div>
+        <div className="col-md-4 personal-links__column">
+          <div className="small-heading">
+            Semantic Scholar URL
+            <a className="personal-links__faqlink" href="/faq#question-semantic-scholar" target="_blank" rel="noreferrer">
+              <Icon name="info-sign" />
+            </a>
+          </div>
+          <PersonalLinkInput type="semanticScholar" links={links} setLinks={setLinks} />
+        </div>
+      </div>
+
+      <DblpImportModal
+        profileId={profileId}
+        profileNames={names?.map(name => (
+          name.middle ? `${name.first} ${name.middle} ${name.last}` : `${name.first} ${name.last}`
+        ))}
+        email={preferredEmail}
+        updateDBLPUrl={(url) => { setLinks({ type: 'dblp', data: url }) }}
+      />
+    </div>
+  )
+}
+
+export default PersonalLinksSection
