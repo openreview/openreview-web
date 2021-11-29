3b37a7559f7e05f68c810b2adc428ce191158941
Nadia L\'Bahy <nlbahy@umass.edu>
2021-11-29 15:15:32 -0500

    added line about _ to faq on latex

--- pages/faq.js
+++ pages/faq.js
@@ -144,7 +144,7 @@ Keep in mind that your mathematics is part of an HTML document, so you need to b
     id: 'question-tex-not-displaying-properly',
     a: `One possible reason is that the macro is not supported by MathJax. If this is the case then macro will appear as plain red text in with the rendered TeX.
 
-Another possibility is that the field has Markdown enabled, but not all the backslashes in the TeX notation were escaped. This can lead to some layout problems, such as all the elements of a matrix appearing in 1 row instead of many.
+Another possibility is that the field has Markdown enabled, but not all the backslashes in the TeX notation were escaped. This can lead to some layout problems, such as all the elements of a matrix appearing in 1 row instead of many. Similarly, underscores should also be escaped with a backslash: '\\\\_'.
 
 For more details on the difference between OpenReview's TeX support and other systems, see the answer above.`,
   }, {
