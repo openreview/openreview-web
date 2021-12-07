4f9d36493802ee961ad5554765d35d39759056a5
Nadia L\'Bahy <nlbahy@umass.edu>
2021-12-07 14:11:11 -0500

    added picture of form

--- pages/faq.js
+++ pages/faq.js
@@ -463,19 +463,21 @@ The reviewer can then respond to the invitation. If you want to invite a reviewe
   {
     q: 'How can I calculate Affinity Scores and Conflicts?',
     id: 'question-paper-matching-setup',
-    a: `You can calculate affinity scores and conflicts using the 'Paper Matching Setup' feature from your venue request page. Paper Matching Setup is enabled for any venue with a submission deadline and a selection for the 'Paper Matching' field of the venue request form. This feature allows Program Chairs to compute or upload affinity scores and/or compute conflicts. 
+    a: `You can calculate affinity scores and conflicts for your venue using OpenReview's 'Paper Matching Setup' feature. Paper Matching Setup is enabled for any venue that selected an option for the 'Paper Matching' question on the venue request form and that has set a submission deadline. This feature allows Program Chairs to compute or upload affinity scores and/or compute conflicts. 
 
-    You can find 'Paper Matching Setup' on your venue request form next to 'Remind Recruitment'. 
+You can find the 'Paper Matching Setup' button on your venue request form next to 'Remind Recruitment'. 
 
-    <img src="/images/faq-paper-matching-button.png" alt="Paper Matching Button" class="img-answer"/></br> 
+<img src="/images/faq-paper-matching-button.png" alt="Paper Matching Button" class="img-answer"/></br> 
 
-    Clicking it should bring up the following form. The ‘Matching Group’ is a dropdown menu of the groups you can use in the matcher (Reviewers, Area Chairs, Senior Area Chairs), depending on whichever you selected for your venue. You can select if you would like affinity scores and/or conflicts computed. Alternatively, you can compute and upload your own affinity scores using the OpenReview expertise API: https://github.com/openreview/openreview-expertise
+Clicking it should bring up the following form. The 'Matching Group' is a dropdown menu of the groups you can use in the matcher (Reviewers, Area Chairs, Senior Area Chairs), depending on whichever you selected for your venue. You can select if you would like affinity scores and/or conflicts computed. Alternatively, you can compute and upload your own affinity scores using the OpenReview expertise API: https://github.com/openreview/openreview-expertise
 
-    Running the paper matching setup should output a comment on your venue request page. If there were members missing profiles or publications, the message will identify them and say ‘Affinity scores and/or conflicts could not be computed for these users. Please ask these users to sign up in OpenReview and upload their papers. Alternatively, you can remove these users from the Reviewers group.’ This message does not mean that the process failed, but that those members were excluded from the calculations. You can run the Paper Matching Setup as many times as you want or until all users have completed profiles. 
+![paper matching form](/images/faq-paper-matching-form.png)
 
-    You can confirm that the affinity scores were computed by checking if an invitation for the scores was created: https://api.openreview.net/edges?invitation=your/venue/id/-/Affinity_Score
+Running the paper matching setup should output a comment on your venue request page. If there were members missing profiles or publications, the message will identify them and say 'Affinity scores and/or conflicts could not be computed for these users. Please ask these users to sign up in OpenReview and upload their papers. Alternatively, you can remove these users from the Reviewers group.' This message does not mean that the process failed, but that those members were excluded from the calculations. You can run the Paper Matching Setup as many times as you want or until all users have completed profiles. 
 
-    After you run Paper Matching Setup, a link for ‘Paper Assignments’ should appear on your Program Chair console. From there, you should be able to use the edge browser to automatically make assignments: https://openreview.net/faq#question-edge-browswer
+You can confirm that the affinity scores were computed by checking if an invitation for the scores was created: https://api.openreview.net/edges?invitation=your/venue/id/-/Affinity_Score
+
+After you compute the scores by running Paper Matching Setup, a link for 'Paper Assignments' should appear on your Program Chair console. From there, you should be able to use the edge browser to automatically make assignments: https://openreview.net/faq#question-edge-browswer
 
     `,
   },
