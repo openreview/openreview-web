bf04e9dec1c5b5654a0bf03cd189cb0bddf4b1f4
xukun <xukun@openreview.net>
2021-12-09 11:54:54 -0500

    fix merge error

--- components/ErrorDisplay.js
+++ components/ErrorDisplay.js
@@ -27,39 +27,20 @@ const ErrorDisplay = ({ statusCode, message }) => (
           <p className="error-help">
             To add a new email to your profile, navigate to the
             {' '}
-            <a href="/profile/edit">Edit Profile page</a>
+            <Link href="/profile/edit"><a>Edit Profile page</a></Link>
             , click the + icon at the bottom of the Emails section and enter the new email
             address. Then, click the Save Profile Changes button at the bottom of the page.
           </p>
         </>
       )}
 
-        {statusCode === 403 && (
-          <>
-            <p className="error-help">
-              <strong>
-                Important: If this URL was sent to you in an email, then in order to access this
-                page you must first add that same email address to your OpenReview profile.
-              </strong>
-            </p>
-            <p className="error-help">
-              To add a new email to your profile, navigate to the
-              {' '}
-              <Link href="/profile/edit"><a>Edit Profile page</a></Link>
-              , click the + icon at the bottom of the Emails section and enter the new email
-              address. Then, click the Save Profile Changes button at the bottom of the page.
-            </p>
-          </>
-        )}
-
-        <p>
-          If you&apos;d like to report this error to the developers, please use the
-          {' '}
-          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-          <a href="#" data-toggle="modal" data-target="#feedback-modal">Feedback form</a>
-          .
-        </p>
-      </div>
+      <p>
+        If you&apos;d like to report this error to the developers, please use the
+        {' '}
+        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+        <a href="#" data-toggle="modal" data-target="#feedback-modal">Feedback form</a>
+        .
+      </p>
     </div>
   </div>
 )
