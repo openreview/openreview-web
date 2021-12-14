328a17539ff0b722cef24410212a3880893d6af3
xukun <xukun@openreview.net>
2021-12-14 16:34:01 -0500

    update relatedInvitation and signedNotes

--- components/group/GroupRelatedInvitations.js
+++ components/group/GroupRelatedInvitations.js
@@ -1,4 +1,3 @@
-/* globals promptError: false */
 import { useEffect, useState } from 'react'
 import Link from 'next/link'
 import { getGroupVersion, prettyId } from '../../lib/utils'
@@ -6,60 +5,45 @@ import api from '../../lib/api-client'
 import EditorSection from '../EditorSection'
 import PaginatedList from '../PaginatedList'
 
-const RelatedInvitationRow = ({ relatedInvitation }) => (
-  <li>
-    <Link href={`/invitation/edit?id=${relatedInvitation.id}`}>
-      <a>{prettyId(relatedInvitation.id)}</a>
+const RelatedInvitationRow = ({ item }) => (
+    <Link href={`/invitation/edit?id=${item.id}`}>
+      <a>{prettyId(item.id)}</a>
     </Link>
-  </li>
 )
 
 const GroupRelatedInvitations = ({ groupId, accessToken }) => {
-  const [relatedInvitations, setRelatedInvitations] = useState([])
   const [totalCount, setTotalCount] = useState(null)
-  const [currentPage, setCurrentPage] = useState(1)
   const groupVersion = getGroupVersion(groupId)
 
   const loadRelatedInvitations = async (limit = 15, offset = 0) => {
-    try {
-      const result = await api.get(
-        '/invitations',
-        {
-          regex: `${groupId}/-/.*`,
-          expired: true,
-          type: 'all',
-          limit,
-          offset,
-        },
-        { accessToken, version: groupVersion },
-      )
-      setTotalCount(result.count)
-      setRelatedInvitations(result.invitations)
-    } catch (error) {
-      promptError(error.message)
+    const result = await api.get(
+      '/invitations',
+      {
+        regex: `${groupId}/-/.*`,
+        expired: true,
+        type: 'all',
+        limit,
+        offset,
+      },
+      { accessToken, version: groupVersion },
+    )
+    setTotalCount(result.count)
+    return {
+      items: result.invitations,
+      count: result.count,
     }
   }
-
-  const renderRelatedInvitation = invitation => <RelatedInvitationRow
-    key={invitation.id}
-    relatedInvitation={invitation}
-    version={groupVersion}
-  />
 
   useEffect(() => {
     loadRelatedInvitations()
   }, [])
 
-  if (!relatedInvitations.length) return null
   return (
     <EditorSection title={`Related Invitations ${totalCount ? `(${totalCount})` : ''}`}>
       <PaginatedList
-        items={relatedInvitations}
-        renderItem={renderRelatedInvitation}
-        totalCount={totalCount}
+        ListItem={RelatedInvitationRow}
         loadItems={loadRelatedInvitations}
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
+        emptyMessage="No related invitations"
       />
     </EditorSection>
   )
--- components/group/GroupSignedNotes.js
+++ components/group/GroupSignedNotes.js
@@ -12,43 +12,37 @@ const SignedNotesRow = ({ signedNote, version }) => {
   const title = version === 2 ? signedNote.content?.title?.value : signedNote.content?.title
   const user = version === 2 ? signedNote.content?.user?.value : signedNote.content?.user
   return (
-    <li>
-      <Link
-        href={`/forum?id=${signedNote.forum}${signedNote.forum === signedNote.id ? '' : `&noteId=${signedNote.id}`}`}
-      >
-        <a>
-          {`${prettyInvitationId(invitationId)}: ${title ?? signedNote.forum}${user ? ` - ${user}` : ''}`}
-        </a>
-      </Link>
-    </li>
+    <Link
+      href={`/forum?id=${signedNote.forum}${signedNote.forum === signedNote.id ? '' : `&noteId=${signedNote.id}`}`}
+    >
+      <a>
+        {`${prettyInvitationId(invitationId)}: ${title ?? signedNote.forum}${user ? ` - ${user}` : ''}`}
+      </a>
+    </Link>
   )
 }
 
 const GroupSignedNotes = ({ groupId, accessToken }) => {
-  const [signedNotes, setSignedNotes] = useState([])
   const [totalCount, setTotalCount] = useState(null)
-  const [currentPage, setCurrentPage] = useState(1)
   const groupVersion = getGroupVersion(groupId)
 
   const loadNotes = async (limit = 15, offset = 0) => {
-    try {
-      // TODO: how signatures is passed to api may change
-      const result = await api.get(
-        '/notes',
-        { 'signatures[]': [groupId], limit, offset },
-        { accessToken, version: groupVersion },
-      )
-      setTotalCount(result.count)
-      setSignedNotes(result.notes)
-    } catch (error) {
-      promptError(error.message)
+    // TODO: how signatures is passed to api may change
+    const result = await api.get(
+      '/notes',
+      { 'signatures[]': [groupId], limit, offset },
+      { accessToken, version: groupVersion },
+    )
+    setTotalCount(result.count)
+    return {
+      items: result.notes,
+      count: result.count,
     }
   }
 
-  const renderSignedNote = signedNote => (
+  const renderSignedNote = ({ item }) => (
     <SignedNotesRow
-      key={signedNote.id}
-      signedNote={signedNote}
+      signedNote={item}
       version={groupVersion}
     />
   )
@@ -57,17 +51,12 @@ const GroupSignedNotes = ({ groupId, accessToken }) => {
     loadNotes()
   }, [])
 
-  if (signedNotes.length) return null
-
   return (
     <EditorSection title={`Signed Notes ${totalCount ? `(${totalCount})` : ''}`}>
       <PaginatedList
-        items={signedNotes}
-        renderItem={renderSignedNote}
-        totalCount={totalCount}
+        ListItem={renderSignedNote}
         loadItems={loadNotes}
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
+        emptyMessage="No signed notes"
       />
     </EditorSection>
   )
