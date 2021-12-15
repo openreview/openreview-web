0f4a2bf8377777df575738197478cd8542922180
xukun <xukun@openreview.net>
2021-12-15 11:30:56 -0500

    update invitation page to use updated paginationlist

---
+++ components/BasePaginatedList.js
@@ -0,0 +1,27 @@
+import PaginationLinks from './PaginationLinks'
+
+export default function BasePaginatedList({
+  listItems, totalCount, currentPage, setCurrentPage, itemsPerPage, ListItem, emptyMessage, className,
+}) {
+  return (
+    <div className={className}>
+      <ul className="list-unstyled">
+        {listItems?.length > 0 ? listItems.map(item => (
+          <li key={item.id}>
+            <ListItem item={item} />
+          </li>
+        )) : (
+          <li><p className="empty-message">{emptyMessage || 'No items to display'}</p></li>
+        )}
+      </ul>
+
+      <PaginationLinks
+        setCurrentPage={setCurrentPage}
+        totalCount={totalCount}
+        itemsPerPage={itemsPerPage}
+        currentPage={currentPage}
+        options={{ noScroll: true, noBottomMargin: true }}
+      />
+    </div>
+  )
+}
