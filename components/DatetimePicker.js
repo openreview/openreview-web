e3c95df2872531b1ee73f49922ecd7226e5f512b
xukun <xukun@openreview.net>
2022-01-04 12:19:16 -0500

    fix date picker

--- components/DatetimePicker.js
+++ components/DatetimePicker.js
@@ -4,7 +4,6 @@ import { useState } from 'react'
 import locale from 'rc-picker/lib/locale/en_US'
 import timezone from 'dayjs/plugin/timezone'
 import utc from 'dayjs/plugin/utc'
-import { nanoid } from 'nanoid'
 import dayjsGenerator from '../lib/dayjsGenerator'
 import { getDefaultTimezone } from '../lib/utils'
 
@@ -13,16 +12,12 @@ dayjs.extend(utc)
 
 const DatetimePicker = ({ existingValue, onChange, timeZone }) => {
   const [value, setValue] = useState(existingValue ? dayjs(existingValue) : '')
-  const [open, setOpen] = useState(false)
 
   const handleOkClick = (e) => {
     onChange(e.tz(timeZone ?? getDefaultTimezone().value, true).valueOf())
-    setOpen(false)
   }
 
   return <Picker
-    key={nanoid()}
-    open={open}
     generateConfig={dayjsGenerator}
     showTime={{
       showSecond: false,
@@ -36,8 +31,6 @@ const DatetimePicker = ({ existingValue, onChange, timeZone }) => {
       if (!date) onChange(null)
     }}
     placeholder="Select datetime"
-    onBlur={() => { setOpen(false) }}
-    onClick={() => { setOpen(true) }}
     autoFocus
     allowClear
   />
