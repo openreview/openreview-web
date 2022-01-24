19af372555e2f87257208d92d234011016d94cc7
xkopenreview <60613434+xkopenreview@users.noreply.github.com>
2022-01-24 09:35:55 -0500

    Fix/react assignment stat page (#782)
    * wip
    
    * scalar stats
    
    * wip
    
    * remove unused prop
    
    * load data with promise all
    
    * remove client script
    
    * remove unused file
    
    * try fixing package lock
    
    * try fixing package lock
    
    * style change; add hover rectange
    
    * fix lint
    
    * move calculation to separate functions
    
    * fix lint
    
    * Extract function, dedupe package-lock
    
    * Small code changes
    
    * Update histogram title positioning
    
    * Change back button dropdown, fix bug with Recommendation Distributions charts
    
    Co-authored-by: Zach Bialecki <zach@openreview.net>

---
+++ components/assignments/ScalarStat.js
@@ -0,0 +1,12 @@
+import LoadingSpinner from '../LoadingSpinner'
+
+const ScalarStat = ({ value, name }) => (
+  <div className="stat-scalar">
+    {typeof value === 'undefined'
+      ? <LoadingSpinner inline text={null} />
+      : <div className="stat-val">{value}</div>}
+    <div className="stat-name">{name}</div>
+  </div>
+)
+
+export default ScalarStat
