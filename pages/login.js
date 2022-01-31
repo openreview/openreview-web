51365f2dcec8e1c6c145d3f2ca4930d739e70889
xkopenreview <60613434+xkopenreview@users.noreply.github.com>
2022-01-31 15:22:04 -0500

    Revert "Feature/ Token refresh (#800)" (#817)
    This reverts commit bdf9a933f1843674e49b8d23790837c34bd5613f.

--- pages/login.js
+++ pages/login.js
@@ -24,7 +24,7 @@ const LoginForm = () => {
 
     try {
       const { user, token } = await api.post('/login', { id: email, password })
-      loginUser(user, token, query?.redirect ?? '/')
+      loginUser(user, token, query?.redirect)
     } catch (error) {
       setLoginError(error)
       promptError(error.message)
