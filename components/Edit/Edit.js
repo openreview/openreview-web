e42c3f138d48b25214afe886f50786bbcade0643
xukun <xukun@openreview.net>
2022-02-03 15:38:25 -0500

    fix lint

--- components/Edit/Edit.js
+++ components/Edit/Edit.js
@@ -3,49 +3,47 @@ import { NoteContentV2 } from '../NoteContent'
 import EditTitle from './EditTitle'
 import EditValue from './EditValue'
 
-const Edit = ({ edit, options }) => {
-  return (
-    <div className={`edit ${options.extraClasses ?? ''}`}>
-      <EditTitle invitation={edit.invitations[0]} signatures={edit.signatures} />
+const Edit = ({ edit, options }) => (
+  <div className={`edit ${options.extraClasses ?? ''}`}>
+    <EditTitle invitation={edit.invitations[0]} signatures={edit.signatures} />
 
-      <ul className="edit_meta_info list-inline">
-        <li>
-          {forumDate(
-            edit.cdate,
-            edit.tcdate,
-            edit.mdate,
-            edit.tmdate,
-            edit.content?.year?.value
-          )}
-        </li>
-      </ul>
+    <ul className="edit_meta_info list-inline">
+      <li>
+        {forumDate(
+          edit.cdate,
+          edit.tcdate,
+          edit.mdate,
+          edit.tmdate,
+          edit.content?.year?.value
+        )}
+      </li>
+    </ul>
 
-      {options.showContents && (!edit.ddate || edit.ddate > Date.now()) && (
-        <NoteContentV2
-          id={edit.id}
-          content={{
-            ...edit?.note?.content,
-            ...(edit?.note?.readers && { 'Note readers': { value: edit.note.readers } }),
-            ...(edit?.note?.writers && { 'Note writers': { value: edit.note.writers } }),
-            ...(edit?.note?.signatures && {
-              'Note signatures': { value: edit.note.signatures },
-            }),
-          }}
-          omit={options.omitFields}
-          isEdit={options.isReference}
-          presentation={edit.details?.presentation}
-          noteReaders={edit.readers?.sort()}
-        />
-      )}
-      <div className="edit_info">
-        <h4>Edit Info</h4>
-        <hr className="small" />
-        <EditValue name="Readers" value={edit.readers} />
-        <EditValue name="Writers" value={edit.writers} />
-        <EditValue name="Signatures" value={edit.signatures} />
-      </div>
+    {options.showContents && (!edit.ddate || edit.ddate > Date.now()) && (
+      <NoteContentV2
+        id={edit.id}
+        content={{
+          ...edit?.note?.content,
+          ...(edit?.note?.readers && { 'Note readers': { value: edit.note.readers } }),
+          ...(edit?.note?.writers && { 'Note writers': { value: edit.note.writers } }),
+          ...(edit?.note?.signatures && {
+            'Note signatures': { value: edit.note.signatures },
+          }),
+        }}
+        omit={options.omitFields}
+        isEdit={options.isReference}
+        presentation={edit.details?.presentation}
+        noteReaders={edit.readers?.sort()}
+      />
+    )}
+    <div className="edit_info">
+      <h4>Edit Info</h4>
+      <hr className="small" />
+      <EditValue name="Readers" value={edit.readers} />
+      <EditValue name="Writers" value={edit.writers} />
+      <EditValue name="Signatures" value={edit.signatures} />
     </div>
-  )
-}
+  </div>
+)
 
 export default Edit
