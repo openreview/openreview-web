2aa448f7d6eb5b9095cc7279fbcd0f816f3c1006
xukun <xukun@openreview.net>
2022-02-04 17:25:36 -0500

    fix lint

--- pages/revisions/index.js
+++ pages/revisions/index.js
@@ -168,7 +168,7 @@ const RevisionsList = ({
       },
       onCompleted: (editor) => {
         $('#note-editor-modal .modal-body').empty().addClass('legacy-styles').append(editor)
-        $('#note-editor-modal').on('hidden.bs.modal', function () {
+        $('#note-editor-modal').on('hidden.bs.modal', () => {
           $('#note-editor-modal').find('div.note_editor.panel').remove()
         })
       },
@@ -270,7 +270,7 @@ const RevisionsList = ({
       },
       onCompleted: (editor) => {
         $('#edit-edit-modal .modal-body').empty().addClass('legacy-styles').append(editor)
-        $('#edit-edit-modal').on('hidden.bs.modal', function () {
+        $('#edit-edit-modal').on('hidden.bs.modal', () => {
           $('#edit-edit-modal').find('div.note_editor.panel').remove()
         })
       },
