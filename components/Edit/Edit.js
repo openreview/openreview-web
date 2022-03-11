4f0d54cebc9284c4a30f32e30596477fee08b70c
xkopenreview <60613434+xkopenreview@users.noreply.github.com>
2022-03-11 15:41:07 -0500

    Feature/ Display all edit info on revisions page for v2 notes (#825)
    * poc wip
    
    * fix lint
    
    * update based on feedback
    
    * fix trash
    
    * fix lint
    
    * remove private icon
    
    * fix lint
    
    * some style changes based on feedback
    
    * remove reference from note title component
    
    * fix lint
    
    * CSS tweaks
    
    * show all fields, pretty list
    
    * some changes based on feedback
    
    * add missing contentorder
    
    * clean up
    
    * adjust position of action button
    
    * some changes based on feedback
    
    * update content order logic
    
    * update content order logic
    
    * remove unnecessary object value
    
    * remove sort as presentation is already sorted
    
    * manual code merge
    
    * manual code merge
    
    * add changes back
    
    * some changes based on feedback
    
    * fix lint
    
    * remove polyfill
    
    * add reader/writer/signature when edit has presentation
    
    Co-authored-by: Zach Bialecki <zach@openreview.net>
    Co-authored-by: Zach Bialecki <86174+zbialecki@users.noreply.github.com>
    Co-authored-by: Melisa Bok <melisabok@gmail.com>

---
+++ components/Edit/Edit.js
@@ -0,0 +1,35 @@
+import { buildNoteTitle, forumDate, prettyList } from '../../lib/utils'
+import EditContent from './EditContent'
+import EditValue from './EditValue'
+
+const Edit = ({ edit, options }) => (
+  <div className={`edit ${options.extraClasses ?? ''}`}>
+    <h4>{buildNoteTitle(edit.invitations[0], edit.signatures)}</h4>
+
+    <ul className="edit_meta_info list-inline">
+      <li>
+        {forumDate(
+          edit.cdate,
+          edit.tcdate,
+          edit.mdate,
+          edit.tmdate,
+          edit.content?.year?.value
+        )}
+      </li>
+    </ul>
+
+    {options.showContents && (!edit.ddate || edit.ddate > Date.now()) && (
+      <EditContent edit={edit} />
+    )}
+
+    <div className="edit_info">
+      <h4>Edit Info</h4>
+      <hr className="small" />
+      <EditValue name="Readers" value={prettyList(edit.readers, 'long', 'unit')} />
+      <EditValue name="Writers" value={prettyList(edit.writers, 'long', 'unit')} />
+      <EditValue name="Signatures" value={prettyList(edit.signatures, 'long', 'unit')} />
+    </div>
+  </div>
+)
+
+export default Edit
