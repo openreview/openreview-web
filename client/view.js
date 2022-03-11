a731cc7be6f3c93f9ce0051c1575aa45f3e8c903
celestemg <acmartinez@umass.edu>
2022-03-11 11:10:13 -0500

    change tooltip text when we have number and signature placeholders

--- client/view.js
+++ client/view.js
@@ -567,6 +567,11 @@ module.exports = (function() {
     var cssClass = title ? 'hover_title' : 'hover_item';
     var $hoverItem = $('<div>', { class: cssClass + (removable ? ' removable_item' : ''), translate: 'no' });
     if (_.isString(resultText) && resultText.length > 0) {
+      if (resultText.includes('${note.number}')) {
+        resultText = '"number" will be replaced by the paper number after the submission has been completed.';
+      } else if (resultText.includes('${signatures}')) {
+        resultText = 'This field will be automatically populated with the edit signature shown below.';
+      }
       var $hoverResult = $('<div>', {class: 'hover_result'}).text(resultText).hide();
       $hoverItem.append($hoverResult).hover(function() {
         $hoverResult.show();
