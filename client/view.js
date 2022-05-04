591ecf2f77a3e0f54d7fc158260f8f5e09e684cb
xukun <xukun@openreview.net>
2022-05-04 11:27:24 -0400

    update regex

--- client/view.js
+++ client/view.js
@@ -1760,7 +1760,7 @@ module.exports = (function() {
   var autolinkHtml = function(value) {
     // Regex based on https://gist.github.com/dperini/729294 modified to not accept FTP urls
     var urlRegex = /(?:(?:https?):\/\/)(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*[^.,()"'\s])?/ig;
-    var profileRegex = /^\s*(~[^\d\s]+\_[^\d\s]+[0-9]+)/ig;
+    var profileRegex = /(?:.)?(~[^\d\s]+\_[^\d\s]+[0-9]+)/ig;
 
     var intermediate = value.replace(urlRegex, function (match) {
       var url = match.startsWith('https://openreview.net') ? match.replace('https://openreview.net', '') : match
@@ -1768,6 +1768,7 @@ module.exports = (function() {
     });
 
     return intermediate.replace(profileRegex, function(fullMatch, match) {
+      if (fullMatch.startsWith("=")) return match;
       return ' <a href="/profile?id=' + match + '" target="_blank">' + prettyId(match) + '</a>';
     });
   };
