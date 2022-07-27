422dd24e38685b963b3a5969d3fd680adc42d464
xukun <xukun@openreview.net>
2022-07-27 16:28:32 -0400

    pretty updated files

--- tests/profilePage.ts
+++ tests/profilePage.ts
@@ -114,8 +114,15 @@ test('user open own profile', async (t) => {
     .click(Selector('button').withText('Remove').filterVisible())
 
   const { superUserToken } = t.fixtureCtx
-  const messages = await getMessages({ to: 'a@aa.com', subject: 'OpenReview Account Linking' }, superUserToken)
-  await t.expect(messages[0].content.text).contains('Click on the link below to confirm that a@aa.com and a@a.com both belong to the same person')
+  const messages = await getMessages(
+    { to: 'a@aa.com', subject: 'OpenReview Account Linking' },
+    superUserToken
+  )
+  await t
+    .expect(messages[0].content.text)
+    .contains(
+      'Click on the link below to confirm that a@aa.com and a@a.com both belong to the same person'
+    )
     // personal links
     .expect(addDBLPPaperToProfileButton.hasAttribute('disabled'))
     .ok()
