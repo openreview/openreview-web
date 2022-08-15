53a0f9e407bb92c2d1432a4169369b894563fded
Celeste Martinez <celeste@openreview.net>
2022-08-15 18:05:37 -0400

    fix values-dropdown condition

--- client/view-v2.js
+++ client/view-v2.js
@@ -279,7 +279,7 @@ module.exports = (function() {
         });
         contentInputResult = valueInput('<div class="note_content_value no-wrap">' + checkboxes.join('\n') + '</div>', fieldName, fieldDescription);
       } else if (fieldDescription.value.param.input === 'select' || !(_.has(fieldDescription.value.param, 'input'))) {
-        if (Array.isArray(fieldDescription.value.param.enum) || fieldDescription.value.param.type.endsWith('[]')) {
+        if (fieldDescription.value.param.type.endsWith('[]')) {
           //values-dropdown
           contentInputResult = view.mkDropdownAdder(
             fieldName, fieldDescription.description, fieldDescription.value.param.enum,
