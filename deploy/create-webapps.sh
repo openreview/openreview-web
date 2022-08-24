b3eb7401c0620decf85e359bd677e518fa28a294
carlosmondra <carlos@openreview.net>
2022-08-24 12:24:59 -0500

    Feature/GitHub actions deploy script (#1072)
    * Feat: Remove old legacy deploy scripts
    
    * Feat: Add deploy scripts for openreview-web that use github actions
    
    Co-authored-by: Zach Bialecki <86174+zbialecki@users.noreply.github.com>

--- deploy/create-webapps.sh
+++
@@ -1,9 +0,0 @@
-gcloud compute instances create "$@" \
-      --zone us-central1-b \
-      --machine-type n2-standard-4 \
-      --boot-disk-size 20GB \
-      --boot-disk-type pd-ssd \
-      --image-family ubuntu-2004-lts \
-      --image-project ubuntu-os-cloud \
-      --metadata-from-file startup-script=deploy/setup-openreview-web.sh
-
