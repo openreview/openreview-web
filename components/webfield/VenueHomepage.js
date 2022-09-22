f06c9bb5b01643b9a34e9b14ab1a08bee675e31e
Zach Bialecki <zach@openreview.net>
2022-09-22 16:54:39 -0400

    Reload consoles tab after sucessfull submission

--- components/webfield/VenueHomepage.js
+++ components/webfield/VenueHomepage.js
@@ -147,6 +147,7 @@ export default function VenueHomepage({ appContext }) {
   const { user, accessToken, userLoading } = useUser()
   const [userConsoles, setUserConsoles] = useState([])
   const [activityNotes, setActivityNotes] = useState([])
+  const [reloadConsoles, setReloadConsoles] = useState(false)
   const router = useRouter()
   const { setBannerContent } = appContext
 
@@ -206,7 +207,7 @@ export default function VenueHomepage({ appContext }) {
       .catch((error) => {
         promptError(error.message)
       })
-  }, [user, accessToken, userLoading])
+  }, [user, accessToken, userLoading, reloadConsoles])
 
   if (userLoading) return null
 
@@ -222,6 +223,7 @@ export default function VenueHomepage({ appContext }) {
             onNoteCreated={() => {
               const defaultConfirmationMessage = 'Your submission is complete. Check your inbox for a confirmation email. The author console page for managing your submissions will be available soon.'
               promptMessage(submissionConfirmationMessage || defaultConfirmationMessage)
+              setReloadConsoles(!reloadConsoles)
             }}
             options={{ largeLabel: true }}
           />
