932467a84a19eeb6f44c03e2604d090908d690bc
Nadia L'Bahy <nadia@openreview.net>
2022-10-06 13:29:37 -0400

    adds a check for {tail}

--- client/webfield.js
+++ client/webfield.js
@@ -1053,7 +1053,10 @@ module.exports = (function() {
           return _.compact(_.map(tagInvitation.reply[fieldName]['values-copied'], function(value) {
             if (value === '{signatures}') {
               return window.user.profile.id;
-            } else if (value[0] === '{') {
+            } if (value === '{tail}') {
+              return window.user.profile.id;
+            }
+             else if (value[0] === '{') {
               return null;
             } else {
               return value;
