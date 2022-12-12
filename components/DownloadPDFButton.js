60ef0568f3da52b2b5a5168930e9639ef90fa12b
xkopenreview <60613434+xkopenreview@users.noreply.github.com>
2022-12-12 12:48:34 -0500

    Feature/ AC console download pdf as zip (#1255)
    * add download pdf button to ac console
    
    * fix lint
    
    * some changes based on feedback; filter out notes without pdf
    
    Co-authored-by: Zach Bialecki <86174+zbialecki@users.noreply.github.com>

---
+++ components/DownloadPDFButton.js
@@ -0,0 +1,54 @@
+/* globals promptError: false */
+
+import { useContext, useState } from 'react'
+import useUser from '../hooks/useUser'
+import api from '../lib/api-client'
+import SpinnerButton from './SpinnerButton'
+import WebFieldContext from './WebFieldContext'
+
+const DownloadPDFButton = ({ records, fileName }) => {
+  const [isLoading, setIsLoading] = useState(false)
+  const { apiVersion } = useContext(WebFieldContext)
+  const { accessToken } = useUser()
+
+  const hasPdf = (record) =>
+    apiVersion === 2 ? record.note?.content?.pdf?.value : record.note?.content?.pdf
+
+  const handleDownloadPDFClick = async () => {
+    setIsLoading(true)
+    try {
+      let ids = records.flatMap((p) => (hasPdf(p) ? p.note.id : []))
+
+      if (ids.length > 50) {
+        ids = ids.slice(0, 50)
+        promptError('Max 50 PDFs allowed.')
+      }
+      const zipBlob = await api.get(
+        '/attachment',
+        { [ids.length === 1 ? 'id' : 'ids']: ids, name: 'pdf' },
+        { accessToken, contentType: 'blob' }
+      )
+      const url = window.URL || window.webkitURL
+      const link = document.createElement('a')
+      link.href = url.createObjectURL(zipBlob)
+      link.download = fileName
+      link.click()
+    } catch (error) {
+      promptError(error.message)
+    }
+    setIsLoading(false)
+  }
+
+  return (
+    <SpinnerButton
+      className="btn btn-export-data"
+      disabled={!records?.some(hasPdf)}
+      loading={isLoading}
+      onClick={handleDownloadPDFClick}
+    >
+      Download PDFs
+    </SpinnerButton>
+  )
+}
+
+export default DownloadPDFButton
