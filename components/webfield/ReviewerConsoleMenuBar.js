0a503ddcd9140b3213d45ab2881b448c4e95321b
xukun <xukun@openreview.net>
2023-01-13 11:57:22 -0500

    fix lint

--- components/webfield/ReviewerConsoleMenuBar.js
+++ components/webfield/ReviewerConsoleMenuBar.js
@@ -1,16 +1,14 @@
 import DownloadPDFButton from '../DownloadPDFButton'
 
-const ReviewerConsoleMenuBar = ({ venueId, records }) => {
-  return (
-    <div className="menu-bar">
-      <DownloadPDFButton
-        records={records?.map((p) => ({ note: p }))}
-        fileName={`${venueId}_pdfs.zip`}
-        text="Download all PDFs"
-      />
-      <div className="space-taker" />
-    </div>
-  )
-}
+const ReviewerConsoleMenuBar = ({ venueId, records }) => (
+  <div className="menu-bar">
+    <DownloadPDFButton
+      records={records?.map((p) => ({ note: p }))}
+      fileName={`${venueId}_pdfs.zip`}
+      text="Download all PDFs"
+    />
+    <div className="space-taker" />
+  </div>
+)
 
 export default ReviewerConsoleMenuBar
