8ac51552271fedf2d84a66e010dd2076fdbaa0de
xukun <xukun@openreview.net>
2023-01-18 13:02:47 -0500

    add some test

--- unitTests/RadioButtonWidget.test.js
+++ unitTests/RadioButtonWidget.test.js
@@ -2,6 +2,7 @@ import RadioButtonWidget from '../components/EditorComponents/RadioButtonWidget'
 import { renderWithEditorComponentContext } from './util'
 import { screen } from '@testing-library/react'
 import { prettyDOM } from '@testing-library/dom'
+import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom'
 
 let baseProviderProps
@@ -54,6 +55,42 @@ describe('RadioButtonWidget', () => {
       },
     }
     renderWithEditorComponentContext(<RadioButtonWidget />, providerProps)
-    console.log(prettyDOM())
+    expect(screen.getByDisplayValue('option 1'))
+    expect(screen.getByDisplayValue('OPTION TWO'))
+  })
+
+  test('display option as selected when match with existing value', () => {
+    const providerProps = baseProviderProps
+    providerProps.value.value = 'OPTION TWO'
+    providerProps.value.field.radiobutton_widget.value = {
+      param: {
+        enum: ['option 1', 'OPTION TWO'],
+      },
+    }
+    renderWithEditorComponentContext(<RadioButtonWidget />, providerProps)
+    expect(screen.getByDisplayValue('option 1')).not.toHaveAttribute('checked')
+    expect(screen.getByDisplayValue('OPTION TWO')).toHaveAttribute('checked')
+  })
+
+  test('call update when option is selected', async () => {
+    const providerProps = baseProviderProps
+    providerProps.value.field.radiobutton_widget.value = {
+      param: {
+        enum: ['option 1', 'OPTION TWO'],
+      },
+    }
+    const onChange = jest.fn()
+    providerProps.value.onChange = onChange
+
+    renderWithEditorComponentContext(<RadioButtonWidget />, providerProps)
+
+    const optionTwo = screen.getByDisplayValue('OPTION TWO')
+    const optionOne = screen.getByDisplayValue('option 1')
+
+    await userEvent.click(optionTwo)
+    expect(onChange).toBeCalledWith(expect.objectContaining({ value: 'OPTION TWO' }))
+
+    await userEvent.click(optionOne)
+    expect(onChange).toHaveBeenNthCalledWith(2, expect.objectContaining({ value: 'option 1' }))
   })
 })
