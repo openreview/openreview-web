6badbbd7bfdb9ef0ace2550a6895c41ec0464320
xkopenreview <60613434+xkopenreview@users.noreply.github.com>
2023-02-28 14:47:49 -0500

    Fix/ Bidding console: add subject area dropdown (#1357)
    * add subject areas dropdown
    
    * drop bid console v1 support
    
    * some changes based on feedback
    
    * fix misplaced submissions-list class
    
    * hide bidscore when user filter by searchterm or subject area
    
    ---------
    
    Co-authored-by: Zach Bialecki <86174+zbialecki@users.noreply.github.com>

--- components/NoteListWithBidWidget.js
+++ components/NoteListWithBidWidget.js
@@ -1,7 +1,8 @@
 import List from 'rc-virtual-list'
-import Note, { NoteV2 } from './Note'
+import { NoteV2 } from './Note'
 import { BidRadioButtonGroup, BidScore } from './webfield/BidWidget'
 
+// only work with v2 api
 const NoteListWithBidWidget = ({
   notes,
   bidOptions,
@@ -10,16 +11,12 @@ const NoteListWithBidWidget = ({
   displayOptions,
   updateBidOption,
   virtualList,
-  apiVersion,
   bidUpdateStatus,
+  showBidScore = true,
 }) => {
   const renderNoteWithBidWidget = (note, selectedBidOption, scoreEdge) => (
-    <>
-      {apiVersion === 2 ? ( // bid has no mixed notes from v1 and v2
-        <NoteV2 note={note} options={displayOptions} />
-      ) : (
-        <Note note={note} options={displayOptions} />
-      )}
+    <div className="bid-container">
+      <NoteV2 note={note} options={displayOptions} />
       <BidRadioButtonGroup
         label="Bid"
         options={bidOptions}
@@ -27,51 +24,57 @@ const NoteListWithBidWidget = ({
         updateBidOption={(updatedOption) => updateBidOption(note, updatedOption)}
         bidUpdateStatus={bidUpdateStatus}
       />
-      <BidScore scoreEdge={scoreEdge} />
-    </>
+      {showBidScore && <BidScore scoreEdge={scoreEdge} />}
+    </div>
   )
 
   if (virtualList) {
     return (
-      <ul className="list-unstyled submissions-list">
-        <List
-          data={notes}
-          height={notes.length === 0 ? 0 : window.innerHeight}
-          itemHeight={135}
-          itemKey="id"
-        >
-          {(note) => {
-            const selectedBidOption = bidEdges?.find((p) => p.head === note.id)?.label
-            const scoreEdge = scoreEdges?.find((p) => p.head === note.id)
-            return renderNoteWithBidWidget(note, selectedBidOption, scoreEdge)
-          }}
-        </List>
+      <div className="submissions-list">
+        <ul className="list-unstyled">
+          <List
+            data={notes}
+            height={notes.length === 0 ? 0 : window.innerHeight}
+            itemHeight={135}
+            itemKey="id"
+          >
+            {(note) => {
+              const selectedBidOption = bidEdges?.find((p) => p.head === note.id)?.label
+              const scoreEdge = scoreEdges?.find((p) => p.head === note.id)
+              return renderNoteWithBidWidget(note, selectedBidOption, scoreEdge)
+            }}
+          </List>
+          {notes.length === 0 && (
+            <li>
+              <p className="empty-message">{displayOptions.emptyMessage}</p>
+            </li>
+          )}
+        </ul>
+      </div>
+    )
+  }
+
+  return (
+    <div className="submissions-list">
+      <ul className="list-unstyled">
+        {notes.map((note) => {
+          const selectedBidOption = bidEdges?.find((p) => p.head === note.id)?.label
+          const scoreEdge = scoreEdges?.find((p) => p.head === note.id)
+
+          return (
+            <li key={note.id}>
+              {renderNoteWithBidWidget(note, selectedBidOption, scoreEdge)}
+            </li>
+          )
+        })}
+
         {notes.length === 0 && (
           <li>
             <p className="empty-message">{displayOptions.emptyMessage}</p>
           </li>
         )}
       </ul>
-    )
-  }
-
-  return (
-    <ul className="list-unstyled submissions-list">
-      {notes.map((note) => {
-        const selectedBidOption = bidEdges?.find((p) => p.head === note.id)?.label
-        const scoreEdge = scoreEdges?.find((p) => p.head === note.id)
-
-        return (
-          <li key={note.id}>{renderNoteWithBidWidget(note, selectedBidOption, scoreEdge)}</li>
-        )
-      })}
-
-      {notes.length === 0 && (
-        <li>
-          <p className="empty-message">{displayOptions.emptyMessage}</p>
-        </li>
-      )}
-    </ul>
+    </div>
   )
 }
 
