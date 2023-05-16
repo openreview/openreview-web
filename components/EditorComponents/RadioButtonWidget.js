b47e84b1d9254a255a6e9def8c6b9a589ec693b1
xukun <xukun@openreview.net>
2023-05-16 12:04:02 -0400

    support object enum in radiobutton

--- components/EditorComponents/RadioButtonWidget.js
+++ components/EditorComponents/RadioButtonWidget.js
@@ -1,29 +1,49 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import EditorComponentContext from '../EditorComponentContext'
 
 import styles from '../../styles/components/RadioButtonWidget.module.scss'
+import { convertToType } from '../../lib/webfield-utils'
 
 const RadioButtonWidget = () => {
   const { field, onChange, value, isWebfield, clearError } = useContext(EditorComponentContext)
   const fieldName = Object.keys(field)[0]
-  const options = field[fieldName].value?.param?.enum
+  const dataType = field[fieldName]?.value?.param?.type
+  const [radioButtonOptions, setRadioButtonOptions] = useState([])
 
-  if (!Array.isArray(options)) return null
+  useEffect(() => {
+    const enumValues = field[fieldName].value?.param?.enum
+    if (Array.isArray(enumValues) && enumValues.length) {
+      setRadioButtonOptions(
+        enumValues.map((p) =>
+          typeof p === 'object'
+            ? { value: p.value, description: p.description }
+            : { value: p, description: p }
+        )
+      )
+      const defaultValue = field[fieldName].value?.param?.default
+      if (!value && defaultValue) onChange({ fieldName, value: defaultValue })
+    }
+  }, [])
+
+  if (!radioButtonOptions.length) return null
 
   return (
     <div className={styles.radioButtonContainer}>
-      {options.map((option) => (
-        <div className={styles.radioButtonOptionContainer} key={`${fieldName}-${option}`}>
+      {radioButtonOptions.map((option) => (
+        <div
+          className={styles.radioButtonOptionContainer}
+          key={`${fieldName}-${option.value}`}
+        >
           <input
             type="radio"
-            value={option}
-            checked={value === option}
+            value={option.value}
+            checked={value == option.value} // eslint-disable-line eqeqeq
             onChange={(e) => {
-              onChange({ fieldName, value: e.target.value })
+              onChange({ fieldName, value: convertToType(e.target.value, dataType) })
               clearError?.()
             }}
           />
-          <span className={styles.radioButtonOptionText}>{option}</span>
+          <span className={styles.radioButtonOptionText}>{option.description}</span>
         </div>
       ))}
     </div>
--- unitTests/RadioButtonWidget.test.js
+++ unitTests/RadioButtonWidget.test.js
@@ -45,7 +45,7 @@ describe('RadioButtonWidget', () => {
     expect(screen.queryByText('* Radiobutton Widget')).not.toBeInTheDocument()
   })
 
-  test('display enum options', () => {
+  test('display enum options (string)', () => {
     const providerProps = {
       value: {
         invitation: { id: 'invitaitonId' },
@@ -73,7 +73,45 @@ describe('RadioButtonWidget', () => {
     ).toHaveAttribute('type', 'radio')
   })
 
-  test('display option as selected when match with existing value', () => {
+  test('display description of enum options (object)', () => {
+    const providerProps = {
+      value: {
+        invitation: { id: 'invitaitonId' },
+        field: {
+          ['submission_length']: {
+            value: {
+              param: {
+                input: 'radio',
+                enum: [
+                  {
+                    value: 0,
+                    description: 'Regular submission (no more than 12 pages of main content)',
+                  },
+                  {
+                    value: 1,
+                    description: 'Long submission (more than 12 pages of main content)',
+                  },
+                ],
+              },
+            },
+          },
+        },
+      },
+    }
+    renderWithEditorComponentContext(<RadioButtonWidget />, providerProps)
+    expect(screen.getByDisplayValue(0)).toHaveAttribute('type', 'radio')
+    expect(
+      screen.getByText('Regular submission (no more than 12 pages of main content)')
+        .parentElement.firstChild
+    ).toHaveAttribute('value', '0')
+    expect(screen.getByDisplayValue(1)).toHaveAttribute('type', 'radio')
+    expect(
+      screen.getByText('Long submission (more than 12 pages of main content)').parentElement
+        .firstChild
+    ).toHaveAttribute('value', '1')
+  })
+
+  test('display option as selected when match with existing value (enum string)', () => {
     const providerProps = {
       value: {
         invitation: { id: 'invitaitonId' },
@@ -102,7 +140,38 @@ describe('RadioButtonWidget', () => {
     ).toHaveAttribute('checked')
   })
 
-  test('call update when option is selected', async () => {
+  test('display option as selected when match with existing value (enum object)', () => {
+    const providerProps = {
+      value: {
+        invitation: { id: 'invitaitonId' },
+        field: {
+          ['submission_length']: {
+            value: {
+              param: {
+                input: 'radio',
+                enum: [
+                  {
+                    value: 0,
+                    description: 'Regular submission (no more than 12 pages of main content)',
+                  },
+                  {
+                    value: 1,
+                    description: 'Long submission (more than 12 pages of main content)',
+                  },
+                ],
+              },
+            },
+          },
+        },
+        value: 1,
+      },
+    }
+    renderWithEditorComponentContext(<RadioButtonWidget />, providerProps)
+    expect(screen.getByDisplayValue(0)).not.toHaveAttribute('checked')
+    expect(screen.getByDisplayValue(1)).toHaveAttribute('checked')
+  })
+
+  test('call update when option is selected (enum string)', async () => {
     const onChange = jest.fn()
     const clearError = jest.fn()
     const providerProps = {
@@ -151,5 +220,275 @@ describe('RadioButtonWidget', () => {
       })
     )
     expect(clearError).toBeCalledTimes(2)
+  })
+
+  test('call update when a new option is selected (enum string)', async () => {
+    const onChange = jest.fn()
+    const clearError = jest.fn()
+    const providerProps = {
+      value: {
+        invitation: { id: 'invitaitonId' },
+        field: {
+          ['submission_length']: {
+            value: {
+              param: {
+                input: 'radio',
+                enum: [
+                  'Regular submission (no more than 12 pages of main content)',
+                  'Long submission (more than 12 pages of main content)',
+                ],
+              },
+            },
+          },
+        },
+        value: 'Long submission (more than 12 pages of main content)',
+        onChange,
+        clearError,
+      },
+    }
+
+    renderWithEditorComponentContext(<RadioButtonWidget />, providerProps)
+
+    await userEvent.click(
+      screen.getByDisplayValue('Regular submission (no more than 12 pages of main content)')
+    )
+    expect(onChange).toBeCalledWith(
+      expect.objectContaining({
+        value: 'Regular submission (no more than 12 pages of main content)',
+      })
+    )
+    expect(clearError).toBeCalled()
+  })
+
+  test('call update when option is selected (enum object)', async () => {
+    const onChange = jest.fn()
+    const clearError = jest.fn()
+    const providerProps = {
+      value: {
+        invitation: { id: 'invitaitonId' },
+        field: {
+          ['submission_length']: {
+            value: {
+              param: {
+                input: 'radio',
+                type: 'integer',
+                enum: [
+                  {
+                    value: 0,
+                    description: 'Regular submission (no more than 12 pages of main content)',
+                  },
+                  {
+                    value: 1,
+                    description: 'Long submission (more than 12 pages of main content)',
+                  },
+                ],
+              },
+            },
+          },
+        },
+        onChange,
+        clearError,
+      },
+    }
+
+    renderWithEditorComponentContext(<RadioButtonWidget />, providerProps)
+
+    const optionTwo = screen.getByDisplayValue('1')
+    const optionOne = screen.getByDisplayValue('0')
+
+    await userEvent.click(optionTwo)
+    expect(onChange).toBeCalledWith(
+      expect.objectContaining({
+        value: 1,
+      })
+    )
+    expect(clearError).toBeCalledTimes(1)
+
+    await userEvent.click(optionOne)
+    expect(onChange).toHaveBeenNthCalledWith(
+      2,
+      expect.objectContaining({
+        value: 0,
+      })
+    )
+    expect(clearError).toBeCalledTimes(2)
+  })
+
+  test('call update when a new option is selected (enum object)', async () => {
+    const onChange = jest.fn()
+    const clearError = jest.fn()
+    const providerProps = {
+      value: {
+        invitation: { id: 'invitaitonId' },
+        field: {
+          ['submission_length']: {
+            value: {
+              param: {
+                input: 'radio',
+                type: 'string',
+                enum: [
+                  {
+                    value: '0',
+                    description: 'Regular submission (no more than 12 pages of main content)',
+                  },
+                  {
+                    vale: '1',
+                    description: 'Long submission (more than 12 pages of main content)',
+                  },
+                ],
+              },
+            },
+          },
+        },
+        value: '1',
+        onChange,
+        clearError,
+      },
+    }
+
+    renderWithEditorComponentContext(<RadioButtonWidget />, providerProps)
+
+    await userEvent.click(screen.getByDisplayValue('0'))
+    expect(onChange).toBeCalledWith(
+      expect.objectContaining({
+        value: '0',
+      })
+    )
+    expect(clearError).toBeCalled()
+  })
+
+  test('display default value as selected if there is no value (enum string)', () => {
+    const onChange = jest.fn()
+    const providerProps = {
+      value: {
+        invitation: { id: 'invitaitonId' },
+        field: {
+          ['submission_length']: {
+            value: {
+              param: {
+                input: 'radio',
+                enum: [
+                  'Regular submission (no more than 12 pages of main content)',
+                  'Long submission (more than 12 pages of main content)',
+                ],
+                default: 'Long submission (more than 12 pages of main content)',
+              },
+            },
+          },
+        },
+        onChange,
+        value: undefined,
+      },
+    }
+
+    renderWithEditorComponentContext(<RadioButtonWidget />, providerProps)
+
+    expect(onChange).toBeCalledWith(
+      expect.objectContaining({
+        value: 'Long submission (more than 12 pages of main content)',
+      })
+    ) // onChange will cause the option to be shown as selected
+  })
+
+  test('not to display default value as selected if there is value (enum string)', () => {
+    const onChange = jest.fn()
+    const providerProps = {
+      value: {
+        invitation: { id: 'invitaitonId' },
+        field: {
+          ['submission_length']: {
+            value: {
+              param: {
+                input: 'radio',
+                enum: [
+                  'Regular submission (no more than 12 pages of main content)',
+                  'Long submission (more than 12 pages of main content)',
+                ],
+                default: 'Long submission (more than 12 pages of main content)',
+              },
+            },
+          },
+        },
+        onChange,
+        value: 'Regular submission (no more than 12 pages of main content)',
+      },
+    }
+
+    renderWithEditorComponentContext(<RadioButtonWidget />, providerProps)
+
+    expect(
+      screen.getByDisplayValue('Regular submission (no more than 12 pages of main content)')
+    ).toHaveAttribute('checked')
+    expect(onChange).not.toBeCalled()
+  })
+
+  test('display default value as selected if there is no value (enum object)', () => {
+    const onChange = jest.fn()
+    const providerProps = {
+      value: {
+        invitation: { id: 'invitaitonId' },
+        field: {
+          ['submission_length']: {
+            value: {
+              param: {
+                input: 'radio',
+                type: 'integer',
+                enum: [
+                  {
+                    value: 0,
+                    description: 'Regular submission (no more than 12 pages of main content)',
+                  },
+                  {
+                    value: 1,
+                    description: 'Long submission (more than 12 pages of main content)',
+                  },
+                ],
+                default: 1,
+              },
+            },
+          },
+        },
+        onChange,
+        value: undefined,
+      },
+    }
+
+    renderWithEditorComponentContext(<RadioButtonWidget />, providerProps)
+    expect(onChange).toBeCalledWith(expect.objectContaining({ value: 1 }))
+  })
+
+  test('not to display default value as selected if there is value (enum object)', () => {
+    const onChange = jest.fn()
+    const providerProps = {
+      value: {
+        invitation: { id: 'invitaitonId' },
+        field: {
+          ['submission_length']: {
+            value: {
+              param: {
+                input: 'radio',
+                type: 'integer',
+                enum: [
+                  {
+                    value: 0,
+                    description: 'Regular submission (no more than 12 pages of main content)',
+                  },
+                  {
+                    value: 1,
+                    description: 'Long submission (more than 12 pages of main content)',
+                  },
+                ],
+                default: 1,
+              },
+            },
+          },
+        },
+        onChange,
+        value: 1,
+      },
+    }
+
+    renderWithEditorComponentContext(<RadioButtonWidget />, providerProps)
+    expect(onChange).not.toBeCalled()
   })
 })
