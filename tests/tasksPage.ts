671217283a4f0feb0787cecb66a014e6810772e8
Zach Bialecki <zach@openreview.net>
2023-07-27 10:47:20 -0400

    Remove unecessary signature selection from test

--- tests/tasksPage.ts
+++ tests/tasksPage.ts
@@ -38,8 +38,6 @@ test('should open tasks page and complete pending task', async (t) => {
           '4: The reviewer is confident but not absolutely certain that the evaluation is correct'
         )
     ) // fill in comment content
-    .click(Selector('input[placeholder="signatures"]')) // select signature
-    .click(Selector('.dropdown_content div').withExactText('FirstA LastA'))
     .click(Selector('button').withText('Submit')) // submit
 
     // should see 0 pending task and 1 completed
