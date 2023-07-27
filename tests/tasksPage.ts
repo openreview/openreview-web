cd4dbe2890734d56654ed8cb014cd6949523dc7d
Zach Bialecki <zach@openreview.net>
2023-07-27 11:28:53 -0400

    Fix other test

--- tests/tasksPage.ts
+++ tests/tasksPage.ts
@@ -54,7 +54,6 @@ test('task should change when note is deleted and restored', async (t) => {
     .click(Selector('a').withText('Paper1 Official Review')) // go to forum page
     .wait(2000)
     .click(Selector('#note_children').find('button.trash_button'))
-    .click(Selector('button').withText('Delete'))
     .click(Selector('a').withText('Tasks'))
     .wait(2000)
     .expect(Selector('span.task-count-message').innerText)
