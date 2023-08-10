01a3b57ac3b3e68646db924eb34ecc15248848cd
xkopenreview <60613434+xkopenreview@users.noreply.github.com>
2023-08-11 06:14:36 +0800

    Fix/ AC Console: allow extra export columns in CSV export (#1575)

--- components/webfield/AreaChairConsole.js
+++ components/webfield/AreaChairConsole.js
@@ -134,6 +134,7 @@ const AreaChairConsole = ({ appContext }) => {
     propertiesAllowed,
     enableQuerySearch,
     emailReplyTo,
+    extraExportColumns,
   } = useContext(WebFieldContext)
   const {
     showEdgeBrowserUrl,
@@ -337,6 +338,7 @@ const AreaChairConsole = ({ appContext }) => {
             const anonymousId = getIndentifierFromGroup(q.signatures[0], anonReviewerName)
             const reviewValue = q.content.review?.value
             return {
+              ...q,
               anonymousId,
               confidence: parseNumberField(q.content[reviewConfidenceName]?.value),
               ...Object.fromEntries(
@@ -348,7 +350,6 @@ const AreaChairConsole = ({ appContext }) => {
                 )
               ),
               reviewLength: reviewValue?.length,
-              id: q.id,
             }
           })
 
@@ -471,6 +472,7 @@ const AreaChairConsole = ({ appContext }) => {
             setAcConsoleData={setAcConsoleData}
             shortPhrase={shortPhrase}
             enableQuerySearch={enableQuerySearch}
+            extraExportColumns={extraExportColumns}
             filterOperators={filterOperators}
             propertiesAllowed={propertiesAllowed}
             reviewRatingName={reviewRatingName}
@@ -487,6 +489,7 @@ const AreaChairConsole = ({ appContext }) => {
           setAcConsoleData={setAcConsoleData}
           shortPhrase={shortPhrase}
           enableQuerySearch={enableQuerySearch}
+          extraExportColumns={extraExportColumns}
           filterOperators={filterOperators}
           propertiesAllowed={propertiesAllowed}
           reviewRatingName={reviewRatingName}
