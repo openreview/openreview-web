cc6b1c453bcdee4c9866116be2c9e2418d554239
Melisa Bok <melisabok@gmail.com>
2023-09-26 14:23:33 -0400

    filter by domain when it doesn't start with super user

--- components/forum/Forum.js
+++ components/forum/Forum.js
@@ -74,6 +74,7 @@ export default function Forum({
   const { id, details } = parentNote
   const repliesLoaded = replyNoteMap && displayOptionsMap && orderedReplies
   const newNoteEditor = useNewNoteEditor(details.invitation.domain)
+  const domain = details.invitation.domain.startsWith(process.env.SUPER_USER) ? undefined : details.invitation.domain
 
   // Process forum views config
   let replyForumViews = null
@@ -105,7 +106,7 @@ export default function Forum({
     return api
       .get(
         '/invitations',
-        { replyForum: forumId, expired: true, domain: forumNote.domain, ...extraParams },
+        { replyForum: forumId, expired: true, domain: domain, ...extraParams },
         { accessToken, version: 2 }
       )
       .then(({ invitations }) => {
@@ -135,7 +136,7 @@ export default function Forum({
         forum: forumId,
         trash: true,
         details: 'replyCount,writable,signatures,invitation,presentation',
-        domain: forumNote.domain
+        domain: domain
       },
       { accessToken, version: 2 }
     )
@@ -245,7 +246,7 @@ export default function Forum({
           sort: 'tmdate:asc',
           details: 'writable',
           trash: true,
-          domain: forumNote.domain
+          domain: domain
         },
         { accessToken, version: 2 }
       )
