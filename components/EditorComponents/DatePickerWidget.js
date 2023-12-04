1aeb50a3fdc632dc425ef3a304d5fb0836126df5
xukun <xukun@openreview.net>
2023-12-04 16:42:28 -0500

    some changes based on feedback

--- components/EditorComponents/DatePickerWidget.js
+++ components/EditorComponents/DatePickerWidget.js
@@ -7,25 +7,10 @@ import { getDefaultTimezone, prettyField } from '../../lib/utils'
 
 import styles from '../../styles/components/DatePickerWidget.module.scss'
 
-const DatePickerWidget = ({
-  isEditor = true,
-  showTime,
-  onChange: propsOnChange,
-  field: propsField,
-  error: propsError,
-  clearError: propsClearError,
-  value: propsValue,
-}) => {
+const DatePickerWidget = (props) => {
   const editorComponentContext = useContext(EditorComponentContext) ?? {}
-  const { field, onChange, value, error, clearError } = isEditor
-    ? editorComponentContext
-    : {
-        field: propsField,
-        onChange: propsOnChange,
-        clearError: propsClearError,
-        value: propsValue,
-        error: propsError,
-      }
+  const { field, onChange, value, error, clearError } =
+    props.isEditor === false ? props : editorComponentContext
   const fieldName = Object.keys(field ?? {})[0]
   const [timeZone, setTimeZone] = useState(getDefaultTimezone().value)
 
@@ -48,9 +33,9 @@ const DatePickerWidget = ({
         onChange={onDateTimeChange}
         placeholder={fieldName ? `Select ${prettyField(fieldName)}` : 'Select datetime'}
         autoFocus={false}
-        showTime={showTime}
+        showTime={props.showTime}
       />
-      {showTime !== false && (
+      {props.showTime !== false && (
         <div className={styles.timeZonePicker}>
           <TimezoneDropdown
             className={styles.timeZoneDropdown}
