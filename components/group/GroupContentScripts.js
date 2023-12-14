f772fd10172de373aa0459661caf8f7703ef0589
Zach Bialecki <zach@openreview.net>
2023-12-14 16:00:30 -0500

    Show group content scripts in a separate section

---
+++ components/group/GroupContentScripts.js
@@ -0,0 +1,102 @@
+/* globals promptMessage: false */
+/* globals promptError: false */
+
+import { useState } from 'react'
+import { Tab, TabList, TabPanel, TabPanels, Tabs } from '../Tabs'
+import EditorSection from '../EditorSection'
+import CodeEditor from '../CodeEditor'
+import SpinnerButton from '../SpinnerButton'
+import api from '../../lib/api-client'
+import { prettyField } from '../../lib/utils'
+
+export default function GroupContentScripts({
+  group,
+  profileId,
+  accessToken,
+  reloadGroup,
+}) {
+  const contentScripts = Object.keys(group.content ?? {}).filter(
+    (key) => key.endsWith('_script') && typeof group.content[key].value === 'string'
+  )
+  if (contentScripts.length === 0) {
+    return null
+  }
+
+  return (
+    <EditorSection title="Content Scripts" className="process-functions">
+      <Tabs>
+        <TabList>
+          {contentScripts.map((fieldName, i) => (
+            <Tab key={fieldName} id={fieldName} active={i === 0}>
+              {prettyField(fieldName)}
+            </Tab>
+          ))}
+        </TabList>
+
+        <TabPanels>
+          {contentScripts.map((fieldName) => (
+            <TabPanel key={fieldName} id={fieldName}>
+              <GroupCodeEditor
+                group={group}
+                fieldName={fieldName}
+                profileId={profileId}
+                accessToken={accessToken}
+                reloadGroup={reloadGroup}
+              />
+            </TabPanel>
+          ))}
+        </TabPanels>
+      </Tabs>
+    </EditorSection>
+  )
+}
+
+function GroupCodeEditor({ group, fieldName, profileId, accessToken, reloadGroup }) {
+  const [code, setCode] = useState(group.content[fieldName].value || '')
+  const [isSaving, setIsSaving] = useState(false)
+
+  const saveCode = async () => {
+    setIsSaving(true)
+    try {
+      const requestBody = {
+        group: {
+          id: group.id,
+          content: {
+            ...group.content,
+            [fieldName]: { value: code }
+          },
+        },
+        readers: [profileId],
+        writers: [profileId],
+        signatures: [profileId],
+        invitation: group.domain ? `${group.domain}/-/Edit` : group.invitations[0],
+      }
+      await api.post('/groups/edits', requestBody, { accessToken })
+      promptMessage(`Content object for ${group.id} has been updated`, { scrollToTop: false })
+      reloadGroup()
+    } catch (error) {
+      promptError(error.message)
+    }
+    setIsSaving(false)
+  }
+
+  return (
+    <div>
+      <CodeEditor
+        code={code}
+        onChange={setCode}
+      />
+
+      <div className="mt-3">
+        <SpinnerButton
+          type="primary"
+          onClick={saveCode}
+          disabled={isSaving}
+          loading={isSaving}
+        >
+          {isSaving ? 'Saving' : 'Update Code'}
+        </SpinnerButton>
+      </div>
+    </div>
+  )
+}
