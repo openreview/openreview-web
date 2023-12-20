e98db2efa55703cb356aea5ed1e9b9d05a891972
Zach Bialecki <zach@openreview.net>
2023-12-20 17:11:42 -0500

    Remove source code link from minimal footer

--- components/FooterMinimal.js
+++ components/FooterMinimal.js
@@ -5,12 +5,10 @@ const FooterMinimal = () => (
     <div className="row">
       <div className="col-xs-12">
         <p className="text-center">
-          &copy; 2023 OpenReview.net &nbsp;&nbsp;&bull;&nbsp;&nbsp;
+          &copy; {new Date().getFullYear()} OpenReview.net &nbsp;&nbsp;&bull;&nbsp;&nbsp;
           <Link href="/legal/terms">Terms of Use</Link>
           &nbsp;&nbsp;&bull;&nbsp;&nbsp;
           <Link href="/legal/privacy">Privacy Policy</Link>
-          &nbsp;&nbsp;&bull;&nbsp;&nbsp;
-          <Link href="https://github.com/openreview/openreview-web">Source Code</Link>
         </p>
       </div>
     </div>
