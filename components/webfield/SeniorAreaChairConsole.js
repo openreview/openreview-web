9b349510866969960abc0be415021841b439eedf
celestemartinez <32438984+celestemartinez@users.noreply.github.com>
2024-04-01 16:53:14 -0400

    Update components/webfield/SeniorAreaChairConsole.js
    Co-authored-by: Zach Bialecki <86174+zbialecki@users.noreply.github.com>

--- components/webfield/SeniorAreaChairConsole.js
+++ components/webfield/SeniorAreaChairConsole.js
@@ -643,7 +643,7 @@ const SeniorAreaChairConsole = ({ appContext }) => {
             id="areachair-status"
             active={activeTabId === '#areachair-status' ? true : undefined}
             onClick={() => setActiveTabId('#areachair-status')}
-            hidden={assignmentInvitation ? false : true}
+            hidden={!assignmentInvitation}
           >
             Area Chair Status
           </Tab>
