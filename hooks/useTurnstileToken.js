6c8ce9359061440787d119675ebe3eed6d2fa862
xukun <xukun@openreview.net>
2024-04-11 12:55:01 -0400

    change recruitment and signup to use hook

--- hooks/useTurnstileToken.js
+++ hooks/useTurnstileToken.js
@@ -3,17 +3,25 @@ import { useEffect, useState } from 'react'
 
 export default function useTurnstileToken(key, renderWidget) {
   const [turnstileToken, setTurnstileToken] = useState(null)
+  const [widgetId, setWidgetId] = useState(null)
 
   useEffect(() => {
-    if (!renderWidget) setTurnstileToken(null)
+    if (!renderWidget) {
+      setTurnstileToken(null)
+      if (widgetId) {
+        window.turnstile.remove(widgetId)
+      }
+      return
+    }
     if (window.turnstile) {
-      window.turnstile.render(`#turnstile-${key}`, {
+      const id = window.turnstile.render(`#turnstile-${key}`, {
         sitekey: process.env.TURNSTILE_SITEKEY,
         action: key,
         callback: (token) => {
           setTurnstileToken(token)
         },
       })
+      setWidgetId(id)
     } else {
       promptError(
         'Could not verify browser. Please make sure third-party scripts are not being blocked and try again.'
