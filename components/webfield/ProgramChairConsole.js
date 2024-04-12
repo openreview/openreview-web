9b9690f9972bc60c4fa39b4770f58c67e57b0873
haroldrubio <harrywill.rubio@gmail.com>
2024-04-12 14:54:25 -0400

    Restore import and new field

--- components/webfield/ProgramChairConsole.js
+++ components/webfield/ProgramChairConsole.js
@@ -14,6 +14,7 @@ import {
   getNumberFromGroup,
   getProfileName,
   prettyId,
+  prettyField,
   parseNumberField,
   isValidEmail,
 } from '../../lib/utils'
@@ -70,6 +71,7 @@ const ProgramChairConsole = ({ appContext }) => {
     emailReplyTo,
     reviewerEmailFuncs,
     acEmailFuncs,
+    submissionContentFields=[],
   } = useContext(WebFieldContext)
   const { setBannerContent } = appContext
   const { user, accessToken, userLoading } = useUser()
