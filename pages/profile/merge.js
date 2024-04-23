ae25dc568682be8fb889278f72b7ae74a858242f
xkopenreview <60613434+xkopenreview@users.noreply.github.com>
2024-04-23 16:24:24 -0400

    Feature/ Add merge page to confirm profile merge (#1890)
    * add page for merge confirm; remove merge modal prefill logic
    
    * Add test for merge profile modal when user confirms email of another user
    
    * update confirmation message
    
    * update merge url and message
    
    * check token type
    
    * update action name
    
    * Update mergeUser fullname and tildeId in profilePage.ts test
    
    * show email on page
    
    ---------
    
    Co-authored-by: Zach Bialecki <86174+zbialecki@users.noreply.github.com>

--- pages/profile/merge.js
+++ pages/profile/merge.js
@@ -3,25 +3,25 @@
 
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
-import LoadingSpinner from '../components/LoadingSpinner'
-import ErrorDisplay from '../components/ErrorDisplay'
-import api from '../lib/api-client'
-import useLoginRedirect from '../hooks/useLoginRedirect'
-import SpinnerButton from '../components/SpinnerButton'
+import LoadingSpinner from '../../components/LoadingSpinner'
+import ErrorDisplay from '../../components/ErrorDisplay'
+import api from '../../lib/api-client'
+import useLoginRedirect from '../../hooks/useLoginRedirect'
+import SpinnerButton from '../../components/SpinnerButton'
 
-const Confirm = () => {
-  const { user, accessToken } = useLoginRedirect()
+const Merge = () => {
+  const { user } = useLoginRedirect()
   const router = useRouter()
   const [error, setError] = useState(null)
   const [isLoading, setIsLoading] = useState(false)
-  const [emailToConfirm, setEmailToConfirm] = useState(null)
+  const [profileToMergeFrom, setProfileToMergeFrom] = useState(null)
 
-  const confirmEmail = () => {
+  const confirmProfileMerge = () => {
     setIsLoading(true)
     api
-      .put(`/activatelink/${router.query.token}`)
-      .then(({ confirmedEmail }) => {
-        promptMessage(`Thank you for confirming your email ${confirmedEmail ?? ''}`)
+      .put(`/mergelink/${router.query.token}`)
+      .then(() => {
+        promptMessage(`Thank you for confirming the profile merge.`)
         setIsLoading(false)
         router.replace('/')
       })
@@ -34,20 +34,20 @@ const Confirm = () => {
   const getActivatable = async () => {
     try {
       const {
-        activatable: { groupId, username, action },
+        activatable: { action, duplicateProfile, username, groupId },
       } = await api.get(`/activatable/${router.query.token}`)
-      if (action !== 'alternate') {
+      if (action !== 'merge') {
         setError({ statusCode: 403, message: 'Token is invalid.' })
         return
       }
       if (!user?.profile?.usernames?.includes(username)) {
         setError({
           statusCode: 403,
-          message: 'You are not authorized to activate this email.',
+          message: 'You are not authorized to perform this merge.',
         })
         return
       }
-      setEmailToConfirm(groupId)
+      setProfileToMergeFrom({ profileId: duplicateProfile, email: groupId })
     } catch (apiError) {
       setError({ statusCode: apiError.status, message: apiError.message })
     }
@@ -57,40 +57,47 @@ const Confirm = () => {
     if (!router.isReady) return
 
     if (!router.query.token) {
-      setError({ statusCode: 404, message: 'Activation token not found' })
+      setError({ statusCode: 404, message: 'Token is invalid' })
     }
-
     getActivatable()
   }, [router.isReady, router.query])
 
   if (error) return <ErrorDisplay statusCode={error.statusCode} message={error.message} />
-  if (!emailToConfirm) return <LoadingSpinner />
+  if (!user || !profileToMergeFrom) return <LoadingSpinner />
 
   return (
     <>
       <header>
-        <h1>Account Linking</h1>
+        <h1>Profile Merge</h1>
       </header>
       <p className="mt-4">
-        Click Confirm Email button below to confirm adding <strong>{emailToConfirm}</strong> to
-        your account.
+        Click the confirm button below to merge{' '}
+        <a
+          href={`/profile?id=${profileToMergeFrom.profileId}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <strong>{profileToMergeFrom.profileId}</strong>
+        </a>
+        <strong>{`<${profileToMergeFrom.email}> `}</strong>
+        into your user profile.
       </p>
       <p className="mb-4">
         Please contact{' '}
         <a href="mailto:info@openreview.net" target="_blank" rel="noreferrer">
           info@openreview.net
         </a>{' '}
-        if you didn&apos;t add this email.
+        if you didn&apos;t request for this profile to be merged with your account.
       </p>
 
       <div className="response-buttons">
-        <SpinnerButton loading={isLoading} disabled={isLoading} onClick={confirmEmail}>
-          Confirm Email
+        <SpinnerButton loading={isLoading} disabled={isLoading} onClick={confirmProfileMerge}>
+          Confirm Profile Merge
         </SpinnerButton>
       </div>
     </>
   )
 }
 
-Confirm.bodyClass = 'confirm'
-export default Confirm
+Merge.bodyClass = 'confirm'
+export default Merge
