57cacd5fdfc4e04d164d2dcbdb218652adf4c612
Zach Bialecki <zach@openreview.net>
2024-05-14 11:16:35 -0400

    Refactor component

--- components/forum/ChatReply.js
+++ components/forum/ChatReply.js
@@ -113,7 +113,7 @@ export default forwardRef(function ChatReply(
     hideDeleteModal()
   }
 
-  const addOrRemoveTag = async (tagValue, existingTags) => {
+  const addOrRemoveTag = (tagValue, existingTags) => {
     if (loading || !accessToken || !reactionInvitation || !signature) return
 
     setLoading(true)
@@ -265,61 +265,12 @@ export default forwardRef(function ChatReply(
           />
         )}
 
-        {note.reactions.length > 0 && (
-          <ul className="chat-reactions list-inline">
-            {note.reactions.map(([reaction, tags]) => {
-              const sigs = tags.map((t) => t.signature)
-              const isActive = sigs.includes(signature)
-              const tooltipText = `${inflect(
-                tags.length,
-                'reaction',
-                'reactions',
-                true
-              )} from ${prettyList(sigs, 'short', 'conjunction')}`
-              return (
-                <li key={reaction}>
-                  <button
-                    className={`btn btn-xs btn-default ${isActive ? 'selected' : ''}`}
-                    data-toggle="tooltip"
-                    data-placement="top"
-                    data-container="body"
-                    title={tooltipText}
-                    onClick={(e) => {
-                      $(e.target).tooltip('destroy')
-                      addOrRemoveTag(reaction, tags)
-                    }}
-                    onMouseLeave={(e) => {
-                      $(e.target).tooltip('destroy')
-                    }}
-                  >
-                    <span>{reaction}</span> {tags.length}
-                  </button>
-                </li>
-              )
-            })}
-            <li key={'add-reaction'}>
-              <button
-                className="btn btn-xs btn-default add-reaction"
-                data-toggle="tooltip"
-                data-container="body"
-                data-placement="top"
-                title="Add Reaction"
-                onClick={(e) => {
-                  $(e.target).tooltip('hide')
-                  toggleReactionPicker()
-                }}
-                onMouseLeave={(e) => {
-                  $(e.target).tooltip('hide')
-                }}
-              >
-                <svg viewBox="0 0 16 16" version="1.1" aria-hidden="true">
-                  <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0ZM1.5 8a6.5 6.5 0 1 0 13 0 6.5 6.5 0 0 0-13 0Zm3.82 1.636a.75.75 0 0 1 1.038.175l.007.009c.103.118.22.222.35.31.264.178.683.37 1.285.37.602 0 1.02-.192 1.285-.371.13-.088.247-.192.35-.31l.007-.008a.75.75 0 0 1 1.222.87l-.022-.015c.02.013.021.015.021.015v.001l-.001.002-.002.003-.005.007-.014.019a2.066 2.066 0 0 1-.184.213c-.16.166-.338.316-.53.445-.63.418-1.37.638-2.127.629-.946 0-1.652-.308-2.126-.63a3.331 3.331 0 0 1-.715-.657l-.014-.02-.005-.006-.002-.003v-.002h-.001l.613-.432-.614.43a.75.75 0 0 1 .183-1.044ZM12 7a1 1 0 1 1-2 0 1 1 0 0 1 2 0ZM5 8a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm5.25 2.25.592.416a97.71 97.71 0 0 0-.592-.416Z"></path>
-                </svg>{' '}
-                +
-              </button>
-            </li>
-          </ul>
-        )}
+        <ReactionButtons
+          noteReactions={note.reactions}
+          signature={signature}
+          addOrRemoveTag={addOrRemoveTag}
+          toggleReactionPicker={toggleReactionPicker}
+        />
       </div>
 
       <div className={styles['chat-actions']}>
@@ -465,6 +416,67 @@ function ChatSignature({ groupId, signatureGroup }) {
         </span>
       )}
     </strong>
+  )
+}
+
+function ReactionButtons({ noteReactions, signature, addOrRemoveTag, toggleReactionPicker }) {
+  if (!noteReactions || noteReactions.length === 0) return null
+
+  return (
+    <ul className="chat-reactions list-inline">
+      {noteReactions.map(([reaction, tags]) => {
+        const sigs = tags.map((t) => t.signature)
+        const isActive = sigs.includes(signature)
+        const tooltipText = `${inflect(
+          tags.length,
+          'reaction',
+          'reactions',
+          true
+        )} from ${prettyList(sigs, 'short', 'conjunction')}`
+
+        return (
+          <li key={reaction}>
+            <button
+              className={`btn btn-xs btn-default ${isActive ? 'selected' : ''}`}
+              data-toggle="tooltip"
+              data-placement="top"
+              data-container="body"
+              title={tooltipText}
+              onClick={(e) => {
+                $(e.target).tooltip('destroy')
+                addOrRemoveTag(reaction, tags)
+              }}
+              onMouseLeave={(e) => {
+                $(e.target).tooltip('destroy')
+              }}
+            >
+              <span>{reaction}</span> {tags.length}
+            </button>
+          </li>
+        )
+      })}
+      <li key={'add-reaction'}>
+        <button
+          className="btn btn-xs btn-default add-reaction"
+          data-toggle="tooltip"
+          data-container="body"
+          data-placement="top"
+          title="Add Reaction"
+          onClick={(e) => {
+            $(e.target).tooltip('hide')
+            toggleReactionPicker()
+          }}
+          onMouseLeave={(e) => {
+            $(e.target).tooltip('hide')
+          }}
+        >
+          <svg viewBox="0 0 16 16" version="1.1" aria-hidden="true">
+            <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0ZM1.5 8a6.5 6.5 0 1 0 13 0 6.5 6.5 0 0 0-13 0Zm3.82 1.636a.75.75 0 0 1 1.038.175l.007.009c.103.118.22.222.35.31.264.178.683.37 1.285.37.602 0 1.02-.192 1.285-.371.13-.088.247-.192.35-.31l.007-.008a.75.75 0 0 1 1.222.87l-.022-.015c.02.013.021.015.021.015v.001l-.001.002-.002.003-.005.007-.014.019a2.066 2.066 0 0 1-.184.213c-.16.166-.338.316-.53.445-.63.418-1.37.638-2.127.629-.946 0-1.652-.308-2.126-.63a3.331 3.331 0 0 1-.715-.657l-.014-.02-.005-.006-.002-.003v-.002h-.001l.613-.432-.614.43a.75.75 0 0 1 .183-1.044ZM12 7a1 1 0 1 1-2 0 1 1 0 0 1 2 0ZM5 8a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm5.25 2.25.592.416a97.71 97.71 0 0 0-.592-.416Z"></path>
+          </svg>{' '}
+          +
+        </button>
+      </li>
+    </ul>
   )
 }
 
