39276b8e67c801d7301d71c5617c41ade631245d
xukun <xukun@openreview.net>
2024-05-31 14:32:16 -0400

    Refactor GroupEditor component to use dynamic tabs and sections

--- components/group/GroupEditor.js
+++ components/group/GroupEditor.js
@@ -7,48 +7,112 @@ import GroupUICode from './GroupUICode'
 import GroupContent from './GroupContent'
 import GroupContentScripts from './GroupContentScripts'
 import WorkFlowInvitations from './WorkflowInvitations'
+import { Tab, TabList, TabPanel, TabPanels, Tabs } from '../Tabs'
 
 const GroupEditor = ({ group, isSuperUser, profileId, accessToken, reloadGroup }) => {
   if (!group) return null
-
+  const groupTabsConfig = [
+    {
+      id: 'groupInfo',
+      label: 'Group Info',
+      sections: ['groupGeneral', 'groupMembers', 'groupContent'],
+      default: true,
+    },
+    {
+      id: 'groupContentScripts',
+      label: 'Group Content Scripts',
+      sections: ['groupContentScripts'],
+    },
+    { id: 'signedNotes', label: 'Signed Notes', sections: ['groupSignedNotes'] },
+    { id: 'childGroups', label: 'Child Groups', sections: ['groupChildGroups'] },
+    {
+      id: 'relatedInvitations',
+      label: 'Related Invitations',
+      sections: ['groupRelatedInvitations'],
+    },
+    {
+      id: 'workflowInvitations',
+      label: 'Workflow Invitations',
+      sections: ['workflowInvitations'],
+    },
+  ]
+  const renderSection = (sectionName) => {
+    switch (sectionName) {
+      case 'groupGeneral':
+        return (
+          <GroupGeneral
+            group={group}
+            profileId={profileId}
+            isSuperUser={isSuperUser}
+            accessToken={accessToken}
+            reloadGroup={reloadGroup}
+          />
+        )
+      case 'groupMembers':
+        return (
+          <GroupMembers group={group} accessToken={accessToken} reloadGroup={reloadGroup} />
+        )
+      case 'groupContent':
+        return (
+          group.invitations && (
+            <GroupContent
+              group={group}
+              profileId={profileId}
+              accessToken={accessToken}
+              reloadGroup={reloadGroup}
+            />
+          )
+        )
+      case 'groupContentScripts':
+        return (
+          group.invitations && (
+            <GroupContentScripts
+              key={`${group.id}-content-scripts`}
+              group={group}
+              profileId={profileId}
+              accessToken={accessToken}
+              reloadGroup={reloadGroup}
+            />
+          )
+        )
+      case 'groupSignedNotes':
+        return <GroupSignedNotes group={group} accessToken={accessToken} />
+      case 'groupChildGroups':
+        return <GroupChildGroups groupId={group.id} accessToken={accessToken} />
+      case 'groupRelatedInvitations':
+        return <GroupRelatedInvitations group={group} accessToken={accessToken} />
+      case 'workflowInvitations':
+        return <WorkFlowInvitations group={group} accessToken={accessToken} />
+      case 'groupUICode':
+        return (
+          <GroupUICode
+            group={group}
+            profileId={profileId}
+            accessToken={accessToken}
+            reloadGroup={reloadGroup}
+          />
+        )
+      default:
+        return null
+    }
+  }
   return (
-    <div>
-      <GroupGeneral
-        group={group}
-        profileId={profileId}
-        isSuperUser={isSuperUser}
-        accessToken={accessToken}
-        reloadGroup={reloadGroup}
-      />
-      <GroupMembers group={group} accessToken={accessToken} reloadGroup={reloadGroup} />
-      {group.invitations && (
-        <GroupContent
-          group={group}
-          profileId={profileId}
-          accessToken={accessToken}
-          reloadGroup={reloadGroup}
-        />
-      )}
-      {group.invitations && (
-        <GroupContentScripts
-          key={`${group.id}-content-scripts`}
-          group={group}
-          profileId={profileId}
-          accessToken={accessToken}
-          reloadGroup={reloadGroup}
-        />
-      )}
-      <GroupSignedNotes group={group} accessToken={accessToken} />
-      <GroupChildGroups groupId={group.id} accessToken={accessToken} />
-      <GroupRelatedInvitations group={group} accessToken={accessToken} />
-      <WorkFlowInvitations group={group} accessToken={accessToken} />
-      <GroupUICode
-        group={group}
-        profileId={profileId}
-        accessToken={accessToken}
-        reloadGroup={reloadGroup}
-      />
-    </div>
+    <Tabs>
+      <TabList>
+        {groupTabsConfig.map((tabConfig) => (
+          <Tab key={tabConfig.id} id={tabConfig.id} active={tabConfig.default}>
+            {tabConfig.label}
+          </Tab>
+        ))}
+      </TabList>
+      <TabPanels>
+        {groupTabsConfig.map((tabConfig) => (
+          <TabPanel key={tabConfig.id} id={tabConfig.id}>
+            {tabConfig.sections.map((section) => renderSection(section))}
+          </TabPanel>
+        ))}
+      </TabPanels>
+    </Tabs>
   )
 }
 
