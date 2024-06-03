202e28d4f093551896028f752a38267c58e9267d
Akshat Chhabra <akshat@openreview.net>
2024-06-03 09:50:31 -0700

    test: add unit tests for PronounsSection

---
+++ unitTests/PronounsSection.test.js
@@ -0,0 +1,40 @@
+import { cleanup, render, screen, waitFor } from '@testing-library/react'
+import { afterEach } from 'node:test'
+import '@testing-library/jest-dom'
+import PronounSection from "../components/profile/PronounSection"
+import GenderSection from '../components/profile/GenderSection'
+
+afterEach(() => {
+  cleanup()
+})
+
+describe ('Pronoun selection dropdown', () => {
+  test ('check if pronouns dropdown renders successfully when no pronouns are selected', () => {
+    const properties = {
+      profilePronouns: '',
+      updatePronoun: jest.fn()
+    }
+
+    const {container} = render(<PronounSection {...properties}/>)
+    const element = screen.getByText('Choose pronouns or type a custom pronouns')
+    expect(element).toBeInTheDocument()
+  })
+
+  test ('check if pronouns dropdown renders successfully', () => {
+    const properties = {
+      profilePronouns: 'he/him',
+      updatePronoun: jest.fn()
+    }
+
+    const {container} = render(<PronounSection {...properties}/>)
+    const element = screen.getByText('he/him')
+    expect(element).toBeInTheDocument()
+  })
+})
+
+
+
+
+
+
+
