2c949e4d93d9581eabc43c1152ed31be044de93a
Akshat Chhabra <akshat@openreview.net>
2024-06-12 13:59:08 -0700

    test: add assertion on updatePronoun

--- unitTests/PronounsSection.test.js
+++ unitTests/PronounsSection.test.js
@@ -4,24 +4,30 @@ import PronounSection from '../components/profile/PronounSection'
 
 describe('Pronoun selection dropdown', () => {
   test('check if pronouns dropdown renders successfully when no pronouns are selected', () => {
+    const updatePronoun = jest.fn()
     const properties = {
       profilePronouns: '',
-      updatePronoun: jest.fn(),
+      updatePronoun,
     }
 
     render(<PronounSection {...properties} />)
     const element = screen.getByText('Choose pronouns or type a custom pronouns')
     expect(element).toBeInTheDocument()
+    expect(updatePronoun).toHaveBeenCalledTimes(1)
+    expect(updatePronoun).toHaveBeenCalledWith('')
   })
 
   test('check if pronouns dropdown renders successfully', () => {
+    const updatePronoun = jest.fn()
     const properties = {
       profilePronouns: 'he/him',
-      updatePronoun: jest.fn(),
+      updatePronoun,
     }
 
     render(<PronounSection {...properties} />)
     const element = screen.getByText('he/him')
     expect(element).toBeInTheDocument()
+    expect(updatePronoun).toHaveBeenCalledTimes(1)
+    expect(updatePronoun).toHaveBeenCalledWith('he/him')
   })
 })
