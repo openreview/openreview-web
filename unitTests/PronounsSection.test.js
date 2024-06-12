6d62d82d7a43b7aef3d76af433a126eca2037df8
Akshat Chhabra <akshat@openreview.net>
2024-06-12 13:38:57 -0700

    refactor: format pronounsSection.test.js

--- unitTests/PronounsSection.test.js
+++ unitTests/PronounsSection.test.js
@@ -1,35 +1,27 @@
-import {render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
-import PronounSection from "../components/profile/PronounSection"
+import PronounSection from '../components/profile/PronounSection'
 
-
-describe ('Pronoun selection dropdown', () => {
-  test ('check if pronouns dropdown renders successfully when no pronouns are selected', () => {
+describe('Pronoun selection dropdown', () => {
+  test('check if pronouns dropdown renders successfully when no pronouns are selected', () => {
     const properties = {
       profilePronouns: '',
-      updatePronoun: jest.fn()
+      updatePronoun: jest.fn(),
     }
 
-    render(<PronounSection {...properties}/>)
+    render(<PronounSection {...properties} />)
     const element = screen.getByText('Choose pronouns or type a custom pronouns')
     expect(element).toBeInTheDocument()
   })
 
-  test ('check if pronouns dropdown renders successfully', () => {
+  test('check if pronouns dropdown renders successfully', () => {
     const properties = {
       profilePronouns: 'he/him',
-      updatePronoun: jest.fn()
+      updatePronoun: jest.fn(),
     }
 
-    render(<PronounSection {...properties}/>)
+    render(<PronounSection {...properties} />)
     const element = screen.getByText('he/him')
     expect(element).toBeInTheDocument()
   })
 })
-
-
-
-
-
-
-
