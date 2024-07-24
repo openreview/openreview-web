a58c4d59856e62b773c6199831e2afd4a43b851a
xkopenreview <xukun@openreview.net>
2024-07-24 06:46:33 -0400

    chore: Mock form component in ContentFieldEditor test

--- unitTests/ContentFieldEditor.test.js
+++ unitTests/ContentFieldEditor.test.js
@@ -4,6 +4,8 @@ import { renderWithEditorComponentContext, reRenderWithEditorComponentContext } 
 import '@testing-library/jest-dom'
 import ContentFieldEditor from '../components/EditorComponents/ContentFieldEditor'
 
+jest.mock('../components/Form', () => () => 'mocked Form')
+
 describe('ContentFieldEditor', () => {
   test('render 3 tabs for widget editing, json editing and preview', () => {
     const providerProps = {
@@ -21,7 +23,7 @@ describe('ContentFieldEditor', () => {
     }
 
     renderWithEditorComponentContext(<ContentFieldEditor />, providerProps)
-    expect(screen.getByText('Widget')).toBeInTheDocument()
+    expect(screen.getByText('Widgets')).toBeInTheDocument()
     expect(screen.getByText('Content JSON')).toBeInTheDocument()
     expect(screen.getByText('Preview')).toBeInTheDocument()
   })
