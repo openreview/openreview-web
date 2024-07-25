41f96c7fb87525d40f3eedcaf477bd4eee88ff5e
xkopenreview <xukun@openreview.net>
2024-07-25 10:30:45 -0400

    remove email from edge browser; add copy email link

--- components/browser/ProfileEntity.js
+++ components/browser/ProfileEntity.js
@@ -5,6 +5,7 @@
 
 import { nanoid } from 'nanoid'
 import React, { useContext } from 'react'
+import copy from 'copy-to-clipboard'
 import api from '../../lib/api-client'
 import {
   getInterpolatedValues,
@@ -19,6 +20,7 @@ import EditEdgeDropdown from './EditEdgeDropdown'
 import EditEdgeToggle from './EditEdgeToggle'
 import EditEdgeTwoDropdowns from './EditEdgeTwoDropdowns'
 import ScoresList from './ScoresList'
+import useQuery from '../../hooks/useQuery'
 
 export default function ProfileEntity(props) {
   const {
@@ -30,6 +32,7 @@ export default function ProfileEntity(props) {
     version,
   } = useContext(EdgeBrowserContext)
   const { user, accessToken } = useContext(UserContext)
+  const query = useQuery()
 
   if (!props.profile || !props.profile.content) {
     return null
@@ -38,7 +41,6 @@ export default function ProfileEntity(props) {
   // Format profile data for rendering
   const {
     id,
-    email,
     content,
     editEdges,
     editEdgeTemplates,
@@ -139,6 +141,21 @@ export default function ProfileEntity(props) {
         props.updateChildColumn(props.columnIndex, null)
       }
       props.reloadColumnEntities()
+    } catch (error) {
+      promptError(error.message)
+    }
+  }
+
+  const getEmail = async () => {
+    try {
+      const result = await api.get(`/edges`, {
+        invitation: query.preferredEmailInvitationId,
+        head: id,
+      })
+      const email = result.edges?.[0]?.tail
+      if (!email) throw new Error('Email is not available.')
+      copy(`${content.name.fullname} <${email}>`)
+      promptMessage(`${email} copied to clipboard`)
     } catch (error) {
       promptError(error.message)
     }
@@ -425,10 +442,18 @@ export default function ProfileEntity(props) {
             rel="noreferrer"
           >
             {content.name.fullname}
-          </a>{' '}
-          <span>({content.email})</span>
+          </a>
         </h3>
-
+        {query.preferredEmailInvitationId && !content.isDummyProfile && (
+          <p
+            onClick={(e) => {
+              e.stopPropagation()
+              getEmail()
+            }}
+          >
+            Copy Email
+          </p>
+        )}
         <p>{content.title}</p>
       </div>
 
