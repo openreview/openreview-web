c22b46b3415e899cedc3048ee0f4c4e08112a853
xkopenreview <xukun@openreview.net>
2024-10-24 16:41:52 -0400

    increase font loading speed

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,14 @@ export default class OpenReviewDocument extends Document {
       <Html lang="en">
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+            rel="preload"
+            href="https://fonts.gstatic.com/s/notosans/v36/o-0bIpQlx3QUlC5A4PNB6Ryti20_6n1iPHjc5aDdu3mhPy1Fig.woff2"
+            as="font"
+            type="font/woff2"
+            crossorigin
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&display=fallback"
             rel="stylesheet"
           />
           <script
