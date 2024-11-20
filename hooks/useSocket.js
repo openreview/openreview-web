1dcf5818479cb65c84fe0d734b2ad3ace74708da
xkopenreview <xukun@openreview.net>
2024-11-20 13:59:43 -0500

    update auth when token change

--- hooks/useSocket.js
+++ hooks/useSocket.js
@@ -24,6 +24,12 @@ export default function useSocket(namespace, eventNames, options) {
     }
   }, [namespace])
 
+  useEffect(() => {
+    if (!accessToken || !socket.current) return
+    socket.current.auth = { accessToken }
+    socket.current.connect()
+  }, [accessToken])
+
   useEffect(
     () => () => {
       if (socket.current) {
