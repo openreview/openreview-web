27a1991f092f7b569f2f5112aa808a43a6055894
xkopenreview <xukun@openreview.net>
2025-02-05 14:18:05 -0500

    enable logging in prod

--- middleware.js
+++ middleware.js
@@ -2,26 +2,18 @@ import { NextResponse } from 'next/server'
 
 // eslint-disable-next-line import/prefer-default-export
 export async function middleware(request) {
-  // console.log('middleware.js request')
   const response = NextResponse.next()
   const accessToken = request.cookies.get(process.env.ACCESS_TOKEN_NAME)
   const refreshToken = request.cookies.get(process.env.REFRESH_TOKEN_NAME)
-  // console.log('middleware.js accesstoken is ', accessToken)
-  // console.log('middleware.js refreshToken is ', refreshToken)
-  // console.log('middleware request cookies', request.cookies)
-
   if (accessToken) {
-    // console.log('middleware has access token so return')
     return response
   }
 
   if (!refreshToken) {
-    // console.log('middleware.js has no refresh token so return')
     return response
   }
 
   try {
-    // console.log('middleware start to refresh token')
     const tokenRefreshResponse = await fetch(`${process.env.API_V2_URL}/refreshToken`, {
       method: 'POST',
       headers: {
@@ -36,14 +28,12 @@ export async function middleware(request) {
       console.error('middleware.js refresh token failed', data)
       return response
     }
-    // console.log('middleware.js refreshed token')
     return NextResponse.redirect(request.nextUrl, {
       headers: {
         'Set-Cookie': tokenRefreshResponse.headers.get('set-cookie'),
       },
     })
   } catch (error) {
-    // console.error('middleware.js refresh token failed')
     return response
   }
 }
