b367895d3f6e71ce116b90d61ab256e732134333
xkopenreview <xukun@openreview.net>
2025-02-25 15:53:27 -0500

    fix timeshift

--- app/(Home)/VenueListItem.js
+++ app/(Home)/VenueListItem.js
@@ -1,10 +1,19 @@
+'use client'
+
 import Link from 'next/link'
+import { useEffect, useState } from 'react'
 import { formatTimestamp, prettyId } from '../../lib/utils'
 import Icon from '../../components/Icon'
 
 export default function VenueListItem({ groupId, dueDate, hidden, isLeadingVenue = false }) {
   const styles = hidden ? { display: 'none' } : {}
+  const [isClientRendering, setIsClientRendering] = useState(false)
 
+  useEffect(() => {
+    setIsClientRendering(true)
+  }, [])
+
+  if (!isClientRendering) return null
   return (
     <li style={styles}>
       <h2>
