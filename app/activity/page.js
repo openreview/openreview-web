682245a0144eba250993239b039247dfa6d3bfaa
xkopenreview <xukun@openreview.net>
2025-03-18 14:29:53 -0400

    check token

--- app/activity/page.js
+++ app/activity/page.js
@@ -1,9 +1,5 @@
 import { redirect } from 'next/navigation'
-import { Suspense } from 'react'
-import { headers } from 'next/headers'
 import serverAuth from '../auth'
-import LoadingSpinner from '../../components/LoadingSpinner'
-import api from '../../lib/api-client'
 import Activity from './Activity'
 import styles from './Activity.module.scss'
 
@@ -13,13 +9,12 @@ export const metadata = {
 
 export default async function page() {
   const { token } = await serverAuth()
+  console.log(token)
   if (!token) redirect('/login?redirect=/activity')
 
   return (
     <div className={styles.activity}>
-      <Suspense fallback={<LoadingSpinner />}>
-        <Activity />
-      </Suspense>
+      <Activity />
     </div>
   )
 }
