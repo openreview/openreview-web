02859f083229f360c2ecd13d74ebac9e85e5b5f9
xkopenreview <xukun@openreview.net>
2025-03-28 16:54:53 -0400

    convert activity to client rendering

--- app/activity/page.js
+++ app/activity/page.js
@@ -1,67 +1,87 @@
-import { redirect } from 'next/navigation'
-import { Suspense } from 'react'
-import { headers } from 'next/headers'
-import { connection } from 'next/server'
-import serverAuth from '../auth'
+'use client'
+
+/* globals $,typesetMathJax: false */
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/navigation'
 import LoadingSpinner from '../../components/LoadingSpinner'
 import api from '../../lib/api-client'
-import Activity from './Activity'
 import styles from './Activity.module.scss'
+import useUser from '../../hooks/useUser'
+import BaseActivityList from '../../components/BaseActivityList'
+import ErrorAlert from '../../components/ErrorAlert'
 
-export const metadata = {
-  title: 'Activity | OpenReview',
-}
+export default function Page() {
+  const { accessToken, isRefreshing } = useUser()
+  const [activityNotes, setActivityNotes] = useState(null)
+  const [error, setError] = useState(null)
+  const router = useRouter()
 
-export default async function page() {
-  await connection()
-  const { token } = await serverAuth()
-  if (!token) redirect('/login?redirect=/activity')
-  const headersList = await headers()
-  const remoteIpAddress = headersList.get('x-forwarded-for')
+  const loadActivityData = async () => {
+    const queryParamV1 = {
+      tauthor: true,
+      trash: true,
+      details: 'forumContent,writable,invitation',
+      sort: 'tmdate:desc',
+      limit: 100,
+    }
+    const queryParamV2 = {
+      tauthor: true,
+      trash: true,
+      details: 'writable,invitation',
+      sort: 'tmdate:desc',
+      limit: 100,
+    }
 
-  const activityDataP = api
-    .get(
-      '/notes/edits',
-      {
-        tauthor: true,
-        trash: true,
-        details: 'writable,invitation',
-        sort: 'tmdate:desc',
-        limit: 100,
-      },
-      { accessToken: token, remoteIpAddress }
-    )
-    .then((result) => {
-      const edits = result.edits || []
-      return {
-        activityNotes: edits
-          .map((edit) => ({ ...edit, apiVersion: 2 }))
-          .sort((a, b) => b.tmdate - a.tmdate),
-      }
-    })
-    .catch((error) => {
-      console.log('Error in activityDataP', {
-        page: 'activity',
-        apiError: error,
-        apiRequest: {
-          endpoint: '/notes/edits',
-          params: {
-            tauthor: true,
-            trash: true,
-            details: 'writable,invitation',
-            sort: 'tmdate:desc',
-            limit: 100,
-          },
-        },
+    Promise.all([
+      api.get('/notes', queryParamV1, { accessToken, version: 1 }).then(
+        ({ notes }) => (notes?.length > 0 ? notes : []),
+        () => []
+      ),
+      api
+        .get('/notes/edits', queryParamV2, { accessToken })
+        .then(
+          ({ edits }) => (edits?.length > 0 ? edits : []),
+          () => []
+        )
+        .then((edits) => edits.map((edit) => ({ ...edit, apiVersion: 2 }))),
+    ])
+      .then(([notes, edits]) => {
+        setActivityNotes(notes.concat(edits).sort((a, b) => b.tmdate - a.tmdate))
       })
-      return { errorMessage: error.message }
-    })
+      .catch((apiError) => {
+        setError(apiError)
+      })
+  }
+
+  useEffect(() => {
+    if (isRefreshing) return
+    if (!accessToken) {
+      router.push('/login?redirect=/activity')
+      return
+    }
+    loadActivityData()
+  }, [isRefreshing])
+
+  useEffect(() => {
+    if (!activityNotes) return
+
+    setTimeout(() => {
+      typesetMathJax()
+      $('[data-toggle="tooltip"]').tooltip()
+    }, 100)
+  }, [activityNotes])
+
+  if (!activityNotes && !error) return <LoadingSpinner />
+  if (error) return <ErrorAlert error={error} />
 
   return (
     <div className={styles.activity}>
-      <Suspense fallback={<LoadingSpinner />}>
-        <Activity activityDataP={activityDataP} />
-      </Suspense>
+      <BaseActivityList
+        notes={activityNotes}
+        emptyMessage="No recent activity to display."
+        showActionButtons
+        showGroup
+      />
     </div>
   )
 }
