c993e581f52a11d1895eee5b82a1ed7d3440cd80
xkopenreview <xukun@openreview.net>
2025-03-31 16:44:54 -0400

    refactor assignments page

--- app/assignments/Assignments.js
+++ app/assignments/Assignments.js
@@ -1,460 +1,105 @@
 'use client'
 
-/* globals $, promptMessage, promptError: false */
-import { use, useEffect, useState } from 'react'
-import { cloneDeep } from 'lodash'
-import Link from 'next/link'
-import { useRouter } from 'next/navigation'
-import { cloneAssignmentConfigNoteV2, formatDateTime, prettyId } from '../../lib/utils'
-import BasicModal from '../../components/BasicModal'
-import LoadingSpinner from '../../components/LoadingSpinner'
-import NoteContent from '../../components/NoteContent'
-import ErrorAlert from '../../components/ErrorAlert'
-import NoteEditor from '../../components/NoteEditor'
-import Table from '../../components/Table'
-import ActionLink from './ActionLink'
-import { getNoteContentValues } from '../../lib/forum-utils'
-import { getEdgeBrowserUrl } from '../../lib/edge-utils'
+import { useEffect, useState } from 'react'
+import { useSearchParams } from 'next/navigation'
 import api from '../../lib/api-client'
-import PaginationLinks from '../../components/PaginationLinks'
-import useSocket from '../../hooks/useSocket'
+import V2Assignments from './V2Assignments'
+import LoadingSpinner from '../../components/LoadingSpinner'
+import V1Assignments from './V1Assignments'
+import styles from './Assignments.module.scss'
+import ErrorDisplay from '../../components/ErrorDisplay'
+import CommonLayout from '../CommonLayout'
+import { referrerLink, venueHomepageLink } from '../../lib/banner-links'
+import Banner from '../../components/Banner'
 
-const NewNoteEditorModal = ({
-  editorNote,
-  setEditorNote,
-  configInvitation,
-  assignmentNotes,
-}) => {
-  const [errorMessage, setErrorMessage] = useState(null)
-  const router = useRouter()
+export default function Assignments({ accessToken }) {
+  const [error, setError] = useState(null)
+  const [configInvitation, setConfigInvitation] = useState(null)
+  const [preferredEmailInvitationId, setPreferredEmailInvitationId] = useState(null)
+  const searchParams = useSearchParams()
+  const group = searchParams.get('group')
+  const referrer = searchParams.get('referrer')
 
-  const invitationWithHiddenFields = cloneDeep(configInvitation)
-  const fieldsToHide = [
-    'config_invitation',
-    'assignment_invitation',
-    'error_message',
-    'status',
-  ]
-  fieldsToHide.forEach((p) => {
-    const fieldDescription = invitationWithHiddenFields?.edit?.note?.content?.[p]?.value?.param
-    if (fieldDescription) {
-      fieldDescription.hidden = true
+  const notFoundMessage =
+    'There is currently no assignment configuration ready for use. Please go to your venue request form and use the Paper Matching Setup to compute conflicts and/or affinity scores.'
+
+  const assignmentNotesP =
+    configInvitation?.apiVersion === 2
+      ? api
+          .get(
+            '/notes',
+            {
+              invitation: `${group}/-/Assignment_Configuration`,
+            },
+            { accessToken, version: 2 }
+          )
+          .then((response) => {
+            const { notes, count } = response
+            return { notes, count }
+          })
+          .catch((apiError) => ({ errorMessage: apiError.message }))
+      : Promise.resolve(null)
+
+  const getPreferredEmailInvitationId = async (invitation) => {
+    try {
+      const domainGroup = await api.getGroupById(invitation.domain, accessToken)
+      setPreferredEmailInvitationId(domainGroup?.content?.preferred_emails_id?.value)
+    } catch (_) {
+      /* empty */
     }
-  })
-
-  const closeModal = () => {
-    $('#new-note-editor-modal').modal('hide')
-    $('.modal-backdrop').remove()
   }
 
-  const validator = (formData) => {
-    const noteWithMatchingTitle = assignmentNotes.find(
-      (p) =>
-        p.content.title.value === formData.title && (!editorNote || p.id !== editorNote.id)
-    )
-    if (noteWithMatchingTitle) {
-      return {
-        isValid: false,
-        errorMessage: 'The configuration title must be unique within the conference',
+  const getConfigInvitation = async () => {
+    try {
+      const invitation = await api.getInvitationById(
+        `${group}/-/Assignment_Configuration`,
+        accessToken
+      )
+      if (invitation) {
+        setConfigInvitation(invitation)
+        getPreferredEmailInvitationId(invitation)
+      } else {
+        setError({
+          statusCode: 404,
+          message: notFoundMessage,
+        })
       }
-    }
-    return { isValid: true }
-  }
-
-  if (!configInvitation) return null
-
-  return (
-    <BasicModal
-      id="new-note-editor-modal"
-      options={{ hideFooter: true, extraClasses: 'modal-lg' }}
-      onClose={() => {
-        setEditorNote(null)
-        setErrorMessage(null)
-      }}
-    >
-      {errorMessage && <ErrorAlert error={{ message: errorMessage }} />}
-
-      <NoteEditor
-        key={editorNote?.content?.title?.value || 'new-note'}
-        note={editorNote}
-        invitation={invitationWithHiddenFields}
-        closeNoteEditor={() => {
-          closeModal()
-        }}
-        onNoteCreated={() => {
-          closeModal()
-          promptMessage('Note updated successfully')
-          router.refresh()
-        }}
-        setErrorAlertMessage={(msg) => {
-          setErrorMessage(msg)
-          $('#new-note-editor-modal').animate({ scrollTop: 0 }, 400)
-        }}
-        customValidator={validator}
-      />
-    </BasicModal>
-  )
-}
-
-const AssignmentRow = ({
-  note,
-  configInvitation,
-  handleEditConfiguration,
-  handleViewConfiguration,
-  handleCloneConfiguration,
-  handleRunMatcher,
-  handleDeployMatcher,
-  handleUndeployMatcher,
-  referrer,
-  shouldShowDeployLink,
-  preferredEmailInvitationId,
-  handleConfigNoteUpdate,
-}) => {
-  const statusToCheck = ['Running', 'Queued', 'Deploying', 'Undeploying']
-  const [loading, setLoading] = useState(false)
-  const router = useRouter()
-  const noteContent = getNoteContentValues(note.content)
-  const events = useSocket(
-    statusToCheck.includes(noteContent.status) ? 'note' : undefined,
-    ['edit-upserted'],
-    {
-      id: note.id,
-    }
-  )
-  const edgeBrowserUrl = getEdgeBrowserUrl(noteContent, {
-    version: 2,
-    preferredEmailInvitationId,
-  })
-  const edgeEditUrl = getEdgeBrowserUrl(noteContent, {
-    editable: true,
-    version: 2,
-    preferredEmailInvitationId,
-  })
-  const { status, error_message: errorMessage } = noteContent
-
-  useEffect(() => {
-    if (!events) return
-    handleConfigNoteUpdate(events.data?.noteId)
-  }, [events?.uniqueId])
-
-  return (
-    <tr>
-      <td>{note.number}</td>
-
-      <td className="assignment-label">
-        <Link href={edgeBrowserUrl} disabled={edgeBrowserUrl ? null : true}>
-          {noteContent.title ? noteContent.title : noteContent.label}
-        </Link>
-      </td>
-
-      <td>{formatDateTime(note.tcdate)}</td>
-
-      <td>{note.tmdate === note.tcdate ? null : formatDateTime(note.tmdate)}</td>
-
-      <td>
-        {['Error', 'No Solution', 'Deployment Error', 'Undeployment Error'].includes(
-          status
-        ) ? (
-          <>
-            <strong>{status}</strong>
-            <br />
-            <a
-              tabIndex="0"
-              role="button"
-              className="assignment-status"
-              data-toggle="popover"
-              data-placement="top"
-              data-trigger="foucus"
-              data-content={errorMessage}
-              title="Error Details"
-            >
-              View Details
-            </a>
-          </>
-        ) : (
-          <span className="assignment-status">{status}</span>
-        )}
-      </td>
-
-      <td>
-        <ActionLink
-          label="View"
-          iconName="info-sign"
-          onClick={() => handleViewConfiguration(note.id, noteContent)}
-          disabled={!configInvitation}
-        />
-        <ActionLink
-          label="Edit"
-          iconName="pencil"
-          onClick={() => handleEditConfiguration(note)}
-          disabled={
-            [
-              'Running',
-              'Complete',
-              'Deploying',
-              'Deployed',
-              'Deployment Error',
-              'Undeploying',
-              'Undeployment Error',
-            ].includes(status) || !configInvitation
-          }
-        />
-        <ActionLink
-          label="Copy"
-          iconName="duplicate"
-          onClick={() => handleCloneConfiguration(note)}
-          disabled={!configInvitation}
-        />
-      </td>
-
-      <td className="assignment-actions">
-        {['Initialized', 'Error', 'No Solution', 'Cancelled'].includes(status) && (
-          <ActionLink
-            label="Run Matcher"
-            iconName="cog"
-            onClick={(e) => {
-              setLoading(true)
-              handleRunMatcher(note.id).then(() => setLoading(false))
-            }}
-            disabled={loading}
-          />
-        )}
-        {['Complete', 'Deploying', 'Deployment Error'].includes(status) && (
-          <>
-            <ActionLink
-              label="Browse Assignments"
-              iconName="eye-open"
-              href={edgeBrowserUrl}
-              disabled={!edgeBrowserUrl}
-            />
-            <ActionLink
-              label="View Statistics"
-              iconName="stats"
-              href={`/assignments/stats?id=${note.id}&referrer=${referrer}`}
-            />
-          </>
-        )}
-        {['Deployed', 'Undeployment Error'].includes(status) && (
-          <>
-            <ActionLink
-              label="Edit Assignments "
-              iconName="random"
-              href={edgeEditUrl}
-              disabled={!edgeEditUrl}
-            />
-            <ActionLink
-              label="View Statistics"
-              iconName="stats"
-              href={`/assignments/stats?id=${note.id}&referrer=${referrer}`}
-            />
-            <ActionLink
-              label="Undeploy Assignment"
-              iconName="share"
-              onClick={() => handleUndeployMatcher(note.id)}
-            />
-          </>
-        )}
-        {['Complete', 'Deployment Error'].includes(status) && shouldShowDeployLink && (
-          <ActionLink
-            label="Deploy Assignment"
-            iconName="share"
-            onClick={() => handleDeployMatcher(note.id)}
-          />
-        )}
-      </td>
-    </tr>
-  )
-}
-
-const pageSize = 25
-
-export default function Assignments({
-  assignmentNotesP,
-  query,
-  configInvitation,
-  accessToken,
-  preferredEmailInvitationId,
-}) {
-  const { notes: allConfigNotes = [], count, errorMessage } = use(assignmentNotesP)
-  if (errorMessage) throw new Error(errorMessage)
-  const [assignmentNotes, setAssignmentNotes] = useState(allConfigNotes.slice(0, pageSize))
-  const [viewModalContent, setViewModalContent] = useState(null)
-  const [editorNote, setEditorNote] = useState(null)
-  const [currentPage, setCurrentPage] = useState(1)
-
-  const shouldShowDeployLink =
-    configInvitation?.content?.multiple_deployments?.value ||
-    !assignmentNotes?.some((p) => p?.content?.status?.value === 'Deployed')
-
-  const handleNewConfiguration = () => {
-    setEditorNote(null)
-    $('#new-note-editor-modal').modal({ backdrop: 'static' })
-  }
-
-  const handleEditConfiguration = (note) => {
-    setEditorNote(note)
-    $('#new-note-editor-modal').modal({ backdrop: 'static' })
-  }
-
-  const handleCloneConfiguration = (note) => {
-    const clone = cloneAssignmentConfigNoteV2(note)
-    setEditorNote(clone)
-    $('#new-note-editor-modal').modal({ backdrop: 'static' })
-  }
-
-  const handleViewConfiguration = (noteId, noteContent) => {
-    if (noteId !== viewModalContent?.id) {
-      setViewModalContent({
-        id: noteId,
-        title: noteContent.title || noteContent.label,
-        content: noteContent,
+    } catch (apiError) {
+      setError({
+        statusCode: 404,
+        message: notFoundMessage,
       })
     }
-
-    $('#note-view-modal').modal('show')
   }
-
-  const handleRunMatcher = async (id) => {
-    try {
-      await api.post('/match', { configNoteId: id }, { accessToken })
-      promptMessage(
-        'Matching started. The status of the assignments will be updated when the matching process is complete'
-      )
-    } catch (apiError) {
-      promptError(apiError.message)
-    }
-  }
-
-  const handleDeployMatcher = async (id) => {
-    try {
-      await api.post('/deploy', { configNoteId: id }, { accessToken })
-      promptMessage('Deployment started.')
-    } catch (apiError) {
-      promptError(apiError.message)
-    }
-  }
-
-  const handleUndeployMatcher = async (id) => {
-    try {
-      await api.post('/undeploy', { configNoteId: id }, { accessToken })
-      promptMessage('Undeployment started.')
-    } catch (apiError) {
-      promptError(apiError.message)
-    }
-  }
-
-  const handleConfigNoteUpdate = async (noteId) => {
-    if (!noteId) return
-    try {
-      const updatedConfigNote = await api.getNoteById(noteId, accessToken)
-      setAssignmentNotes((notes) =>
-        notes.map((note) => (note.id === noteId ? updatedConfigNote : note))
-      )
-    } catch (error) {
-      promptError(error.message)
-    }
-  }
-
   useEffect(() => {
-    setAssignmentNotes(
-      allConfigNotes.slice((currentPage - 1) * pageSize, currentPage * pageSize)
-    )
-  }, [allConfigNotes, currentPage])
+    getConfigInvitation()
+  }, [])
 
-  useEffect(() => {
-    setCurrentPage(1)
-  }, [allConfigNotes])
+  if (error) return <ErrorDisplay statusCode={error.statusCode} message={error.message} />
+  if (!configInvitation) return <LoadingSpinner />
 
+  const banner = referrer ? referrerLink(referrer) : venueHomepageLink(group)
   return (
-    <>
-      <header className="row">
-        <div className="col-xs-12 col-md-9">
-          <h1>{`${prettyId(query?.group)} Assignments`}</h1>
-        </div>
-        <div className="col-xs-12 col-md-3 text-right">
-          <button
-            type="button"
-            className="btn"
-            disabled={!configInvitation}
-            onClick={handleNewConfiguration}
-          >
-            New Assignment Configuration
-          </button>
-        </div>
+    <CommonLayout banner={<Banner>{banner}</Banner>}>
+      <header>
+        <h1>Activity</h1>
       </header>
-      <div className="row">
-        <div className="col-xs-12 horizontal-scroll">
-          <Table
-            className="table-hover assignments-table"
-            headings={[
-              { content: '#', width: '40px' },
-              { content: 'Title' },
-              { content: 'Created On', width: '185px' },
-              { content: 'Last Modified', width: '185px' },
-              { content: 'Status', width: '110px' },
-              { content: 'Parameters', width: '110px' },
-              { content: 'Actions', width: '175px' },
-            ]}
-          >
-            {assignmentNotes.map((assignmentNote) => (
-              <AssignmentRow
-                key={assignmentNote.id}
-                note={assignmentNote}
-                configInvitation={configInvitation}
-                handleEditConfiguration={handleEditConfiguration}
-                handleViewConfiguration={handleViewConfiguration}
-                handleCloneConfiguration={handleCloneConfiguration}
-                handleRunMatcher={handleRunMatcher}
-                handleDeployMatcher={handleDeployMatcher}
-                handleUndeployMatcher={handleUndeployMatcher}
-                referrer={encodeURIComponent(
-                  `[all assignments for ${prettyId(query?.group)}](/assignments?group=${
-                    query?.group
-                  })`
-                )}
-                shouldShowDeployLink={shouldShowDeployLink}
-                preferredEmailInvitationId={preferredEmailInvitationId}
-                handleConfigNoteUpdate={handleConfigNoteUpdate}
-              />
-            ))}
-          </Table>
-          {count === 0 && (
-            <p className="empty-message">
-              No assignments have been generated for this venue. Click the New Assignment
-              button above to get started.
-            </p>
-          )}
-
-          <PaginationLinks
-            setCurrentPage={setCurrentPage}
-            totalCount={count}
-            itemsPerPage={pageSize}
-            currentPage={currentPage}
-          />
-        </div>
-      </div>
-      <BasicModal
-        id="note-view-modal"
-        title={viewModalContent?.title}
-        cancelButtonText="Done"
-        primaryButtonText={null}
-      >
-        {viewModalContent ? (
-          <NoteContent
-            id={viewModalContent.id}
-            content={viewModalContent.content}
-            invitation={configInvitation}
+      <div className={styles.assignments}>
+        {configInvitation.apiVersion === 2 ? (
+          <V2Assignments
+            assignmentNotesP={assignmentNotesP}
+            configInvitation={configInvitation}
+            accessToken={accessToken}
+            preferredEmailInvitationId={preferredEmailInvitationId}
           />
         ) : (
-          <LoadingSpinner inline />
+          <V1Assignments
+            configInvitation={configInvitation}
+            // query={query}
+            accessToken={accessToken}
+          />
         )}
-      </BasicModal>
-      <NewNoteEditorModal
-        editorNote={editorNote}
-        configInvitation={configInvitation}
-        setEditorNote={setEditorNote}
-        assignmentNotes={assignmentNotes}
-      />
-    </>
+      </div>
+    </CommonLayout>
   )
 }
--- app/assignments/page.js
+++ app/assignments/page.js
@@ -1,20 +1,9 @@
 import { redirect } from 'next/navigation'
 import { stringify } from 'query-string'
-import { Suspense } from 'react'
-import { headers } from 'next/headers'
+import Assignments from './Assignments'
 import { prettyId } from '../../lib/utils'
 import serverAuth from '../auth'
-import CommonLayout from '../CommonLayout'
-import Banner from '../../components/Banner'
-import { referrerLink, venueHomepageLink } from '../../lib/banner-links'
-import api from '../../lib/api-client'
-import Assignments from './Assignments'
-import LoadingSpinner from '../../components/LoadingSpinner'
-import V1Assignments from './V1Assignments'
-import styles from './Assignments.module.scss'
 import ErrorDisplay from '../../components/ErrorDisplay'
-
-export const dynamic = 'force-dynamic'
 
 export async function generateMetadata({ searchParams }) {
   const { group } = await searchParams
@@ -26,101 +15,11 @@ export async function generateMetadata({ searchParams }) {
 
 export default async function page({ searchParams }) {
   const query = await searchParams
-  const { group, referrer } = query
+  const { group } = query
   const { token: accessToken, user } = await serverAuth()
   if (!accessToken) redirect(`/login?redirect=/assignments?${stringify(query)}`)
   if (!group)
     return <ErrorDisplay message="Could not list assignments. Missing parameter group." />
 
-  const banner = referrer ? referrerLink(referrer) : venueHomepageLink(group)
-
-  const notFoundMessage =
-    'There is currently no assignment configuration ready for use. Please go to your venue request form and use the Paper Matching Setup to compute conflicts and/or affinity scores.'
-
-  let configInvitation = null
-  let preferredEmailInvitationId = null
-  const headersList = await headers()
-  const remoteIpAddress = headersList.get('x-forwarded-for')
-  try {
-    configInvitation = await api.getInvitationById(
-      `${group}/-/Assignment_Configuration`,
-      accessToken,
-      null,
-      null,
-      remoteIpAddress
-    )
-    const domainGroup = await api.getGroupById(configInvitation.domain, accessToken)
-    preferredEmailInvitationId = domainGroup?.content?.preferred_emails_id?.value
-  } catch (error) {
-    console.log('Error in configInvitation', {
-      page: 'assignments',
-      user: user?.id,
-      apiError: error,
-      apiRequest: {
-        endpoint: 'getInvitationById',
-        params: { id: `${group}/-/Assignment_Configuration` },
-      },
-    })
-    return <ErrorDisplay message={notFoundMessage} />
-  }
-  if (!configInvitation) {
-    console.log('Error in page', {
-      page: 'assignments',
-      user: user?.id,
-      apiError: 'No config invitation',
-    })
-    return <ErrorDisplay message={notFoundMessage} />
-  }
-
-  const assignmentNotesP =
-    configInvitation.apiVersion === 2
-      ? api
-          .get(
-            '/notes',
-            {
-              invitation: `${query.group}/-/Assignment_Configuration`,
-            },
-            { accessToken, version: 2, remoteIpAddress }
-          )
-          .then((response) => {
-            const { notes, count } = response
-            return { notes, count }
-          })
-          .catch((error) => {
-            console.log('Error in assignmentNotesP', {
-              page: 'assignments',
-              user: user?.id,
-              apiError: error,
-              apiRequest: {
-                endpoint: '/notes',
-                params: { invitation: `${query.group}/-/Assignment_Configuration` },
-              },
-            })
-            return { errorMessage: error.message }
-          })
-      : Promise.resolve(null)
-
-  return (
-    <CommonLayout banner={<Banner>{banner}</Banner>}>
-      <Suspense fallback={<LoadingSpinner />}>
-        <div className={styles.assignments}>
-          {configInvitation.apiVersion === 2 ? (
-            <Assignments
-              assignmentNotesP={assignmentNotesP}
-              query={query}
-              configInvitation={configInvitation}
-              accessToken={accessToken}
-              preferredEmailInvitationId={preferredEmailInvitationId}
-            />
-          ) : (
-            <V1Assignments
-              configInvitation={configInvitation}
-              query={query}
-              accessToken={accessToken}
-            />
-          )}
-        </div>
-      </Suspense>
-    </CommonLayout>
-  )
+  return <Assignments accessToken={accessToken} />
 }
