1ea655999094004f9e3e2d9a5a368d4c9ae23291
xkopenreview <xukun@openreview.net>
2025-03-31 17:22:24 -0400

    refactor stats page

--- app/assignments/stats/Stats.js
+++ app/assignments/stats/Stats.js
@@ -1,24 +1,46 @@
 'use client'
 
+/* globals promptError: false */
 import Link from 'next/link'
-import { use } from 'react'
+import { useEffect, useState } from 'react'
+import { get, upperFirst } from 'lodash'
 import { getEdgeBrowserUrl } from '../../../lib/edge-utils'
 import ScalarStat from '../../../components/assignments/ScalarStat'
 import HistogramStat from '../../../components/assignments/HistogramStat'
-import { prettyId } from '../../../lib/utils'
+import { getSingularRoleName, prettyId } from '../../../lib/utils'
+import { getNoteContentValues } from '../../../lib/forum-utils'
+import api from '../../../lib/api-client'
+import {
+  getAssignmentMap,
+  getDistributionPapersByUserCount,
+  getMeanFinalScore,
+  getMeanGroupCountPerPaper,
+  getMeanPaperCountPerGroup,
+  getPaperCount,
+  getUnassignedPapersList,
+  getUnassignedUsersList,
+  getUserCount,
+  getDistributionUsersByPaperCount,
+  getDistributionAssignmentByScore,
+  getDistributionPapersByMeanScore,
+  getDistributionUsersByMeanScore,
+  getDistributionRecomGroupCountPerPaper,
+  getDistributionRecomGroupCountPerWeight,
+  getNumDataPerGroupDataByBidScore,
+} from '../../../lib/assignment-stats-utils'
 
-export default function Stats({ valuesP, assignmentConfigNoteContent, names, configNoteId }) {
-  const { values, errorMessage } = use(valuesP)
-  if (errorMessage) throw new Error(errorMessage)
+export default function Stats({ configNote, accessToken }) {
+  const [labelNames, setLabelNames] = useState({})
+  const [values, setValues] = useState({})
+  const assignmentConfigNoteContent = getNoteContentValues(configNote.content)
 
-  const {
-    headName,
-    tailName,
-    upperHeadName,
-    upperTailName,
-    upperSingularHeadName,
-    upperSingularTailName,
-  } = names
+  const headName = labelNames.headName ?? 'papers'
+  const tailName = labelNames.tailName ?? 'users'
+  const upperHeadName = upperFirst(headName)
+  const upperTailName = upperFirst(tailName)
+  const upperSingularHeadName = getSingularRoleName(upperHeadName)
+  const upperSingularTailName = getSingularRoleName(upperTailName)
+
   const edgeBrowserUrlParams = {
     browseInvitations: Object.keys(assignmentConfigNoteContent.scores_specification ?? {}),
     editInvitation:
@@ -35,8 +57,198 @@ export default function Stats({ valuesP, assignmentConfigNoteContent, names, con
     assignmentLabel: encodeURIComponent(assignmentConfigNoteContent.title),
     referrerText: `${prettyId(assignmentConfigNoteContent.title)} Statistics`,
     apiVersion: 2,
-    configNoteId,
+    configNoteId: configNote.id,
   }
+
+  const getValues = async () => {
+    try {
+      const assignmentInvitationId = configNote.content?.assignment_invitation?.value
+      const assignmentInvitation = assignmentInvitationId
+        ? await api.getInvitationById(assignmentInvitationId, accessToken)
+        : Promise.resolve(null)
+      const headNameInAssignmentInvitation = prettyId(
+        assignmentInvitation?.edge?.head?.param?.inGroup?.split('/').pop()
+      )
+      const tailNameInAssignmentInvitation = prettyId(
+        assignmentInvitation?.edge?.tail?.param?.options?.group?.split('/').pop()
+      )
+
+      if (headNameInAssignmentInvitation && tailNameInAssignmentInvitation) {
+        setLabelNames({
+          headName: headNameInAssignmentInvitation,
+          tailName: tailNameInAssignmentInvitation,
+        })
+      }
+
+      const paperInvitationElements = assignmentConfigNoteContent.paper_invitation.split('&')
+
+      let papersP
+      const indexedContentFields = [
+        'content.authorids',
+        'content.venueid',
+        'content.venue',
+        'content.user',
+      ]
+      if (paperInvitationElements[0].includes('/-/')) {
+        const getNotesArgs = {
+          invitation: paperInvitationElements[0],
+        }
+        const localFilterContentFields = {}
+        paperInvitationElements.slice(1).forEach((filter) => {
+          const filterElements = filter.split('=')
+          if (indexedContentFields.includes(filterElements[0])) {
+            getNotesArgs[filterElements[0]] = filterElements[1]
+          } else {
+            localFilterContentFields[filterElements[0]] = filterElements[1]
+          }
+        })
+        papersP = api
+          .getAll('/notes', getNotesArgs, { accessToken, version: 2 })
+          .then((results) =>
+            results.filter((p) =>
+              Object.keys(localFilterContentFields).every(
+                (key) => get(p, `${key}.value`) === localFilterContentFields[key]
+              )
+            )
+          )
+      } else {
+        papersP = api.get('/groups', { id: paperInvitationElements[0] }, { accessToken })
+      }
+
+      const usersP = api.get(
+        '/groups',
+        { id: assignmentConfigNoteContent.match_group },
+        { accessToken }
+      )
+
+      const queryParams =
+        assignmentConfigNoteContent.status === 'Deployed' &&
+        assignmentConfigNoteContent.deployed_assignment_invitation
+          ? {
+              invitation: assignmentConfigNoteContent.deployed_assignment_invitation,
+              groupBy: 'head',
+              select: 'tail,weight',
+            }
+          : {
+              invitation: assignmentConfigNoteContent.assignment_invitation,
+              label: assignmentConfigNoteContent.title,
+              groupBy: 'head',
+              select: 'tail,weight',
+            }
+
+      const assignmentsP = api.getAll('/edges', queryParams, {
+        accessToken,
+        resultsKey: 'groupedEdges',
+      })
+
+      const bidInvitation = Object.keys(
+        assignmentConfigNoteContent.scores_specification ?? {}
+      ).find((p) => p.endsWith('Bid'))
+
+      const bidsP = bidInvitation
+        ? api.getAll(
+            '/edges',
+            { invitation: bidInvitation, groupBy: 'head', select: 'tail,label' },
+            { accessToken, resultsKey: 'groupedEdges' }
+          )
+        : Promise.resolve([])
+
+      const recommendationInvitation = Object.keys(
+        assignmentConfigNoteContent.scores_specification ?? {}
+      ).find((p) => p.endsWith('Recommendation'))
+
+      const recommendationsP = recommendationInvitation
+        ? api.getAll(
+            '/edges',
+            { invitation: recommendationInvitation, groupBy: 'head', select: 'tail,weight' },
+            { accessToken, resultsKey: 'groupedEdges' }
+          )
+        : Promise.resolve([])
+
+      await Promise.all([papersP, usersP, assignmentsP, bidsP, recommendationsP]).then(
+        (results) => {
+          let papers = []
+          if (paperInvitationElements[0].includes('/-/')) {
+            papers = results[0]
+          } else {
+            papers = results[0]?.groups?.[0]?.members.map((p) => ({ id: p }))
+          }
+
+          const users = results[1]?.groups?.[0]?.members
+          const assignments = results[2]
+          const bids = results[3]
+          const recommendations = results[4]
+
+          const { assignmentMap, usersWithAssignments, papersWithAssignments } =
+            getAssignmentMap(assignments, bids, recommendations, papers, users)
+
+          const unassignedPapersList = getUnassignedPapersList(papers, papersWithAssignments)
+          const unassignedUsersList = getUnassignedUsersList(users, usersWithAssignments)
+
+          const matchLists = [assignmentMap, unassignedPapersList, unassignedUsersList]
+
+          const showRecommendationDistribution =
+            matchLists[0]
+              .map((p) => p?.otherScores?.recommendation)
+              .filter(Number.isFinite)
+              .reduce((a, b) => a + b, 0) > 0
+          setValues({
+            paperCount: getPaperCount(matchLists[0], matchLists[1]),
+            userCount: getUserCount(matchLists[0], matchLists[2]),
+            meanFinalScore: getMeanFinalScore(matchLists[0]),
+            meanGroupCountPerPaper: getMeanGroupCountPerPaper(matchLists[0]),
+            meanPaperCountPerGroup: getMeanPaperCountPerGroup(matchLists[0]),
+            distributionPapersByUserCount: getDistributionPapersByUserCount(
+              matchLists[0],
+              matchLists[1],
+              upperHeadName,
+              upperTailName
+            ),
+            distributionUsersByPaperCount: getDistributionUsersByPaperCount(
+              matchLists[0],
+              matchLists[2],
+              upperHeadName,
+              upperTailName
+            ),
+            distributionAssignmentByScore: getDistributionAssignmentByScore(matchLists[0]),
+            distributionPapersByMeanScore: getDistributionPapersByMeanScore(
+              matchLists[0],
+              matchLists[1],
+              upperHeadName
+            ),
+            distributionUsersByMeanScore: getDistributionUsersByMeanScore(
+              matchLists[0],
+              matchLists[2],
+              upperTailName
+            ),
+            ...(showRecommendationDistribution && {
+              distributionRecomGroupCountPerPaper: getDistributionRecomGroupCountPerPaper(
+                matchLists[0],
+                upperHeadName,
+                upperSingularHeadName
+              ),
+              distributionRecomGroupCountPerWeight: getDistributionRecomGroupCountPerWeight(
+                matchLists[0]
+              ),
+            }),
+            ...getNumDataPerGroupDataByBidScore(
+              matchLists[0],
+              upperHeadName,
+              upperTailName,
+              upperSingularTailName
+            ),
+          })
+        }
+      )
+    } catch (error) {
+      promptError(error.message)
+    }
+  }
+
+  useEffect(() => {
+    getValues()
+  }, [configNote])
+
   return (
     <>
       <div className="header">
--- app/assignments/stats/page.js
+++ app/assignments/stats/page.js
@@ -1,40 +1,19 @@
 import { stringify } from 'query-string'
 import { redirect } from 'next/navigation'
-import { get, upperFirst } from 'lodash'
 import { Suspense } from 'react'
 import { headers } from 'next/headers'
 import serverAuth from '../../auth'
 import api from '../../../lib/api-client'
 import CommonLayout from '../../CommonLayout'
 import { referrerLink } from '../../../lib/banner-links'
-import { getGroupIdfromInvitation, getSingularRoleName, prettyId } from '../../../lib/utils'
+import { getGroupIdfromInvitation, prettyId } from '../../../lib/utils'
 import Banner from '../../../components/Banner'
 import V1Stats from './V1Stats'
 import styles from './Stats.module.scss'
 import { getNoteContentValues } from '../../../lib/forum-utils'
-import {
-  getAssignmentMap,
-  getDistributionPapersByUserCount,
-  getMeanFinalScore,
-  getMeanGroupCountPerPaper,
-  getMeanPaperCountPerGroup,
-  getPaperCount,
-  getUnassignedPapersList,
-  getUnassignedUsersList,
-  getUserCount,
-  getDistributionUsersByPaperCount,
-  getDistributionAssignmentByScore,
-  getDistributionPapersByMeanScore,
-  getDistributionUsersByMeanScore,
-  getDistributionRecomGroupCountPerPaper,
-  getDistributionRecomGroupCountPerWeight,
-  getNumDataPerGroupDataByBidScore,
-} from '../../../lib/assignment-stats-utils'
 import Stats from './Stats'
 import LoadingSpinner from '../../../components/LoadingSpinner'
 import ErrorDisplay from '../../../components/ErrorDisplay'
-
-export const dynamic = 'force-dynamic'
 
 export async function generateMetadata({ searchParams }) {
   const { id } = await searchParams
@@ -66,7 +45,12 @@ export default async function page({ searchParams }) {
 
   const headersList = await headers()
   const remoteIpAddress = headersList.get('x-forwarded-for')
-  const note = await api.getNoteById(id, accessToken, null, null, remoteIpAddress)
+  let note
+  try {
+    note = await api.getNoteById(id, accessToken, null, null, remoteIpAddress)
+  } catch (error) {
+    /* empty */
+  }
   if (!note) {
     console.log('Error in getNoteById', {
       page: 'assignments/stats',
@@ -94,232 +78,13 @@ export default async function page({ searchParams }) {
       </CommonLayout>
     )
 
-  let labelNames = {}
-  const assignmentInvitationId = note.content?.assignment_invitation?.value
-  const assignmentInvitation = assignmentInvitationId
-    ? await api.getInvitationById(
-        assignmentInvitationId,
-        accessToken,
-        null,
-        null,
-        remoteIpAddress
-      )
-    : Promise.resolve(null)
-  const headNameInAssignmentInvitation = prettyId(
-    assignmentInvitation?.edge?.head?.param?.inGroup?.split('/').pop()
-  )
-  const tailNameInAssignmentInvitation = prettyId(
-    assignmentInvitation?.edge?.tail?.param?.options?.group?.split('/').pop()
-  )
-
-  if (headNameInAssignmentInvitation && tailNameInAssignmentInvitation) {
-    labelNames = {
-      headName: headNameInAssignmentInvitation,
-      tailName: tailNameInAssignmentInvitation,
-    }
-  }
-
-  const headName = labelNames.headName ?? 'papers'
-  const tailName = labelNames.tailName ?? 'users'
-  const upperHeadName = upperFirst(headName)
-  const upperTailName = upperFirst(tailName)
-  const upperSingularHeadName = getSingularRoleName(upperHeadName)
-  const upperSingularTailName = getSingularRoleName(upperTailName)
-
-  const indexedContentFields = [
-    'content.authorids',
-    'content.venueid',
-    'content.venue',
-    'content.user',
-  ]
   const noteContent = getNoteContentValues(note.content)
-
-  const paperInvitationElements = noteContent.paper_invitation.split('&')
-  let papersP
-  if (paperInvitationElements[0].includes('/-/')) {
-    const getNotesArgs = {
-      invitation: paperInvitationElements[0],
-    }
-    const localFilterContentFields = {}
-    paperInvitationElements.slice(1).forEach((filter) => {
-      const filterElements = filter.split('=')
-      if (indexedContentFields.includes(filterElements[0])) {
-        getNotesArgs[filterElements[0]] = filterElements[1]
-      } else {
-        localFilterContentFields[filterElements[0]] = filterElements[1]
-      }
-    })
-    papersP = api
-      .getAll('/notes', getNotesArgs, { accessToken, version: 2, remoteIpAddress })
-      .then((results) =>
-        results.filter((p) =>
-          Object.keys(localFilterContentFields).every(
-            (key) => get(p, `${key}.value`) === localFilterContentFields[key]
-          )
-        )
-      )
-  } else {
-    papersP = api.get(
-      '/groups',
-      { id: paperInvitationElements[0] },
-      { accessToken, remoteIpAddress }
-    )
-  }
-
-  const usersP = api.get(
-    '/groups',
-    { id: noteContent.match_group },
-    { accessToken, remoteIpAddress }
-  )
-
-  const queryParams =
-    noteContent.status === 'Deployed' && noteContent.deployed_assignment_invitation
-      ? {
-          invitation: noteContent.deployed_assignment_invitation,
-          groupBy: 'head',
-          select: 'tail,weight',
-        }
-      : {
-          invitation: noteContent.assignment_invitation,
-          label: noteContent.title,
-          groupBy: 'head',
-          select: 'tail,weight',
-        }
-  const assignmentsP = api.getAll('/edges', queryParams, {
-    accessToken,
-    resultsKey: 'groupedEdges',
-    remoteIpAddress,
-  })
-
-  const bidInvitation = Object.keys(noteContent.scores_specification ?? {}).find((p) =>
-    p.endsWith('Bid')
-  )
-  const bidsP = bidInvitation
-    ? api.getAll(
-        '/edges',
-        { invitation: bidInvitation, groupBy: 'head', select: 'tail,label' },
-        { accessToken, resultsKey: 'groupedEdges', remoteIpAddress }
-      )
-    : Promise.resolve([])
-
-  const recommendationInvitation = Object.keys(noteContent.scores_specification ?? {}).find(
-    (p) => p.endsWith('Recommendation')
-  )
-  const recommendationsP = recommendationInvitation
-    ? api.getAll(
-        '/edges',
-        { invitation: recommendationInvitation, groupBy: 'head', select: 'tail,weight' },
-        { accessToken, resultsKey: 'groupedEdges', remoteIpAddress }
-      )
-    : Promise.resolve([])
-
-  const valuesP = Promise.all([papersP, usersP, assignmentsP, bidsP, recommendationsP])
-    .then((results) => {
-      let papers = []
-      if (paperInvitationElements[0].includes('/-/')) {
-        papers = results[0]
-      } else {
-        papers = results[0]?.groups?.[0]?.members.map((p) => ({ id: p }))
-      }
-
-      const users = results[1]?.groups?.[0]?.members
-      const assignments = results[2]
-      const bids = results[3]
-      const recommendations = results[4]
-
-      const { assignmentMap, usersWithAssignments, papersWithAssignments } = getAssignmentMap(
-        assignments,
-        bids,
-        recommendations,
-        papers,
-        users
-      )
-
-      const unassignedPapersList = getUnassignedPapersList(papers, papersWithAssignments)
-      const unassignedUsersList = getUnassignedUsersList(users, usersWithAssignments)
-
-      const matchLists = [assignmentMap, unassignedPapersList, unassignedUsersList]
-
-      const showRecommendationDistribution =
-        matchLists[0]
-          .map((p) => p?.otherScores?.recommendation)
-          .filter(Number.isFinite)
-          .reduce((a, b) => a + b, 0) > 0
-      return {
-        values: {
-          paperCount: getPaperCount(matchLists[0], matchLists[1]),
-          userCount: getUserCount(matchLists[0], matchLists[2]),
-          meanFinalScore: getMeanFinalScore(matchLists[0]),
-          meanGroupCountPerPaper: getMeanGroupCountPerPaper(matchLists[0]),
-          meanPaperCountPerGroup: getMeanPaperCountPerGroup(matchLists[0]),
-          distributionPapersByUserCount: getDistributionPapersByUserCount(
-            matchLists[0],
-            matchLists[1],
-            upperHeadName,
-            upperTailName
-          ),
-          distributionUsersByPaperCount: getDistributionUsersByPaperCount(
-            matchLists[0],
-            matchLists[2],
-            upperHeadName,
-            upperTailName
-          ),
-          distributionAssignmentByScore: getDistributionAssignmentByScore(matchLists[0]),
-          distributionPapersByMeanScore: getDistributionPapersByMeanScore(
-            matchLists[0],
-            matchLists[1],
-            upperHeadName
-          ),
-          distributionUsersByMeanScore: getDistributionUsersByMeanScore(
-            matchLists[0],
-            matchLists[2],
-            upperTailName
-          ),
-          ...(showRecommendationDistribution && {
-            distributionRecomGroupCountPerPaper: getDistributionRecomGroupCountPerPaper(
-              matchLists[0],
-              upperHeadName,
-              upperSingularHeadName
-            ),
-            distributionRecomGroupCountPerWeight: getDistributionRecomGroupCountPerWeight(
-              matchLists[0]
-            ),
-          }),
-          ...getNumDataPerGroupDataByBidScore(
-            matchLists[0],
-            upperHeadName,
-            upperTailName,
-            upperSingularTailName
-          ),
-        },
-      }
-    })
-    .catch((error) => {
-      console.log('Error in valuesP', {
-        page: 'assignments/stats',
-        user: user?.id,
-        apiError: error,
-      })
-      return { errorMessage: error.message }
-    })
 
   return (
     <CommonLayout banner={<Banner>{banner}</Banner>}>
       <div className={styles.stats}>
         <Suspense fallback={<LoadingSpinner />}>
-          <Stats
-            valuesP={valuesP}
-            assignmentConfigNoteContent={noteContent}
-            names={{
-              headName,
-              tailName,
-              upperHeadName,
-              upperTailName,
-              upperSingularHeadName,
-              upperSingularTailName,
-            }}
-            configNoteId={note.id}
-          />
+          <Stats configNote={note} accessToken={accessToken} />
         </Suspense>
       </div>
     </CommonLayout>
