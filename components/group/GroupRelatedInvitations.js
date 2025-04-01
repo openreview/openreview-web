7a1fc41e522ab8fe2318b0e1b03c4256d5fe985b
xkopenreview <xukun@openreview.net>
2025-04-01 15:07:47 -0400

    some changes based on feedback

--- components/group/GroupRelatedInvitations.js
+++ components/group/GroupRelatedInvitations.js
@@ -6,7 +6,12 @@ import api from '../../lib/api-client'
 import { prettyId } from '../../lib/utils'
 
 const RelatedInvitationRow = ({ item }) => (
-  <Link href={`/invitation/edit?id=${item.id}`}>{prettyId(item.id)}</Link>
+  <Link
+    href={`/invitation/edit?id=${item.id}`}
+    className={item.ddate ? 'invitation-deleted' : ''}
+  >
+    {prettyId(item.id)}
+  </Link>
 )
 
 const GroupRelatedInvitations = ({ group, accessToken }) => {
