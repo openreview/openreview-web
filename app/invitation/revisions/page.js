ecc61fa6888aba53d558983fb73e435ed86d8e21
xkopenreview <xukun@openreview.net>
2025-04-09 14:16:57 -0400

    invitation revisions to client

--- app/invitation/revisions/page.js
+++ app/invitation/revisions/page.js
@@ -1,14 +1,6 @@
-import { stringify } from 'query-string'
-import { redirect } from 'next/navigation'
-import { Suspense } from 'react'
-import { headers } from 'next/headers'
-import api from '../../../lib/api-client'
 import { prettyId } from '../../../lib/utils'
-import serverAuth, { isSuperUser } from '../../auth'
-import LoadingSpinner from '../../../components/LoadingSpinner'
 import InvitationRevisions from './InvitationRevisions'
-
-export const dynamic = 'force-dynamic'
+import ErrorDisplay from '../../../components/ErrorDisplay'
 
 export async function generateMetadata({ searchParams }) {
   const { id } = await searchParams
@@ -20,63 +12,9 @@ export async function generateMetadata({ searchParams }) {
 
 export default async function page({ searchParams }) {
   const query = await searchParams
-  const { user, token: accessToken } = await serverAuth()
   const { id } = query
 
-  const headersList = await headers()
-  const remoteIpAddress = headersList.get('x-forwarded-for')
+  if (!id) return <ErrorDisplay message="Missing required parameter id" />
 
-  let redirectPath = null
-  const loadInvitationP = api
-    .get('/invitations', { id }, { accessToken, remoteIpAddress })
-    .then((apiRes) => {
-      if (apiRes.invitations?.length > 0) {
-        if (apiRes.invitations[0].details?.writable) {
-          const invitation = apiRes.invitations[0]
-          return { invitation }
-        }
-        if (!accessToken) {
-          redirectPath = `/login?redirect=/invitation/revisions?${encodeURIComponent(stringify(query))}`
-        } else {
-          redirectPath = `/invitation/info?id=${id}`
-        }
-      } else {
-        throw new Error('Invitation not found')
-      }
-      return null
-    })
-    .catch((error) => {
-      console.log('Error in get loadInvitationP', {
-        page: 'invitation/revisions',
-        user: user?.id,
-        apiError: error,
-        apiRequest: {
-          endpoint: '/invitations',
-          params: { id },
-        },
-      })
-      if (error.name === 'ForbiddenError') {
-        if (!accessToken) {
-          redirectPath = `/login?redirect=/invitation/revisions?${encodeURIComponent(stringify(query))}`
-        } else {
-          return { errorMessage: "You don't have permission to read this invitation" }
-        }
-      }
-      return { errorMessage: error.message }
-    })
-    .finally(() => {
-      if (redirectPath) {
-        redirect(redirectPath)
-      }
-    })
-
-  return (
-    <Suspense fallback={<LoadingSpinner />}>
-      <InvitationRevisions
-        loadInvitationP={loadInvitationP}
-        accessToken={accessToken}
-        isSuperUser={isSuperUser(user)}
-      />
-    </Suspense>
-  )
+  return <InvitationRevisions id={id} query={query} />
 }
