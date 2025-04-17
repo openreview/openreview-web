43cb1a590f7f03b15afbc5dd4dcd63b38fefbe41
xkopenreview <xukun@openreview.net>
2025-04-17 10:57:41 -0400

    fix test

--- tests/registerPage.ts
+++ tests/registerPage.ts
@@ -41,6 +41,7 @@ const claimProfileButtonSelector = Selector('button').withText('Claim Profile')
 const messageSelector = Selector('span').withAttribute('class', 'important_message')
 const messagePanelSelector = Selector('#flash-message-container')
 const nextSectiomButtonSelector = Selector('button').withText('Next Section')
+const errorMessageLabel = Selector('.error-message')
 
 fixture`Signup`.page`http://localhost:${process.env.NEXT_PORT}/signup`.before(async (ctx) => {
   ctx.superUserToken = await getToken(superUserName, strongPassword)
@@ -540,18 +541,18 @@ fixture`Activate with errors`
 test('try to activate a profile with no token and get an error', async (t) => {
   await t
     .navigateTo(`http://localhost:${process.env.NEXT_PORT}/profile/activate`)
-    .expect(messagePanelSelector.exists)
+    .expect(errorMessageLabel.exists)
     .ok()
-    .expect(messageSelector.innerText)
+    .expect(errorMessageLabel.innerText)
     .eql('Invalid profile activation link. Please check your email and try again.')
 }).skipJsErrors()
 
 test('try to activate a profile with empty token and get an error', async (t) => {
   await t
     .navigateTo(`http://localhost:${process.env.NEXT_PORT}/profile/activate?token=`)
-    .expect(messagePanelSelector.exists)
+    .expect(errorMessageLabel.exists)
     .ok()
-    .expect(messageSelector.innerText)
+    .expect(errorMessageLabel.innerText)
     .eql('Invalid profile activation link. Please check your email and try again.')
 }).skipJsErrors()
 
