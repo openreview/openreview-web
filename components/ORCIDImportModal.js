be1c8018338b53523124211fe2f246371177a9a5
xkopenreview <xukun@openreview.net>
2025-05-19 12:17:05 -0400

    wip

--- components/ORCIDImportModal.js
+++ components/ORCIDImportModal.js
@@ -1,12 +1,16 @@
 /* globals $: false */
-import { useEffect, useRef, useState } from 'react'
+import { useContext, useEffect, useRef, useState } from 'react'
 import { nanoid } from 'nanoid'
 import {
+  getAllOrcidPapers,
   getAllPapersByGroupId,
   getOrcidPublicationsFromJsonUrl,
   getOrcidPublicationsFromXmlUrl,
 } from '../lib/profiles'
 import DblpPublicationTable from './DblpPublicationTable'
+import LoadingSpinner from './LoadingSpinner'
+import { inflect } from '../lib/utils'
+import UserContext from './UserContext'
 
 const ORCIDImportModal = ({ profileId, profileNames }) => {
   const modalEl = useRef(null)
@@ -15,16 +19,42 @@ const ORCIDImportModal = ({ profileId, profileNames }) => {
   const [publications, setPublications] = useState([])
   const [publicationsInOpenReview, setPublicationsInOpenReview] = useState([])
   const [selectedPublications, setSelectedPublications] = useState([])
+  const [isFetchingPublications, setIsFetchingPublications] = useState(false)
+  const [hasError, setHasError] = useState(false)
+  const { accessToken } = useContext(UserContext)
+  const maxNumberofPublicationsToImport = 500
 
   const fetchNewPublications = async (orcid) => {
+    setIsFetchingPublications(true)
+    setHasError(false)
     try {
-      setPublicationsInOpenReview(await getAllPapersByGroupId(profileId))
+      // setPublicationsInOpenReview(await getAllPapersByGroupId(profileId))
       const fetchedPublications = await getOrcidPublicationsFromJsonUrl(orcid, profileNames)
       setPublications(fetchedPublications.map((p) => ({ ...p, key: nanoid() })))
       setMessage(`${fetchedPublications.length} publications fetched.`)
+      // get existing orcid publications
+      setPublicationsInOpenReview(await getAllOrcidPapers(profileId, accessToken))
+      // get orcid publications imported by other profiles
+    } catch (error) {
+      setMessage(error.message)
+      setHasError(true)
+    }
+    setIsFetchingPublications(false)
+  }
+
+  const importSelectedPublications = async () => {
+    setIsFetchingPublications(true)
+    try {
+      console.log('Selected publications:', selectedPublications)
+      console.log(
+        'Selected publications IDs:',
+        publications.find((p) => p.key === selectedPublications[0])
+      )
     } catch (error) {
       setMessage(error.message)
     }
+    setIsSavingPublications(false)
+    setSelectedPublications([])
   }
 
   useEffect(() => {
@@ -57,17 +87,56 @@ const ORCIDImportModal = ({ profileId, profileNames }) => {
 
           <div className="modal-body">
             {message && <p>{message}</p>}
-            <DblpPublicationTable
-              dblpPublications={publications}
-              orPublications={publicationsInOpenReview}
-              orPublicationsImportedByOtherProfile={[]}
-              selectedPublications={selectedPublications}
-              setSelectedPublications={setSelectedPublications}
-              // maxNumberofPublicationsToImport={maxNumberofPublicationsToImport}
-            />
+            {isFetchingPublications ? (
+              <LoadingSpinner inline />
+            ) : (
+              <DblpPublicationTable
+                dblpPublications={publications}
+                orPublications={publicationsInOpenReview}
+                orPublicationsImportedByOtherProfile={[]}
+                selectedPublications={selectedPublications}
+                setSelectedPublications={setSelectedPublications}
+                maxNumberofPublicationsToImport={maxNumberofPublicationsToImport}
+              />
+            )}
           </div>
 
-          <div className="modal-footer"></div>
+          <div className="modal-footer">
+            <div className="pull-left selected-count">
+              {selectedPublications.length !== 0 && (
+                <strong>
+                  {inflect(selectedPublications.length, 'publication', 'publications', true)}{' '}
+                  selected{' '}
+                  {selectedPublications.length === maxNumberofPublicationsToImport &&
+                    '(max allowed)'}
+                </strong>
+              )}
+            </div>
+            <button
+              type="button"
+              className="btn btn-default"
+              data-dismiss="modal"
+              disabled={isSavingPublications}
+            >
+              Cancel
+            </button>
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={importSelectedPublications}
+              disabled={!selectedPublications.length || isSavingPublications || hasError}
+            >
+              Add to Your Profile
+              {isSavingPublications && (
+                <div className="spinner-small">
+                  <div className="rect1" />
+                  <div className="rect2" />
+                  <div className="rect3" />
+                  <div className="rect4" />
+                </div>
+              )}
+            </button>
+          </div>
         </div>
       </div>
     </div>
