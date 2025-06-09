875b8aebd5327e469c35de713e1a646cae28d1e9
xkopenreview <xukun@openreview.net>
2025-06-09 15:44:55 -0400

    profile tag on top of nextjs15

---
+++ components/CheckableTag.js
@@ -0,0 +1,71 @@
+/* globals $: false */
+import styles from '../styles/components/CheckableTag.module.scss'
+import Icon from './Icon'
+
+const CheckableTag = ({
+  label,
+  tagText,
+  noTagText,
+  rawCount,
+  checked,
+  onChange,
+  onDelete,
+}) => {
+  const formattedCount = new Intl.NumberFormat('en', { notation: 'compact' }).format(rawCount)
+  const showCount = Number.isNaN(rawCount)
+
+  const getColorClass = () => {
+    switch (label) {
+      case 'require vouch':
+        return styles.requireVouch
+      case 'potential spam':
+        return styles.potentialSpam
+      default:
+        return ''
+    }
+  }
+
+  return (
+    <div
+      className={`${styles.checkableTagContainer} ${getColorClass()}`}
+      onClick={onChange ?? onDelete}
+    >
+      <span className={`${styles.iconLabel} ${checked ? styles.checked : ''}`}>{label}</span>
+      {tagText && noTagText && (
+        <span className={showCount ? styles.tagText : ''}>
+          {checked ? tagText : noTagText}
+        </span>
+      )}
+      {showCount && (
+        <span
+          className={styles.count}
+          data-toggle="tooltip"
+          data-placement="top"
+          title={rawCount}
+          onMouseEnter={(e) => {
+            $(e.target).tooltip('show')
+          }}
+          onMouseLeave={(e) => {
+            $(e.target).tooltip('destroy')
+          }}
+        >
+          {formattedCount}
+        </span>
+      )}
+      {onDelete && (
+        <div
+          className={styles.trashButton}
+          onClick={(e) => {
+            e.preventDefault()
+            e.stopPropagation()
+            onDelete()
+          }}
+        >
+          <Icon name="trash" />
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default CheckableTag
