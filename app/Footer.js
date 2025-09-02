00b3a5fd68e280c6db4b08060a0ca71be9dfe8e9
xkopenreview <xukun@openreview.net>
2025-09-02 14:01:08 -0400

    change posititon;bold;remove join team

--- app/Footer.js
+++ app/Footer.js
@@ -29,21 +29,12 @@ export default function Footer() {
                   <Link href="/contact">Contact</Link>
                 </li>
                 <li>
-                  <Link href="https://donate.stripe.com/eVqdR8fP48bK1R61fi0oM00">Donate</Link>
-                </li>
-                <li></li>
-                <li>
                   <Link href="/sponsors">Sponsors</Link>
                 </li>
                 <li>
-                  <a
-                    className="join-the-team"
-                    href="https://codeforscience.org/jobs?job=OpenReview-Developer"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <strong>Join the Team</strong>
-                  </a>
+                  <Link href="https://donate.stripe.com/eVqdR8fP48bK1R61fi0oM00">
+                    <strong>Donate</strong>
+                  </Link>
                 </li>
               </ul>
             </div>
@@ -83,16 +74,6 @@ export default function Footer() {
                 <li>
                   <Link href="/sponsors">Sponsors</Link>
                 </li>
-                <li>
-                  <a
-                    className="join-the-team"
-                    href="https://codeforscience.org/jobs?job=OpenReview-Developer"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <strong>Join the Team</strong>
-                  </a>
-                </li>
               </ul>
             </div>
 
@@ -107,7 +88,9 @@ export default function Footer() {
                   <Link href="/contact">Contact</Link>
                 </li>
                 <li>
-                  <Link href="https://donate.stripe.com/eVqdR8fP48bK1R61fi0oM00">Donate</Link>
+                  <Link href="https://donate.stripe.com/eVqdR8fP48bK1R61fi0oM00">
+                    <strong>Donate</strong>
+                  </Link>
                 </li>
                 <li>
                   <Link href="/legal/terms">Terms of Use</Link>
