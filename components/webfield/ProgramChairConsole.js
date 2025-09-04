85505361a9efbbb128d8b60531b3939017b05a1b
xkopenreview <xukun@openreview.net>
2025-09-04 12:49:04 -0400

    some fix

--- components/webfield/ProgramChairConsole.js
+++ components/webfield/ProgramChairConsole.js
@@ -685,6 +685,8 @@ const ProgramChairConsole = ({ appContext, extraTabs = [] }) => {
         }
         // eslint-disable-next-line no-param-reassign
         note.replyCount = replies.length
+        // eslint-disable-next-line no-param-reassign
+        delete note.details?.replies
       })
 
       const consoleData = {
