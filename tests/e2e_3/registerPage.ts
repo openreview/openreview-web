26002fb49f5a17c12b230e3bec41503df72138d0
xkopenreview <xukun@openreview.net>
2025-10-30 09:53:42 -0400

    test failing group3

--- tests/e2e_3/registerPage.ts
+++ tests/e2e_3/registerPage.ts
@@ -1,4 +1,4 @@
-import { Selector, ClientFunction, Role, RequestLogger } from 'testcafe'
+import { Selector, ClientFunction, RequestLogger } from 'testcafe'
 import {
   inactiveUser,
   inActiveUserNoPassword,
@@ -9,23 +9,6 @@ import {
   strongPassword,
 } from '../utils/api-helper'
 
-const SURole = Role(`http://localhost:${process.env.NEXT_PORT}`, async (t) => {
-  await t
-    .click(Selector('a').withText('Login'))
-    .typeText(Selector('#email-input'), superUserName)
-    .typeText(Selector('#password-input'), strongPassword)
-    .wait(100)
-    .click(Selector('button').withText('Login to OpenReview'))
-})
-
-const EmailOwnerRole = Role(`http://localhost:${process.env.NEXT_PORT}`, async (t) => {
-  await t
-    .click(Selector('a').withText('Login'))
-    .typeText(Selector('#email-input'), 'melisa@test.com')
-    .typeText(Selector('#password-input'), strongPassword)
-    .wait(100)
-    .click(Selector('button').withText('Login to OpenReview'))
-})
 
 const fullNameInputSelector = Selector('#first-input')
 const emailAddressInputSelector = Selector('input').withAttribute(
@@ -129,6 +112,7 @@ test('create new profile', async (t) => {
 })
 
 test('create another new profile', async (t) => {
+  await t.expect(1).eql(0)
   await t
     .typeText(fullNameInputSelector, 'Peter Pan')
     .expect(emailAddressInputSelector.exists)
