96d4bc418ced9cdadb1ae25a9cd652af14f4ee2f
xkopenreview <60613434+xkopenreview@users.noreply.github.com>
2025-11-03 09:38:46 -0500

    Fix/ Profile tags feedback (#2372)
    * Update ProfileTags and Compare components to handle tag display and invitation logic
    
    * add some test
    
    * update signature invitation compare logic
    
    * remove label of vouch invitation and adjust dropdown styles
    
    * display error inside preview modal
    
    * Fix error handling in ProfilePreviewModal by updating variable name in catch block
    
    * some changes based on feedback
    
    * some changes based on feedback
    
    * Fix: Update Profile component layout and adjust margin in styles
    
    ---------
    
    Co-authored-by: Melisa Bok <545506+melisabok@users.noreply.github.com>

--- components/ProfileTag.js
+++ components/ProfileTag.js
@@ -1,18 +1,31 @@
 /* globals $: false */
 import { useEffect } from 'react'
-import { prettyInvitationId, prettyId } from '../lib/utils'
+import { getTagDispayText } from '../lib/utils'
 import styles from '../styles/components/ProfileTag.module.scss'
 import Icon from './Icon'
 
-const ProfileTag = ({ tag, onDelete }) => {
+const ProfileTag = ({ tag, onDelete, showProfileId }) => {
+  const { label, invitation, readers, signature } = tag
+  const deletable = invitation.startsWith(`${process.env.SUPER_USER}/Support`)
+  const isPrivateTag = !readers.includes('everyone')
+  let tagLink = null
+  if (invitation === `${process.env.SUPER_USER}/Support/-/Vouch`) {
+    tagLink = `/profile?id=${encodeURIComponent(signature)}`
+  } else if (!invitation.startsWith(`${process.env.SUPER_USER}/Support`)) {
+    tagLink = `/group?id=${encodeURIComponent(signature)}`
+  }
+
   const getColorClass = () => {
-    if (tag.label === 'require vouch') return styles.requireVouch
-    if (tag.label === 'potential spam') return styles.potentialSpam
+    if (label === 'require vouch') return styles.requireVouch
+    if (label === 'potential spam') return styles.potentialSpam
     return ''
   }
-  const deletable = tag.invitation.startsWith(`${process.env.SUPER_USER}/Support`)
-  const displayLabelText = `${prettyInvitationId(tag.invitation)}${tag.label !== undefined ? ` "${tag.label}"` : ''}${tag.weight !== undefined ? ` (${tag.weight})` : ''}${!tag.invitation.startsWith(tag.signature) ? '' : ` by ${prettyId(tag.signature)}`}`
-  const isPrivateTag = !tag.readers.includes('everyone')
+
+  const handleTagClick = () => {
+    if (tagLink) {
+      window.open(tagLink, '_blank', 'noopener,noreferrer')
+    }
+  }
 
   useEffect(() => {
     $('[data-toggle="tooltip"]').tooltip({ html: true })
@@ -20,17 +33,24 @@ const ProfileTag = ({ tag, onDelete }) => {
 
   return (
     <div
-      className={`${styles.profileTagContainer} ${getColorClass()} ${deletable ? styles.deletable : ''}`}
-      {...(deletable ? { onClick: onDelete } : {})}
-      data-toggle="tooltip"
-      data-placement="top"
-      {...(isPrivateTag ? { title: `Visible to <br/> ${tag.readers.join(',<br/>')}` } : {})}
+      className={`${styles.profileTagContainer} ${getColorClass()} ${deletable ? styles.deletable : ''} ${tagLink ? styles.withTagLink : ''}`}
     >
-      <span>{displayLabelText}</span>
-      {isPrivateTag && <Icon name="eye-open" extraClasses={styles.readerIcon} />}
+      <span onClick={handleTagClick}>{getTagDispayText(tag, showProfileId)}</span>
+      {isPrivateTag && (
+        <div
+          data-toggle="tooltip"
+          data-placement="top"
+          {...(isPrivateTag ? { title: `Visible to <br/> ${readers.join(',<br/>')}` } : {})}
+        >
+          <Icon name="eye-open" extraClasses={styles.readerIcon} />
+        </div>
+      )}
       {onDelete && deletable && (
         <div
           className={styles.trashButton}
+          data-toggle="tooltip"
+          data-placement="top"
+          title="Delete tag"
           onClick={(e) => {
             e.preventDefault()
             e.stopPropagation()
