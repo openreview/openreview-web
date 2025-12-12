f5301a46ffd5c66be527835811f55d7f6510fc45
xkopenreview <xukun@openreview.net>
2025-12-11 19:41:31 -0500

    Feat: Add loading spinner to donation layout

--- app/donate/layout.js
+++ app/donate/layout.js
@@ -1,7 +1,13 @@
+import { Suspense } from 'react'
 import CommonLayout from '../CommonLayout'
+import LoadingSpinner from '../../components/LoadingSpinner'
 
 export default function Layout({ children }) {
-  return <CommonLayout banner={null}>{children}</CommonLayout>
+  return (
+    <CommonLayout banner={null}>
+      <Suspense fallback={<LoadingSpinner />}>{children}</Suspense>
+    </CommonLayout>
+  )
 }
 
 export const metadata = {
