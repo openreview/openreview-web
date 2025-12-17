de8ae5d858acefc197d2c86775fa8ef069660c93
Melisa Bok <>
2025-12-17 09:22:26 -0300

    add donate link to footer

--- app/Footer.js
+++ app/Footer.js
@@ -32,7 +32,7 @@ export default function Footer() {
                   <Link href="/sponsors">Sponsors</Link>
                 </li>
                 <li>
-                  <Link href="https://donate.stripe.com/eVqdR8fP48bK1R61fi0oM00">
+                  <Link href="/donate">
                     <strong>Donate</strong>
                   </Link>
                 </li>
@@ -100,7 +100,7 @@ export default function Footer() {
                   <Link href="/contact">Contact</Link>
                 </li>
                 <li>
-                  <Link href="https://donate.stripe.com/eVqdR8fP48bK1R61fi0oM00">
+                  <Link href="/donate">
                     <strong>Donate</strong>
                   </Link>
                 </li>
