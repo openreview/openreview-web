a65460cb5b9f65c15f623b80f5edd21f65a3b547
xkopenreview <60613434+xkopenreview@users.noreply.github.com>
2025-12-17 07:38:51 -0500

    Fix/ Donate - add donate page (#2580)
    * Feat: Implement donation page and footer links
    
    * Feat: Add loading spinner to donation layout
    
    * Fix: Update donation logic and improve input handling
    
    * refactor donate form state and add unit tests
    
    * Feat: Disable donate button on form submission
    
    * remove benefits; no show checkbox till amount selection
    
    * Feat: Update donation form to include IRS receipt option and improve button text
    
    * Fix: Update donation contact messages for clarity on donation amounts
    
    * revert footer changes
    
    * update text
    
    * some changes based on feedback
    
    * Melisa's feedback
    
    * add redirection string
    
    * remove success message
    
    * change thousands
    
    * show thank you once
    
    * fix test caused by button text change
    
    * update left text
    
    * add donate link to footer
    
    ---------
    
    Co-authored-by: Melisa Bok <>

---
+++ unitTests/Donate.test.js
@@ -0,0 +1,177 @@
+import Donate from '../app/donate/page'
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import api from '../lib/api-client'
+
+let mockedUser
+
+jest.mock('../hooks/useUser', () => () => mockedUser)
+jest.mock('next/navigation', () => ({
+  useSearchParams: () => ({
+    get: () => jest.fn(),
+  }),
+}))
+
+beforeEach(() => {
+  api.post = jest.fn()
+  global.promptMessage = jest.fn()
+  global.promptError = jest.fn()
+  delete window.location
+  window.location = { href: '' }
+  mockedUser = {}
+})
+
+describe('Donation Page', () => {
+  test('render all required buttons', () => {
+    render(<Donate />)
+
+    expect(screen.queryByRole('button', { name: 'Monthly' })).not.toBeInTheDocument()
+    expect(screen.queryByRole('button', { name: 'One-Time' })).not.toBeInTheDocument()
+
+    expect(screen.getByText('$10')).toBeInTheDocument()
+    expect(screen.getByText('$50')).toBeInTheDocument()
+    expect(screen.getByText('$100')).toBeInTheDocument()
+    expect(screen.getByText('$500')).toBeInTheDocument()
+    expect(screen.getByText('$1,000')).toBeInTheDocument()
+    expect(screen.getByText('$5,000')).toBeInTheDocument()
+    expect(screen.getByText('$10,000')).toBeInTheDocument()
+
+    expect(screen.getByPlaceholderText('$ Other Amount')).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('button', { name: 'Make a Donation through Stripe' })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('button', { name: 'Make a Donation through Stripe' })
+    ).toBeDisabled()
+  })
+
+  test('update selection in button text', async () => {
+    render(<Donate />)
+
+    expect(
+      screen.getByRole('button', { name: 'Make a Donation through Stripe' })
+    ).toBeInTheDocument() // no change when no amount
+
+    await userEvent.click(screen.getByText('$10'))
+    expect(screen.getByText('Make a Donation of $10.00 through Stripe'))
+
+    await userEvent.click(screen.getByText('$10,000'))
+    expect(screen.getByText('Make a Donation of $10000.00 through Stripe'))
+
+    // enter custom amount
+    await userEvent.type(screen.getByPlaceholderText('$ Other Amount'), '999')
+    expect(screen.getByText('Make a Donation of $999.00 through Stripe'))
+
+    await userEvent.clear(screen.getByPlaceholderText('$ Other Amount'))
+    await userEvent.type(screen.getByPlaceholderText('$ Other Amount'), '666', {
+      replace: true,
+    })
+    expect(screen.getByText('Make a Donation of $666.00 through Stripe'))
+  })
+
+  test('handle max amount (custom amount)', async () => {
+    render(<Donate />)
+
+    await userEvent.type(screen.getByPlaceholderText('$ Other Amount'), '12345', {
+      replace: true,
+    })
+    expect(screen.getByText('Make a Donation through Stripe')).toBeDisabled()
+    expect(global.promptMessage).toHaveBeenCalled()
+  })
+
+  test('post correct amount (no user)', async () => {
+    render(<Donate />)
+
+    // fixed amount
+    await userEvent.click(screen.getByText('$100'))
+    await userEvent.click(screen.getByText('Make a Donation of $100.00 through Stripe'))
+    expect(api.post).toHaveBeenCalledWith(
+      // would fail though
+      expect.anything(),
+      {
+        amount: 100,
+        mode: 'payment',
+        email: undefined,
+        irsReceipt: false,
+      },
+      expect.anything()
+    )
+    expect(screen.getByText('Make a Donation of $100.00 through Stripe')).toBeDisabled() // button disabled after submit
+
+    // custom amount
+    await userEvent.clear(screen.getByPlaceholderText('$ Other Amount'))
+    await userEvent.type(screen.getByPlaceholderText('$ Other Amount'), '10000', {
+      replace: true,
+    })
+    await userEvent.click(screen.getByText('Make a Donation of $10000.00 through Stripe'))
+    expect(api.post).toHaveBeenLastCalledWith(
+      expect.anything(),
+      {
+        amount: 10000,
+        mode: 'payment',
+        email: undefined,
+        irsReceipt: true, // auto checked
+      },
+      expect.anything()
+    )
+  })
+
+  test('post correct amount (with user)', async () => {
+    mockedUser = {
+      user: { profile: { preferredEmail: 'test@email.com' } },
+    }
+
+    render(<Donate />)
+
+    // fixed amount
+    await userEvent.click(screen.getByText('$100'))
+    await userEvent.click(screen.getByText('Make a Donation of $100.00 through Stripe'))
+    expect(api.post).toHaveBeenCalledWith(
+      expect.anything(),
+      {
+        amount: 100,
+        mode: 'payment',
+        email: 'test@email.com',
+        irsReceipt: false,
+      },
+      expect.anything()
+    )
+
+    // check IRS receipt
+    await userEvent.click(screen.getByText('$10'))
+    await userEvent.click(
+      screen.getByRole('checkbox', {
+        name: 'Send me an IRS receipt for tax purposes',
+      })
+    )
+    await userEvent.click(screen.getByText('Make a Donation of $10.00 through Stripe'))
+    expect(api.post).toHaveBeenLastCalledWith(
+      expect.anything(),
+      {
+        amount: 10,
+        mode: 'payment',
+        email: 'test@email.com',
+        irsReceipt: true,
+      },
+      expect.anything()
+    )
+
+    // custom amount
+    await userEvent.type(screen.getByPlaceholderText('$ Other Amount'), '123', {
+      replace: true,
+    })
+    await userEvent.click(screen.getByText('Make a Donation of $123.00 through Stripe'))
+    expect(api.post).toHaveBeenLastCalledWith(
+      expect.anything(),
+      {
+        amount: 123,
+        mode: 'payment',
+        email: 'test@email.com',
+        irsReceipt: true,
+      },
+      expect.anything()
+    )
+  })
+})
