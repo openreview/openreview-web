4d47509645b872b03cc204de6cbd5942060dc934
xkopenreview <60613434+xkopenreview@users.noreply.github.com>
2025-12-23 12:50:39 -0500

    Fix/ Banner - replace open...open... default banner with donate banner (#2598)
    * Add DonateBanner component and styles for donation prompts
    
    * keep original text
    
    ---------
    
    Co-authored-by: Melisa Bok <545506+melisabok@users.noreply.github.com>
    Co-authored-by: Melisa Bok <>

--- components/Banner.js
+++ components/Banner.js
@@ -1,22 +1,47 @@
-export default function Banner({ hidden, children }) {
-  const defaultContent = (
-    <span className="tagline">
+import Link from 'next/link'
+import useBreakpoint from '../hooks/useBreakPoint'
+
+import styles from '../styles/components/Banner.module.scss'
+
+const DonateBanner = () => {
+  const isMobile = !useBreakpoint('lg')
+  const defaultContent = isMobile ? (
+    <div className={styles.donateBanner}>
+      Open Peer Review. Open Publishing. Open Access.{' '}
+      <Link href="/donate" className="donate-link">
+        Donate
+      </Link>
+    </div>
+  ) : (
+    <div className={styles.donateBanner}>
       Open Peer Review. Open Publishing. Open Access.{' '}
       <span className="hidden-xs">Open Discussion. Open Recommendations.</span>{' '}
-      <span className="hidden-xs hidden-sm">Open Directory. Open API. Open Source.</span>
-    </span>
+      <span className="hidden-xs hidden-sm">Open Directory. Open API. Open Source.</span>{' '}
+      <Link href="/donate" className="donate-link">
+        Donate
+      </Link>
+    </div>
   )
-
   return (
-    <div
-      id="or-banner"
-      className="banner"
-      role="banner"
-      style={hidden ? { display: 'none' } : null}
-    >
+    <div id="or-banner" className="banner" role="banner">
       <div className="container">
         <div className="row">
-          <div className="col-xs-12">{children || defaultContent}</div>
+          <div className="col-xs-12">{defaultContent}</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default function Banner({ hidden, children }) {
+  if (hidden) return null
+  if (!children) return <DonateBanner />
+
+  return (
+    <div id="or-banner" className="banner" role="banner">
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12">{children}</div>
         </div>
       </div>
     </div>
