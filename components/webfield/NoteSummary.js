2e9a324827aa0bea5ab25eaec548f53678eb4535
xkopenreview <xukun@openreview.net>
2026-02-20 14:22:14 -0500

    Fix/ Author activation status - Enhance profile status indicators in NoteSummary component

--- components/webfield/NoteSummary.js
+++ components/webfield/NoteSummary.js
@@ -1,7 +1,7 @@
 /* globals promptError: false */
 import isEqual from 'lodash/isEqual'
 import { useState } from 'react'
-import { forumDate, getNotePdfUrl } from '../../lib/utils'
+import { forumDate, getNotePdfUrl, isValidEmail } from '../../lib/utils'
 import Collapse from '../Collapse'
 import Icon from '../Icon'
 import NoteContent, { NoteContentV2 } from '../NoteContent'
@@ -38,30 +38,52 @@ const NoteSummary = ({
   const [reportLink, setReportLink] = useState(null)
   const [isLoadingReportLink, setIsLoadingReportLink] = useState(false)
 
+  const getAuthorActivationStatus = (isEmail, hasProfile, isActiveProfile) => {
+    if (isEmail) {
+      return (
+        <Icon
+          name="question-sign"
+          tooltip="Profile status is unknown"
+          extraClasses="pl-1 text-default"
+        />
+      )
+    }
+    if (!hasProfile) {
+      return (
+        <Icon
+          name="minus-sign"
+          tooltip="Profile is not available"
+          extraClasses="pl-1 text-default"
+        />
+      )
+    }
+    if (!isActiveProfile) {
+      return (
+        <Icon
+          name="remove-sign"
+          tooltip="Profile is not yet activated"
+          extraClasses="pl-1 text-danger"
+        />
+      )
+    }
+    return <Icon name="ok-sign" tooltip="Profile is active" extraClasses="pl-1 text-success" />
+  }
+
   const authorNames = authorsValue?.map((authorName, i) => {
     const authorId = authorIdsValue[i]
     const authorProfile = profileMap?.[authorIdsValue[i]]
-    const errorTooltip = authorProfile
-      ? 'Profile not yet activated'
-      : 'Profile not yet created or email not confirmed'
+    const isActiveProfile = authorProfile?.active
+    const isEmailAuthorId = isValidEmail(authorId)
 
     return (
-      <span key={authorId}>
+      <span key={authorId} className="text-nowrap">
         <ProfileLink
           id={authorId}
           name={authorName}
           preferredEmailInvitationId={preferredEmailInvitationId}
         />
         {profileMap &&
-          (authorProfile?.active ? (
-            <Icon
-              name="ok-sign"
-              tooltip="Profile is active and email confirmed"
-              extraClasses="pl-1 text-success"
-            />
-          ) : (
-            <Icon name="remove-sign" tooltip={errorTooltip} extraClasses="pl-1 text-danger" />
-          ))}
+          getAuthorActivationStatus(isEmailAuthorId, !!authorProfile, isActiveProfile)}
       </span>
     )
   })
