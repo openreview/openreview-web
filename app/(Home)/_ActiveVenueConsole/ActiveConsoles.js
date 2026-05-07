723054cd206d79f65af8e71becf9df0839c59702
xkopenreview <xukun@openreview.net>
2026-05-07 14:29:52 -0400

    Fix/ ActiveConsoles - return ActiveVenues component when no venues are found

--- app/(Home)/_ActiveVenueConsole/ActiveConsoles.js
+++ app/(Home)/_ActiveVenueConsole/ActiveConsoles.js
@@ -1,6 +1,7 @@
 import { headers } from 'next/headers'
 import api from '../../../lib/api-client'
 import VenueList from '../VenueList'
+import ActiveVenues from './ActiveVenues'
 
 export default async function ActiveConsoles({ activeVenues, openVenues, user, token }) {
   const activeAndOpenVenues = activeVenues.concat(openVenues)
@@ -34,7 +35,7 @@ export default async function ActiveConsoles({ activeVenues, openVenues, user, t
     })
   }
 
-  if (!venues?.length) return null
+  if (!venues?.length) return <ActiveVenues venues={activeVenues} />
 
   return (
     <section>
