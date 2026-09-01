10dbd9d83467502af3faca674b69dccd01066033
xkopenreview <xukun@openreview.net>
2026-09-01 13:55:20 -0400

    Refactor IdentityDocumentsSection and introduce ParentalConsentSection for better document management

--- unitTests/IdentityDocumentsSection.test.js
+++ unitTests/IdentityDocumentsSection.test.js
@@ -1,6 +1,10 @@
 import { screen, render, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import IdentityDocumentsSection from '../components/profile/IdentityDocumentsSection'
+import {
+  IdentityDocumentsSection,
+  ParentalConsentSection,
+  ProfileDocumentsPreview,
+} from '../components/profile/IdentityDocumentsSection'
 import api from '../lib/api-client'
 import '@testing-library/jest-dom'
 
@@ -11,13 +15,13 @@ jest.mock('antd', () => ({
   Image: { PreviewGroup: () => <span>preview</span> },
 }))
 
-describe('IdentityDocumentsSection', () => {
+describe('ProfileDocumentsPreview', () => {
   test('show loading spinner when documents are being loaded', () => {
     const props = {
       profileDocuments: undefined,
     }
 
-    render(<IdentityDocumentsSection {...props} />)
+    render(<ProfileDocumentsPreview {...props} />)
     expect(screen.getByText('loading spinner')).toBeInTheDocument()
   })
 
@@ -26,9 +30,9 @@ describe('IdentityDocumentsSection', () => {
       profileDocuments: [],
     }
 
-    render(<IdentityDocumentsSection {...props} />)
+    render(<ProfileDocumentsPreview {...props} />)
     expect(screen.queryByText('loading spinner')).not.toBeInTheDocument()
-    expect(screen.getByText('No Identity Documents')).toBeInTheDocument()
+    expect(screen.getByText('No Documents')).toBeInTheDocument()
   })
 
   test('show image preview', () => {
@@ -45,12 +49,53 @@ describe('IdentityDocumentsSection', () => {
       ],
     }
 
-    render(<IdentityDocumentsSection {...props} />)
+    render(<ProfileDocumentsPreview {...props} />)
 
     expect(screen.getByText('preview')).toBeInTheDocument()
   })
+})
 
-  test('show default deleteAllLabel when not passed', () => {
+describe('IdentityDocumentsSection', () => {
+  test('not to show button when there are no documents', () => {
+    const props = {
+      profileDocuments: undefined,
+    }
+
+    render(<IdentityDocumentsSection {...props} />)
+
+    expect(screen.queryAllByRole('button')).toHaveLength(0)
+  })
+
+  test('not to show button when all documents are deleted documents', () => {
+    const props = {
+      profileDocuments: [
+        {
+          id: 'some id',
+          type: 'identity',
+          extension: 'pdf',
+          filename: 'some name',
+          size: '12345',
+          tcdate: 'test tcdate',
+          ddate: 'some ddate',
+        },
+        {
+          id: 'another id',
+          type: 'identity',
+          extension: 'pdf',
+          filename: 'another name',
+          size: '12345',
+          tcdate: 'test tcdate',
+          ddate: 'some ddate',
+        },
+      ],
+    }
+
+    render(<IdentityDocumentsSection {...props} />)
+
+    expect(screen.queryAllByRole('button')).toHaveLength(0)
+  })
+
+  test('show delete documents button', () => {
     const props = {
       profileDocuments: [
         {
@@ -61,18 +106,27 @@ describe('IdentityDocumentsSection', () => {
           size: '12345',
           tcdate: 'test tcdate',
         },
+        {
+          id: 'another id',
+          type: 'identity',
+          extension: 'pdf',
+          filename: 'another name',
+          size: '12345',
+          tcdate: 'test tcdate',
+        },
       ],
-      deleteAllLabel: undefined,
+      isProfileActivatable: false, // for exmaple profile is already active
     }
 
     render(<IdentityDocumentsSection {...props} />)
 
+    expect(screen.queryAllByRole('button')).toHaveLength(1)
     expect(
-      screen.getByRole('button', { name: 'Delete All Identity Documents' })
+      screen.getByRole('button', { name: 'Delete Identity Documents' })
     ).toBeInTheDocument()
   })
 
-  test('allow custom deleteAllLabel', () => {
+  test('show delete documents and activate profile button', () => {
     const props = {
       profileDocuments: [
         {
@@ -83,18 +137,29 @@ describe('IdentityDocumentsSection', () => {
           size: '12345',
           tcdate: 'test tcdate',
         },
+        {
+          id: 'another id',
+          type: 'identity',
+          extension: 'pdf',
+          filename: 'another name',
+          size: '12345',
+          tcdate: 'test tcdate',
+        },
       ],
-      deleteAllLabel: 'Activate with ID check',
+      isProfileActivatable: true,
     }
 
     render(<IdentityDocumentsSection {...props} />)
 
+    expect(screen.queryAllByRole('button')).toHaveLength(2)
+    expect(screen.getByRole('button', { name: 'Delete Documents Only' })).toBeInTheDocument()
     expect(screen.getByRole('button', { name: 'Activate with ID check' })).toBeInTheDocument()
   })
 
   test('show confirm window before deleting all documents', async () => {
     window.confirm = jest.fn()
     const props = {
+      profileId: '~Test_Id1',
       profileDocuments: [
         {
           id: 'some id',
@@ -104,28 +169,36 @@ describe('IdentityDocumentsSection', () => {
           size: '12345',
           tcdate: 'test tcdate',
         },
+        {
+          id: 'another id',
+          type: 'identity',
+          extension: 'pdf',
+          filename: 'another name',
+          size: '12345',
+          tcdate: 'test tcdate',
+        },
       ],
-      deleteAllLabel: undefined,
+      isProfileActivatable: false,
     }
 
     render(<IdentityDocumentsSection {...props} />)
 
-    const deleteButton = screen.getByRole('button', { name: 'Delete All Identity Documents' })
+    const deleteButton = screen.getByRole('button', { name: 'Delete Identity Documents' })
     await userEvent.click(deleteButton)
 
     await waitFor(() => {
       expect(window.confirm).toHaveBeenCalledWith(
-        `Identity documents of undefined will be deleted. This action cannot be undone.`
+        `Identity documents of ~Test_Id1 will be deleted. This action cannot be undone.`
       )
     })
   })
 
-  test('call beforeDelete, delete and afterDelete', async () => {
+  test('delete all documents when delete documents button is clicked', async () => {
     window.confirm = jest.fn(() => true)
-    beforeDelete = jest.fn()
-    afterDelete = jest.fn()
+    const loadIdentityDocuments = jest.fn()
+    const tagAndActivateProfile = jest.fn()
+    const loadTags = jest.fn()
     api.delete = jest.fn(() => Promise.resolve({ deletedCount: 1 }))
-    loadIdentityDocuments = jest.fn()
     global.promptMessage = jest.fn()
 
     const props = {
@@ -139,24 +212,122 @@ describe('IdentityDocumentsSection', () => {
           size: '12345',
           tcdate: 'test tcdate',
         },
+        {
+          id: 'another id',
+          type: 'identity',
+          extension: 'pdf',
+          filename: 'another name',
+          size: '12345',
+          tcdate: 'test tcdate',
+        },
       ],
-      deleteAllLabel: undefined,
-      onBeforeDeleteAll: beforeDelete,
-      onAfterDeleteAll: afterDelete,
-      loadIdentityDocuments: loadIdentityDocuments,
+      isProfileActivatable: true,
+      loadIdentityDocuments,
+      tagAndActivateProfile,
+      loadTags,
     }
 
     render(<IdentityDocumentsSection {...props} />)
 
-    const deleteButton = screen.getByRole('button', { name: 'Delete All Identity Documents' })
+    const deleteButton = screen.getByRole('button', { name: 'Delete Documents Only' })
     await userEvent.click(deleteButton)
 
     await waitFor(() => {
-      expect(beforeDelete).toHaveBeenCalled()
       expect(api.delete).toHaveBeenCalledWith('/profile-documents/identity/profiles/~Test_Id1')
       expect(global.promptMessage).toHaveBeenCalledWith('1 document has been deleted')
       expect(loadIdentityDocuments).toHaveBeenCalled()
-      expect(afterDelete).toHaveBeenCalled()
+      expect(tagAndActivateProfile).not.toHaveBeenCalled()
+      expect(loadTags).not.toHaveBeenCalled()
     })
+  })
+
+  test('delete all documents and activate profile when activate with id check button is clicked', async () => {
+    window.confirm = jest.fn(() => true)
+    const loadIdentityDocuments = jest.fn()
+    const tagAndActivateProfile = jest.fn()
+    const loadTags = jest.fn()
+    api.delete = jest.fn(() => Promise.resolve({ deletedCount: 1 }))
+    global.promptMessage = jest.fn()
+
+    const props = {
+      profileId: '~Test_Id1',
+      profileDocuments: [
+        {
+          id: 'some id',
+          type: 'identity',
+          extension: 'pdf',
+          filename: 'some name',
+          size: '12345',
+          tcdate: 'test tcdate',
+        },
+        {
+          id: 'another id',
+          type: 'identity',
+          extension: 'pdf',
+          filename: 'another name',
+          size: '12345',
+          tcdate: 'test tcdate',
+        },
+      ],
+      isProfileActivatable: true,
+      loadIdentityDocuments,
+      tagAndActivateProfile,
+      loadTags,
+    }
+
+    render(<IdentityDocumentsSection {...props} />)
+
+    const activateWithIDCheckButton = screen.getByRole('button', {
+      name: 'Activate with ID check',
+    })
+    await userEvent.click(activateWithIDCheckButton)
+
+    await waitFor(() => {
+      expect(api.delete).toHaveBeenCalledWith('/profile-documents/identity/profiles/~Test_Id1')
+      expect(global.promptMessage).toHaveBeenCalledWith('1 document has been deleted')
+      expect(loadIdentityDocuments).toHaveBeenCalled()
+      expect(tagAndActivateProfile).toHaveBeenCalled()
+      expect(loadTags).toHaveBeenCalled()
+    })
+  })
+})
+
+describe('ParentalConsentSection', () => {
+  test('show parental consent documents', () => {
+    const props = {
+      profileDocuments: [
+        {
+          id: 'some id',
+          type: 'parentalConsent',
+          extension: 'pdf',
+          filename: 'some name',
+          size: '12345',
+          tcdate: 'test tcdate',
+        },
+      ],
+    }
+
+    render(<ParentalConsentSection {...props} />)
+
+    expect(screen.getByText('preview')).toBeInTheDocument()
+  })
+
+  test('not to show identity documents', () => {
+    const props = {
+      profileDocuments: [
+        {
+          id: 'some id',
+          type: 'identity',
+          extension: 'pdf',
+          filename: 'some name',
+          size: '12345',
+          tcdate: 'test tcdate',
+        },
+      ],
+    }
+
+    render(<ParentalConsentSection {...props} />)
+
+    expect(screen.queryByText('preview')).not.toBeInTheDocument()
   })
 })
