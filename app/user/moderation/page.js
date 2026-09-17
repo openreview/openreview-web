f12cdfeace9c717febee6cb1e3c7b2f65d6d380f
xkopenreview <xukun@openreview.net>
2026-09-17 14:42:30 -0400

    Reorder imports in moderation page for consistency

--- app/user/moderation/page.js
+++ app/user/moderation/page.js
@@ -1,8 +1,8 @@
-import serverAuth from '../../auth'
-import Moderation from './Moderation'
-import CommonLayout from '../../CommonLayout'
 import ErrorDisplay from '../../../components/ErrorDisplay'
 import { checkAccess } from '../../../lib/server-access-control'
+import serverAuth from '../../auth'
+import CommonLayout from '../../CommonLayout'
+import Moderation from './Moderation'
 
 const errorMessage = 'Forbidden. Access to this page is restricted.'
 
